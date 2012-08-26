223a7ead429406cfa854a9208c25b5eab309f8db
Daniel Halperin <dhalperi@cs.washington.edu>
2012-08-25 22:18:53 -0700

    myriad: rename project, make Main program work
    Swap to dsp.cs.washington.edu as the test database.
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- src/edu/washington/escience/parallel/Main.java
+++ src/edu/washington/escience/parallel/Main.java
@@ -8,12 +8,19 @@ import edu.washington.escience.Schema;
 import edu.washington.escience.Type;
 
 public class Main {
+  final static String host = "dsp.cs.washington.edu";
+  final static int port = 3306;
+  final static String user = "myriad";
+  final static String password = "nays26[shark";
+  final static String dbms = "mysql";
+  final static String databaseName = "myriad_test";
 
   public static void main(String[] args) throws NoSuchElementException, DbException {
-
+    String connectionString =
+        "jdbc:" + dbms + "://" + host + ":" + port + "/" + databaseName + "?user=" + user
+        + "&password=" + password;
     JdbcSeqScan scan =
-        new JdbcSeqScan("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/test",
-            "select * from testtable");
+        new JdbcSeqScan("com.mysql.jdbc.Driver", connectionString, "select * from testtable");
     Filter filter1 = new Filter(Predicate.Op.GREATER_THAN_OR_EQ, 0, new Integer(50), scan);
 
     Filter filter2 = new Filter(Predicate.Op.LESS_THAN_OR_EQ, 0, new Integer(60), filter1);
@@ -32,7 +39,7 @@ public class Main {
     Schema schema = root.getSchema();
 
     if (schema != null) {
-      System.out.println(schema);
+      System.out.println("Schema of result is: " + schema);
     } else
       return;
 
