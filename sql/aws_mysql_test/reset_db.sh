f60ee1407f2a4f8228dc23bd66d5dc0e1e672c0d
Daniel Halperin <dhalperi@cs.washington.edu>
2012-08-31 01:27:08 -0700

    myriad: switch from AWS instance DNS name to ElasticIP
    The former changes every reboot; the latter is supposed to always be
    associated with a running instance.
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- sql/aws_mysql_test/reset_db.sh
+++ sql/aws_mysql_test/reset_db.sh
@@ -1,1 +1,1 @@
-mysql5 --host=ec2-50-112-55-255.us-west-2.compute.amazonaws.com -P3306 --user=myriad --password='nays26[shark' myriad_test < setup_testtable.sql
+mysql5 --host=54.245.108.198 -P3306 --user=myriad --password='nays26[shark' myriad_test < setup_testtable.sql
