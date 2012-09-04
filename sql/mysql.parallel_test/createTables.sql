947a175c2fb3df998cd1b77b1d6233700648c380
slxu <slxu@slxu-900A.(none)>
2012-09-04 15:07:36 -0700

    debug the sql

--- sql/mysql.parallel_test/createTables.sql
+++ sql/mysql.parallel_test/createTables.sql
@@ -1,14 +1,14 @@
 use test;
-create table testtable1 if not exists (id int, name varchar(20));
+create table if not exists testtable1 (id int, name varchar(20));
 delete from testtable1 where true;
 insert into testtable1 (id,name) values 
 (1,'name1'), 
 (2, 'name2');
 
-create table testtable2 if not exists (id int, name varchar(20));
+create table if not exists testtable2 (id int, name varchar(20));
 delete from testtable2 where true;
 insert into testtable2 (id,name) values 
 (1,'name1'), 
 (2, 'name2');
 
-create table temptable1 if not exists (id int, name varchar(20));
+create table if not exists temptable1 (id int, name varchar(20));
