c7246f7ad34d32492632b8bbd765245cfefad188
Daniel Halperin <dhalperi@cs.washington.edu>
2012-09-06 08:22:15 -0400

    JbdcAccessMethod: add test method with big table
    This test simply uses JdbcQueryScan to 'select * from testtablebig' and
    count up the number of results. The connection IP is the elastic IP of
    the AWS micro instance hosting the MySQL myriad_test database, and the
    other credentials are hardcoded in the Java file.
    
    The database itself is configured using the sql/aws_mysql_test scripts,
    which are also updated to setup the testtablebig. reset_db.sh must be
    run at least once before this test will pass. (As of this commit, it has
    been run.)
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- sql/aws_mysql_test/reset_db.sh
+++ sql/aws_mysql_test/reset_db.sh
@@ -1,1 +1,2 @@
 mysql5 --host=54.245.108.198 -P3306 --user=myriad --password='nays26[shark' myriad_test < setup_testtable.sql
+mysql5 --host=54.245.108.198 -P3306 --user=myriad --password='nays26[shark' myriad_test < setup_testtablebig.sql
