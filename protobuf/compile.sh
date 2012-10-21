44bfbf25d7338ea1c869516deaa902501be78956
Bill Howe <billhowe@cs.washington.edu>
2012-10-21 16:09:36 -0700

    Added a message to compile.sh with instructions on making the result compatible with the web app. (replacing google.protobuf with protobuf)

--- protobuf/compile.sh
+++ protobuf/compile.sh
@@ -1,1 +1,4 @@
 protoc *.proto --java_out=./java --python_out=./python --cpp_out=./cpp
+echo "To run the datalog web application, you must replace _google.protobuf_ with _protobuf_ in logical_ra_pb2.py!"
+echo "Use this command:"
+echo "sed -i \"\" 's/google\.protobuf/protobuf/g' python/logical_ra_pb2.py"
