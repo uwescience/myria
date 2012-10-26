3ff5d8fd0760aafb7310a4179879c21d6bd12fd6
Daniel Halperin <dhalperi@cs.washington.edu>
2012-10-25 22:06:03 -0700

    Catalog: rework to fit in workers
    Updated Catalog.java, CatalogMaker.java, added WorkerCatalog.java.
    
    Particular to this configuration, moved the catalogs to all be in the
    same place.
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- src/edu/washington/escience/myriad/coordinator/catalog/CatalogMaker.java
+++ src/edu/washington/escience/myriad/coordinator/catalog/CatalogMaker.java
@@ -1,11 +1,17 @@
 package edu.washington.escience.myriad.coordinator.catalog;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.commons.io.FilenameUtils;
+
+import edu.washington.escience.myriad.parallel.SocketInfo;
 
 /**
  * A helper class used to make Catalogs. This will contain the creation code for all the Catalogs we use for
@@ -23,37 +29,112 @@ public final class CatalogMaker {
   /**
    * Helper utility that creates a new Catalog with a given filename and description.
    * 
+   * @param filename specifies where the Catalog will be created.
    * @param description describes and names the Catalog. E.g., "twoNodeLocalParallel".
-   * @return a fresh Catalog with the given description, stored in "catalogs/description.catalog"
+   * @return a fresh Catalog with the given description, stored in the path given by filename.
    * @throws CatalogException if there is an error in the backend database.
    * @throws IOException if the file already exists.
    * 
    *           TODO check the description can be a file basename, e.g., it has no / or space etc.
    */
-  private static Catalog newCatalog(final String description) throws CatalogException, IOException {
+  private static Catalog newCatalog(final String filename, final String description) throws CatalogException,
+      IOException {
     Objects.requireNonNull(description);
-    final String fileName = FilenameUtils.concat("catalogs", description + ".catalog");
-    return Catalog.create(fileName, description, false);
+    return Catalog.create(filename, description, false);
   }
 
   /**
-   * Creates a Catalog for a 2-node parallel system on the local machine.
+   * Helper utility that creates a new WorkerCatalog with a given filename.
+   * 
+   * @param filename path to the WorkerCatalog database.
+   * @return a fresh WorkerCatalog.
+   * @throws CatalogException if there is an error in the backend database.
+   * @throws IOException if the file already exists.
+   * 
+   *           TODO check the description can be a file basename, e.g., it has no / or space etc.
+   */
+  private static WorkerCatalog newWorkerCatalog(final String filename) throws CatalogException, IOException {
+    Objects.requireNonNull(filename);
+    return WorkerCatalog.create(filename, false);
+  }
+
+  /**
+   * Creates a Catalog for a 2-node parallel system on the local machine and the corresponding WorkerCatalogs.
    */
   private static void makeTwoNodeLocalParallelCatalog() {
+    /* The server configuration. */
     final String description = "twoNodeLocalParallel";
+    List<SocketInfo> masters;
+    Map<Integer, SocketInfo> workers;
     try {
       Catalog c;
       try {
-        c = newCatalog(description);
+        String catalogFileName = FilenameUtils.concat(description, "master.catalog");
+        File catalogDir = new File(description);
+        while (!catalogDir.exists()) {
+          catalogDir.mkdirs();
+        }
+        c = newCatalog(catalogFileName, description);
       } catch (IOException e) {
         throw new RuntimeException("There is already a Catalog by that name", e);
       }
       c.addMaster("localhost:8001");
       c.addWorker("localhost:9001");
       c.addWorker("localhost:9002");
+      masters = c.getMasters();
+      workers = c.getWorkers();
       c.close();
     } catch (CatalogException e) {
       throw new RuntimeException(e);
+    }
+
+    /* Worker1's configuration. */
+    for (int workerId : workers.keySet()) {
+      /* Start by making the directories for the worker */
+      String dirName = FilenameUtils.concat(description, "worker_" + workerId);
+      String sqliteDirName = FilenameUtils.concat(dirName, "sqlite_dbs");
+      String tmpDirName = FilenameUtils.concat(dirName, "tmp");
+      /* .. the sqlite directory, and also on the way the worker directory. */
+      File dir = new File(sqliteDirName);
+      while (!dir.exists()) {
+        dir.mkdirs();
+      }
+      /* .. the tmp directory. */
+      dir = new File(tmpDirName);
+      while (!dir.exists()) {
+        dir.mkdirs();
+      }
+
+      String catalogName = FilenameUtils.concat(dirName, "worker.catalog");
+      WorkerCatalog wc;
+      try {
+        /* Create the catalog. */
+        try {
+          wc = newWorkerCatalog(catalogName);
+        } catch (IOException e) {
+          throw new RuntimeException("There is already a Catalog by that name", e);
+        }
+
+        /* Add any and all masters. */
+        for (SocketInfo s : masters) {
+          wc.addMaster(s.getId());
+        }
+
+        /* Add any and all masters. */
+        for (Entry<Integer, SocketInfo> w : workers.entrySet()) {
+          wc.addWorker(w.getKey(), w.getValue().getId());
+        }
+
+        /* Set up the other three configuration variables it uses. */
+        wc.setConfigurationValue("worker.identifier", "" + workerId);
+        wc.setConfigurationValue("worker.data.sqlite.dir", sqliteDirName);
+        wc.setConfigurationValue("worker.tmp.dir", tmpDirName);
+
+      } catch (CatalogException e) {
+        throw new RuntimeException(e);
+      }
+
+      wc.close();
     }
   }
 
--- src/edu/washington/escience/myriad/coordinator/catalog/WorkerCatalog.java
+++ src/edu/washington/escience/myriad/coordinator/catalog/WorkerCatalog.java
@@ -20,76 +20,69 @@ import edu.washington.escience.myriad.Schema;
 import edu.washington.escience.myriad.parallel.SocketInfo;
 
 /**
- * This class is intended to store the configuration information for a Myriad installation.
+ * This class is intended to store the configuration and catalog information for a Myriad worker.
  * 
  * @author dhalperi
  * 
  */
-public final class Catalog {
+public final class WorkerCatalog {
   /** The logger for this class. Defaults to myriad level, but could be set to a finer granularity if needed. */
   private static final Logger LOGGER = LoggerFactory.getLogger("edu.washington.escience.myriad");
 
   /**
-   * @param filename the path to the SQLite database storing the catalog.
-   * @param description specifies a description for the configuration stored in this Catalog.
-   * @return a fresh Catalog fitting the specified description.
+   * @param filename the path to the SQLite database storing the worker catalog.
+   * @return a fresh WorkerCatalog fitting the specified description.
    * @throws IOException if the specified file already exists.
    * @throws CatalogException if there is an error opening the database.
    * 
    *           TODO add some sanity checks to the filename?
    */
-  public static Catalog create(final String filename, final String description) throws IOException, CatalogException {
+  public static WorkerCatalog create(final String filename) throws IOException, CatalogException {
     Objects.requireNonNull(filename);
-    Objects.requireNonNull(description);
-    return Catalog.create(filename, description, false);
+    return WorkerCatalog.create(filename, false);
   }
 
   /**
    * @param filename the path to the SQLite database storing the catalog.
-   * @param description specifies a description for the configuration stored in this Catalog.
-   * @param overwrite specifies whether to overwrite an existing Catalog.
-   * @return a fresh Catalog fitting the specified description.
+   * @param overwrite specifies whether to overwrite an existing WorkerCatalog.
+   * @return a fresh WorkerCatalog fitting the specified description.
    * @throws IOException if overwrite is true and the specified file already exists.
    * @throws CatalogException if there is an error opening the database.
    * 
    *           TODO add some sanity checks to the filename?
    */
-  public static Catalog create(final String filename, final String description, final boolean overwrite)
-      throws IOException, CatalogException {
+  public static WorkerCatalog create(final String filename, final boolean overwrite) throws IOException,
+      CatalogException {
     Objects.requireNonNull(filename);
-    Objects.requireNonNull(description);
 
     /* if overwrite is false, error if the file exists. */
     File catalogFile = new File(filename);
     if (!overwrite && catalogFile.exists()) {
       throw new IOException(filename + " already exists");
     }
-    return Catalog.createFromFile(catalogFile, description);
+    return WorkerCatalog.createFromFile(catalogFile);
   }
 
   /**
    * 
-   * @param catalogFile a File object pointing to the SQLite database that will store the Catalog. If catalogFile is
-   *          null, this creates an in-memory SQLite database.
-   * @param description specifies a description for the configuration stored in this Catalog.
-   * @return a fresh Catalog fitting the specified description.
+   * @param catalogFile a File object pointing to the SQLite database that will store the WorkerCatalog. If catalogFile
+   *          is null, this creates an in-memory SQLite database.
+   * @return a fresh WorkerCatalog fitting the specified description.
    * @throws CatalogException if there is an error opening the database.
    * 
    *           TODO add some sanity checks to the filename?
    */
-  private static Catalog createFromFile(final File catalogFile, final String description) throws CatalogException {
-    Objects.requireNonNull(description);
-
+  private static WorkerCatalog createFromFile(final File catalogFile) throws CatalogException {
     /* Connect to the database. */
     final SQLiteConnection sqliteConnection = new SQLiteConnection(catalogFile);
     try {
       sqliteConnection.open();
     } catch (SQLiteException e) {
       LOGGER.error(e.toString());
-      throw new CatalogException("SQLiteException while creating the new Catalog", e);
+      throw new CatalogException("SQLiteException while creating the new WorkerCatalog", e);
     }
 
-    /* Create all the tables in the Catalog. */
+    /* Create all the tables in the WorkerCatalog. */
     try {
       /* @formatter:off */
       sqliteConnection.exec(
@@ -97,12 +90,12 @@ public final class Catalog {
         + "    key STRING UNIQUE NOT NULL,\n"
         + "    value STRING NOT NULL);");
       sqliteConnection.exec(
-          "CREATE TABLE workers (\n"
-        + "    worker_id INTEGER PRIMARY KEY ASC,\n"
-        + "    host_port STRING NOT NULL);");
-      sqliteConnection.exec(
           "CREATE TABLE masters (\n"
         + "    master_id INTEGER PRIMARY KEY ASC,\n"
+        + "    host_port STRING NOT NULL);");
+      sqliteConnection.exec(
+          "CREATE TABLE workers (\n"
+        + "    worker_id INTEGER PRIMARY KEY ASC,\n"
         + "    host_port STRING NOT NULL);");
       sqliteConnection.exec(
           "CREATE TABLE relations (\n"
@@ -115,68 +108,45 @@ public final class Catalog {
         + "    col_name STRING,\n"
         + "    col_type STRING NOT NULL);");
       sqliteConnection.exec(
-          "CREATE TABLE stored_relations (\n"
-        + "    stored_relation_id INTEGER PRIMARY KEY ASC,\n"
-        + "    relation_id INTEGER NOT NULL REFERENCES relation_metadata(relation_id),\n"
-        + "    num_shards INTEGER NOT NULL,\n"
-        + "    partitioned STRING NOT NULL);");
-      sqliteConnection.exec(
           "CREATE TABLE shards (\n"
         + "    stored_relation_id INTEGER NOT NULL REFERENCES stored_relations(stored_relation_id),\n"
         + "    shard_index INTEGER NOT NULL,\n"
-        + "    worker_id INTEGER NOT NULL REFERENCES workers(worker_id),\n"
         + "    location STRING NOT NULL);");
       /* @formatter:on*/
     } catch (SQLiteException e) {
       LOGGER.error(e.toString());
-      throw new CatalogException("SQLiteException while creating new Catalog tables", e);
-    }
-
-    /* Populate what tables we can. */
-    try {
-      SQLiteStatement statement =
-          sqliteConnection.prepare("INSERT INTO configuration (key, value) VALUES (?,?);", false);
-      statement.bind(1, "description");
-      statement.bind(2, description);
-      statement.step();
-      statement.dispose();
-    } catch (SQLiteException e) {
-      LOGGER.error(e.toString());
-      throw new CatalogException("SQLiteException while populating new Catalog tables", e);
+      throw new CatalogException("SQLiteException while creating new WorkerCatalog tables", e);
     }
 
     try {
-      return new Catalog(sqliteConnection);
+      return new WorkerCatalog(sqliteConnection);
     } catch (SQLiteException e) {
       LOGGER.error(e.toString());
-      throw new CatalogException("SQLiteException while creating Catalog from SQLiteConnection", e);
+      throw new CatalogException("SQLiteException while creating WorkerCatalog from SQLiteConnection", e);
     }
   }
 
   /**
-   * @param description specifies a description for the configuration stored in this Catalog.
-   * @return a fresh Catalog fitting the specified description.
+   * @return a fresh WorkerCatalog fitting the specified description.
    * @throws CatalogException if there is an error opening the database.
    * 
    *           TODO add some sanity checks to the filename?
    */
-  public static Catalog createInMemory(final String description) throws CatalogException {
-    Objects.requireNonNull(description);
-
-    return Catalog.createFromFile(null, description);
+  public static WorkerCatalog createInMemory() throws CatalogException {
+    return WorkerCatalog.createFromFile(null);
   }
 
   /**
-   * Opens the Myriad catalog stored as a SQLite database in the specified file.
+   * Opens the worker catalog stored as a SQLite database in the specified file.
    * 
    * @param filename the path to the SQLite database storing the catalog.
-   * @return an initialized Catalog object ready to be used for experiments.
+   * @return an initialized WorkerCatalog object ready to be used for experiments.
    * @throws FileNotFoundException if the given file does not exist.
    * @throws CatalogException if there is an error connecting to the database.
    * 
    *           TODO add some sanity checks to the filename?
    */
-  public static Catalog open(final String filename) throws FileNotFoundException, CatalogException {
+  public static WorkerCatalog open(final String filename) throws FileNotFoundException, CatalogException {
     Objects.requireNonNull(filename);
 
     /* See if the file exists, and create it if not. */
@@ -190,32 +160,26 @@ public final class Catalog {
     try {
       sqliteConnection.open(false);
 
-      return new Catalog(sqliteConnection);
+      return new WorkerCatalog(sqliteConnection);
     } catch (SQLiteException e) {
       LOGGER.error(e.toString());
       throw new CatalogException(e);
     }
   }
 
-  /**
-   * The description of the setup specified by this Catalog. For example, this could be "two node local test" or
-   * "20-node Greenplum cluster".
-   */
-  private String description = null;
-
-  /** Is the Catalog closed? */
+  /** Is the WorkerCatalog closed? */
   private boolean isClosed = true;
 
-  /** The connection to the SQLite database that stores the Catalog. */
+  /** The connection to the SQLite database that stores the WorkerCatalog. */
   private SQLiteConnection sqliteConnection;
 
   /**
    * Not publicly accessible.
    * 
-   * @param sqliteConnection connection to the SQLite database that stores the Catalog.
+   * @param sqliteConnection connection to the SQLite database that stores the WorkerCatalog.
    * @throws SQLiteException if there is an error turning on foreign keys.
    */
-  private Catalog(final SQLiteConnection sqliteConnection) throws SQLiteException {
+  private WorkerCatalog(final SQLiteConnection sqliteConnection) throws SQLiteException {
     Objects.requireNonNull(sqliteConnection);
     this.sqliteConnection = sqliteConnection;
     isClosed = false;
@@ -223,16 +187,16 @@ public final class Catalog {
   }
 
   /**
-   * Adds a master using the specified host and port to the Catalog.
+   * Adds a master using the specified host and port to the WorkerCatalog.
    * 
    * @param hostPortString specifies the path to the master in the format "host:port"
-   * @return this Catalog
+   * @return this WorkerCatalog
    * @throws CatalogException if the hostPortString is invalid or there is a database exception.
    */
-  public Catalog addMaster(final String hostPortString) throws CatalogException {
+  public WorkerCatalog addMaster(final String hostPortString) throws CatalogException {
     Objects.requireNonNull(hostPortString);
     if (isClosed) {
-      throw new CatalogException("Catalog is closed.");
+      throw new CatalogException("WorkerCatalog is closed.");
     }
     try {
       @SuppressWarnings("unused")
@@ -250,11 +214,11 @@ public final class Catalog {
   }
 
   /**
-   * Adds the metadata for a relation into the Catalog.
+   * Adds the metadata for a relation into the WorkerCatalog.
    * 
    * @param name the name of the relation.
    * @param schema the schema of the relation.
-   * @throws CatalogException if the relation is already in the catalog or there is an error in the database.
+   * @throws CatalogException if the relation is already in the WorkerCatalog or there is an error in the database.
    * 
    *           TODO if we use SQLite in a multithreaded way this will need to be revisited.
    * 
@@ -304,43 +268,13 @@ public final class Catalog {
   }
 
   /**
-   * Adds a worker using the specified host and port to the Catalog.
-   * 
-   * @param hostPortString specifies the path to the worker in the format "host:port"
-   * @return this Catalog
-   * @throws CatalogException if the hostPortString is invalid or there is a database exception.
-   */
-  public Catalog addWorker(final String hostPortString) throws CatalogException {
-    Objects.requireNonNull(hostPortString);
-    if (isClosed) {
-      throw new CatalogException("Catalog is closed.");
-    }
-    try {
-      @SuppressWarnings("unused")
-      /* Just used to verify that hostPortString is legal */
-      final SocketInfo sockInfo = SocketInfo.valueOf(hostPortString);
-      SQLiteStatement statement = sqliteConnection.prepare("INSERT INTO workers(host_port) VALUES(?);", false);
-      statement.bind(1, hostPortString);
-      statement.step();
-      statement.dispose();
-    } catch (SQLiteException e) {
-      LOGGER.error(e.toString());
-      throw new CatalogException(e);
-    }
-    return this;
-  }
-
-  /**
-   * Close the connection to the database that stores the Catalog. Idempotent. Calling any methods (other than close())
-   * on this Catalog will throw a CatalogException.
+   * Close the connection to the database that stores the WorkerCatalog. Idempotent. Calling any methods (other than
+   * close()) on this WorkerCatalog will throw a CatalogException.
    */
   public void close() {
     if (sqliteConnection != null) {
       sqliteConnection.dispose();
       sqliteConnection = null;
-    }
-    if (description != null) {
-      description = null;
     }
     isClosed = true;
   }
@@ -356,7 +290,7 @@ public final class Catalog {
   public String getConfigurationValue(final String key) throws CatalogException {
     Objects.requireNonNull(key);
     if (isClosed) {
-      throw new CatalogException("Catalog is closed.");
+      throw new CatalogException("WorkerCatalog is closed.");
     }
     try {
       /* Getting this out is a simple query, which does not need to be cached. */
@@ -376,29 +310,12 @@ public final class Catalog {
   }
 
   /**
-   * @return the description of this Catalog.
-   * @throws CatalogException if there is an error extracting the description from the database.
-   */
-  public String getDescription() throws CatalogException {
-    if (isClosed) {
-      throw new CatalogException("Catalog is closed.");
-    }
-    /* If we have the answer cached, use it. */
-    if (description != null) {
-      return description;
-    }
-
-    description = getConfigurationValue("description");
-    return description;
-  }
-
-  /**
-   * @return the set of masters stored in this Catalog.
+   * @return the set of masters stored in this WorkerCatalog.
    * @throws CatalogException if there is an error in the database.
    */
   public List<SocketInfo> getMasters() throws CatalogException {
     if (isClosed) {
-      throw new CatalogException("Catalog is closed.");
+      throw new CatalogException("WorkerCatalog is closed.");
     }
 
     ArrayList<SocketInfo> masters = new ArrayList<SocketInfo>();
@@ -417,7 +334,32 @@ public final class Catalog {
   }
 
   /**
-   * @return the set of workers stored in this Catalog.
+   * Extract the value of a particular configuration parameter from the database. Returns null if the parameter is not
+   * configured.
+   * 
+   * @param key the name of the configuration parameter.
+   * @param value the value of the configuration parameter.
+   * @throws CatalogException if there is an error in the backing database.
+   */
+  public void setConfigurationValue(final String key, final String value) throws CatalogException {
+    Objects.requireNonNull(key);
+    if (isClosed) {
+      throw new CatalogException("WorkerCatalog is closed.");
+    }
+    try {
+      /* Getting this out is a simple query, which does not need to be cached. */
+      SQLiteStatement statement =
+          sqliteConnection.prepare("INSERT INTO configuration VALUES(?,?);", false).bind(1, key).bind(2, value);
+      statement.step();
+      statement.dispose();
+    } catch (SQLiteException e) {
+      LOGGER.error(e.toString());
+      throw new CatalogException(e);
+    }
+  }
+
+  /**
+   * @return the set of workers stored in this WorkerCatalog.
    * @throws CatalogException if there is an error in the database.
    */
   public Map<Integer, SocketInfo> getWorkers() throws CatalogException {
@@ -440,4 +382,35 @@ public final class Catalog {
 
     return workers;
   }
+
+  /**
+   * Adds a worker using the specified host and port to the WorkerCatalog.
+   * 
+   * @param workerId specifies the global identifier of this worker.
+   * @param hostPortString specifies the path to the worker in the format "host:port".
+   * @return this WorkerCatalog.
+   * @throws CatalogException if the hostPortString is invalid or there is a database exception.
+   */
+  public WorkerCatalog addWorker(final int workerId, final String hostPortString) throws CatalogException {
+    Objects.requireNonNull(hostPortString);
+    if (isClosed) {
+      throw new CatalogException("WorkerCatalog is closed.");
+    }
+    try {
+      @SuppressWarnings("unused")
+      /* Just used to verify that hostPortString is legal */
+      final SocketInfo sockInfo = SocketInfo.valueOf(hostPortString);
+      SQLiteStatement statement =
+          sqliteConnection.prepare("INSERT INTO workers(worker_id, host_port) VALUES(?,?);", false);
+      statement.bind(1, workerId);
+      statement.bind(2, hostPortString);
+      statement.step();
+      statement.dispose();
+    } catch (SQLiteException e) {
+      LOGGER.error(e.toString());
+      throw new CatalogException(e);
+    }
+    return this;
+  }
+
 }
