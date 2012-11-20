1435746bbcbd4150e04d340df172dcb188308e37
Daniel Halperin <dhalperi@cs.washington.edu>
2012-11-19 17:44:34 -0800

    myriad: delete unused files
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- src/edu/washington/escience/myriad/table/DbAppendWriter.java
+++
@@ -1,7 +0,0 @@
-package edu.washington.escience.myriad.table;
-
-import edu.washington.escience.myriad.DbException;
-
-public interface DbAppendWriter extends DbTable {
-  void append(_TupleBatch tb) throws DbException;
-}
