31063a04092f7c3f2a3453767998a97d508af008
Daniel Halperin <dhalperi@cs.washington.edu>
2013-01-16 16:41:26 -0800

    util: add ThreadUtils
    Contains a function to print out the list of currently running threads.
    Good for debugging threading issues.
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

---
+++ src/edu/washington/escience/myriad/util/ThreadUtils.java
@@ -0,0 +1,21 @@
+package edu.washington.escience.myriad.util;
+
+import java.util.Set;
+
+public class ThreadUtils {
+
+  /**
+   * Print out the list of currently active threads.
+   * 
+   * @param tag a string printed before the message. For identifying calls to this function.
+   */
+  public static void printCurrentThreads(final String tag) {
+    Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+    System.out.println(tag + ":" + threadSet.size() + " threads currently active.");
+    System.out.print("\t");
+    for (Thread t : threadSet) {
+      System.out.print(t.getId() + ", ");
+    }
+    System.out.println();
+  }
+}
