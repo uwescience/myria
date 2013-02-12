d85512916e26768f2f27ebee21ec6959967e4551
Jingjing Wang <co2@co2-MacBook.(none)>
2013-02-11 20:29:30 -0800

    the same change to LocalJoin, putting tuples into hash table explicitly instead of references to TB

--- src/edu/washington/escience/myriad/operator/LocalJoin.java
+++ src/edu/washington/escience/myriad/operator/LocalJoin.java
@@ -4,91 +4,18 @@ import java.io.Externalizable;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 
 import edu.washington.escience.myriad.DbException;
 import edu.washington.escience.myriad.Schema;
 import edu.washington.escience.myriad.TupleBatch;
 import edu.washington.escience.myriad.TupleBatchBuffer;
-import edu.washington.escience.myriad.Type;
+import edu.washington.escience.myriad.column.Column;
+import edu.washington.escience.myriad.column.ColumnFactory;
 
 public final class LocalJoin extends Operator implements Externalizable {
-
-  private class IndexedTuple {
-    private final int index;
-    private final TupleBatch tb;
-
-    public IndexedTuple(final TupleBatch tb, final int index) {
-      this.tb = tb;
-      this.index = index;
-    }
-
-    public boolean compareField(final IndexedTuple another, final int colIndx1, final int colIndx2) {
-      final Type type1 = tb.getSchema().getFieldType(colIndx1);
-      // type check in query plan?
-      final int rowIndx1 = index;
-      final int rowIndx2 = another.index;
-      switch (type1) {
-        case INT_TYPE:
-          return tb.getInt(colIndx1, rowIndx1) == another.tb.getInt(colIndx2, rowIndx2);
-        case DOUBLE_TYPE:
-          return tb.getDouble(colIndx1, rowIndx1) == another.tb.getDouble(colIndx2, rowIndx2);
-        case STRING_TYPE:
-          return tb.getString(colIndx1, rowIndx1).equals(another.tb.getString(colIndx2, rowIndx2));
-        case FLOAT_TYPE:
-          return tb.getFloat(colIndx1, rowIndx1) == another.tb.getFloat(colIndx2, rowIndx2);
-        case BOOLEAN_TYPE:
-          return tb.getBoolean(colIndx1, rowIndx1) == another.tb.getBoolean(colIndx2, rowIndx2);
-        case LONG_TYPE:
-          return tb.getLong(colIndx1, rowIndx1) == another.tb.getLong(colIndx2, rowIndx2);
-      }
-      return false;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-      if (!(o instanceof IndexedTuple)) {
-        return false;
-      }
-      final IndexedTuple another = (IndexedTuple) o;
-      if (!(tb.getSchema().equals(another.tb.getSchema()))) {
-        return false;
-      }
-      for (int i = 0; i < tb.getSchema().numFields(); ++i) {
-        if (!compareField(another, i, i)) {
-          return false;
-        }
-      }
-      return true;
-    }
-
-    @Override
-    public int hashCode() {
-      return tb.hashCode(index);
-    }
-
-    public int hashCode4Keys(final int[] colIndx) {
-      return tb.hashCode(index, colIndx);
-    }
-
-    public boolean joinEquals(final Object o, final int[] compareIndx1, final int[] compareIndx2) {
-      if (!(o instanceof IndexedTuple)) {
-        return false;
-      }
-      if (compareIndx1.length != compareIndx2.length) {
-        return false;
-      }
-      final IndexedTuple another = (IndexedTuple) o;
-      for (int i = 0; i < compareIndx1.length; ++i) {
-        if (!compareField(another, compareIndx1[i], compareIndx2[i])) {
-          return false;
-        }
-      }
-      return true;
-    }
-  }
 
   /** Required for Java serialization. */
   private static final long serialVersionUID = 1L;
@@ -97,12 +24,14 @@ public final class LocalJoin extends Operator implements Externalizable {
   private Schema outputSchema;
   private int[] compareIndx1;
   private int[] compareIndx2;
-  private HashMap<Integer, List<IndexedTuple>> hashTable1;
-  private HashMap<Integer, List<IndexedTuple>> hashTable2;
+  private HashMap<Integer, List<Integer>> hashTable1Indices;
+  private HashMap<Integer, List<Integer>> hashTable2Indices;
+  private List<Column<?>> hashTable1;
+  private List<Column<?>> hashTable2;
   private TupleBatchBuffer ans;
 
   /**
-   * For Java serialization.
+   * For Java serialization. This constructor is called before readExternal()
    */
   public LocalJoin() {
   }
@@ -114,19 +43,17 @@ public final class LocalJoin extends Operator implements Externalizable {
     this.child2 = child2;
     this.compareIndx1 = compareIndx1;
     this.compareIndx2 = compareIndx2;
-    hashTable1 = new HashMap<Integer, List<IndexedTuple>>();
-    hashTable2 = new HashMap<Integer, List<IndexedTuple>>();
-    ans = new TupleBatchBuffer(outputSchema);
   }
 
-  protected void addToAns(final IndexedTuple tuple1, final IndexedTuple tuple2) {
-    final int num1 = tuple1.tb.getSchema().numFields();
-    final int num2 = tuple2.tb.getSchema().numFields();
-    for (int i = 0; i < num1; ++i) {
-      ans.put(i, tuple1.tb.getObject(i, tuple1.index));
+  protected void addToAns(final List<Object> cntTuple, final List<Column<?>> hashTable, final int index,
+      final boolean fromChild1) {
+    final int offset1 = (fromChild1 ? 0 : hashTable.size());
+    final int offset2 = (fromChild1 ? cntTuple.size() : 0);
+    for (int i = 0; i < cntTuple.size(); ++i) {
+      ans.put(i + offset1, cntTuple.get(i));
     }
-    for (int i = 0; i < num2; ++i) {
-      ans.put(i + num1, tuple2.tb.getObject(i, tuple2.index));
+    for (int i = 0; i < hashTable.size(); ++i) {
+      ans.put(i + offset2, hashTable.get(i).get(index));
     }
   }
 
@@ -138,16 +65,17 @@ public final class LocalJoin extends Operator implements Externalizable {
   protected TupleBatch fetchNext() throws DbException {
     TupleBatch nexttb = ans.popFilled();
     while (nexttb == null) {
-      boolean hasNewTuple = false; // might change to EOS instead of hasNext()
+      boolean hasNewTuple = false;
       TupleBatch tb = null;
       if ((tb = child1.next()) != null) {
         hasNewTuple = true;
-        processChildTB(tb, true);
+        processChildTB(tb, hashTable1, hashTable2, hashTable1Indices, hashTable2Indices, compareIndx1, compareIndx2,
+            true);
       }
-      // child2
       if ((tb = child2.next()) != null) {
         hasNewTuple = true;
-        processChildTB(tb, false);
+        processChildTB(tb, hashTable2, hashTable1, hashTable2Indices, hashTable1Indices, compareIndx2, compareIndx1,
+            false);
       }
       nexttb = ans.popFilled();
       if (!hasNewTuple) {
@@ -181,40 +109,47 @@ public final class LocalJoin extends Operator implements Externalizable {
   public void init() throws DbException {
   }
 
-  protected void processChildTB(final TupleBatch tb, final boolean tbFromChild1) {
-    List<IndexedTuple> tupleList = null;
-    int[] compareIndx2Add = compareIndx1;
-    int[] compareIndx2Join = compareIndx2;
-    HashMap<Integer, List<IndexedTuple>> hashTable2Add = hashTable1;
-    HashMap<Integer, List<IndexedTuple>> hashTable2Join = hashTable2;
-    if (!tbFromChild1) {
-      compareIndx2Add = compareIndx2;
-      compareIndx2Join = compareIndx1;
-      hashTable2Add = hashTable2;
-      hashTable2Join = hashTable1;
+  private boolean compareTuple(final List<Object> cntTuple, final List<Column<?>> hashTable, final int index,
+      final int[] compareIndx1, final int[] compareIndx2) {
+    if (compareIndx1.length != compareIndx2.length) {
+      return false;
     }
+    for (int i = 0; i < compareIndx1.length; ++i) {
+      if (!cntTuple.get(compareIndx1[i]).equals(hashTable.get(compareIndx2[i]).get(index))) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-    for (int i = 0; i < tb.numTuples(); ++i) { // outputTuples?
-      final IndexedTuple tuple = new IndexedTuple(tb, i);
-      final int cntHashCode = tuple.hashCode4Keys(compareIndx2Add);
-      tupleList = hashTable2Join.get(cntHashCode);
-      if (tupleList != null) {
-        for (final IndexedTuple tuple2 : tupleList) {
-          if (tuple.joinEquals(tuple2, compareIndx2Add, compareIndx2Join)) {
-            if (tbFromChild1) {
-              addToAns(tuple, tuple2);
-            } else {
-              addToAns(tuple2, tuple);
-            }
+  protected void processChildTB(final TupleBatch tb, final List<Column<?>> hashTable1,
+      final List<Column<?>> hashTable2, final HashMap<Integer, List<Integer>> hashTable1Indices,
+      final HashMap<Integer, List<Integer>> hashTable2Indices, final int[] compareIndx1, final int[] compareIndx2,
+      final boolean fromChild1) {
+
+    for (int i = 0; i < tb.numTuples(); ++i) {
+      final List<Object> cntTuple = new ArrayList<Object>();
+      for (int j = 0; j < tb.numColumns(); ++j) {
+        cntTuple.add(tb.getObject(j, i));
+      }
+      final int nextIndex = hashTable1.get(0).size();
+      final int cntHashCode = tb.hashCode(i, compareIndx1);
+      List<Integer> indexList = hashTable2Indices.get(cntHashCode);
+      if (indexList != null) {
+        for (final int index : indexList) {
+          if (compareTuple(cntTuple, hashTable2, index, compareIndx1, compareIndx2)) {
+            addToAns(cntTuple, hashTable2, index, fromChild1);
           }
         }
       }
-      tupleList = hashTable2Add.get(cntHashCode);
-      if (tupleList == null) {
-        tupleList = new LinkedList<IndexedTuple>();
-        hashTable2Add.put(cntHashCode, tupleList);
+      if (hashTable1Indices.get(cntHashCode) == null) {
+        hashTable1Indices.put(cntHashCode, new ArrayList<Integer>());
       }
-      tupleList.add(tuple);
+      hashTable1Indices.get(cntHashCode).add(nextIndex);
+      for (int j = 0; j < tb.numColumns(); ++j) {
+        hashTable1.get(j).putObject(cntTuple.get(j));
+      }
+
     }
   }
 
@@ -225,9 +160,13 @@ public final class LocalJoin extends Operator implements Externalizable {
     compareIndx1 = (int[]) in.readObject();
     compareIndx2 = (int[]) in.readObject();
     outputSchema = (Schema) in.readObject();
-    hashTable1 = new HashMap<Integer, List<IndexedTuple>>();
-    hashTable2 = new HashMap<Integer, List<IndexedTuple>>();
-    ans = new TupleBatchBuffer(outputSchema);
+    hashTable1 = new ArrayList<Column<?>>();
+    hashTable2 = new ArrayList<Column<?>>();
+    hashTable1Indices = new HashMap<Integer, List<Integer>>();
+    hashTable2Indices = new HashMap<Integer, List<Integer>>();
+    ans = new TupleBatchBuffer(getSchema());
+    hashTable1 = ColumnFactory.allocateColumns(child1.getSchema());
+    hashTable2 = ColumnFactory.allocateColumns(child2.getSchema());
   }
 
   @Override
