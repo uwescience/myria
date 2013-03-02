a25d294ee38ee327806abdc525ceda86045e140d
Vaspol Ruamviboonsuk <vaspol@cs.washington.edu>
2013-03-01 17:56:59 -0800

    added the IFunction interface. The interface for all of the functions to be used in Apply

---
+++ src/edu/washington/escience/myriad/operator/apply/IFunction.java
@@ -0,0 +1,30 @@
+package edu.washington.escience.myriad.operator.apply;
+
+import edu.washington.escience.myriad.Type;
+
+/**
+ * An interface for Math operations to be used with Apply
+ */
+
+public interface IFunction<Tout, Tin> {
+
+  /**
+   * Determines what should the Type of the resulting field be
+   * 
+   * @param srcField
+   *          the type of the source field
+   * @return the desired type of the resulting field
+   */
+  Type getResultType(Type srcField);
+
+  /**
+   * Executes the function on the value, int src
+   * 
+   * @param src
+   *          the buffer to store the result
+   * @return the value after the function is applied on src
+   * 
+   */
+  Tout execute(Tin src);
+
+}
