cc23f14a11aebf9eef4ceb00616c7d606be80642
slxu <slxu@cs.washington.edu>
2013-03-15 17:01:08 -0700

    a Netty ChannelSink for in-jvm channels. The messages written to this sink will get directly sent back for processing.

---
+++ src/edu/washington/escience/myriad/parallel/ipc/InJVMLoopbackChannelSink.java
@@ -0,0 +1,90 @@
+package edu.washington.escience.myriad.parallel.ipc;
+
+import org.jboss.netty.channel.AbstractChannelSink;
+import org.jboss.netty.channel.ChannelEvent;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelState;
+import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.local.LocalAddress;
+import org.slf4j.LoggerFactory;
+
+/**
+ * ChannelSink implementation for InJVM channels. <br>
+ * All the messages reaching the sink will automatically be pushed into the associated @{link MessageChannelHandler}.
+ * */
+public class InJVMLoopbackChannelSink<M> extends AbstractChannelSink {
+
+  /** The logger for this class. */
+  protected static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(InJVMLoopbackChannelSink.class.getName());
+
+  private final MessageChannelHandler<M> messageProcessor;
+  private final int myIPCID;
+  private final LocalAddress pseudoServerAddress = new LocalAddress(LocalAddress.EPHEMERAL);
+
+  public InJVMLoopbackChannelSink(final MessageChannelHandler<M> processor, final int myIPCID) {
+    this.messageProcessor = processor;
+    this.myIPCID = myIPCID;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public void eventSunk(final ChannelPipeline pipeline, final ChannelEvent e) throws Exception {
+    InJVMChannel channel = (InJVMChannel) e.getChannel();
+    ChannelFuture future = e.getFuture();
+
+    try {
+      if (e instanceof ChannelStateEvent) {
+        ChannelStateEvent event = (ChannelStateEvent) e;
+        ChannelState state = event.getState();
+        Object value = event.getValue();
+
+        switch (state) {
+          case OPEN:
+            if (Boolean.FALSE.equals(value)) {
+              channel.closeNow(future);
+            } else {
+              future.setSuccess();
+            }
+            break;
+          case BOUND:
+            if (value != null) {
+              Channels.fireChannelBound(channel, new LocalAddress(LocalAddress.EPHEMERAL));
+              future.setSuccess();
+            } else {
+              channel.closeNow(future);
+            }
+            break;
+          case CONNECTED:
+            if (value != null) {
+              Channels.fireChannelConnected(channel, this.pseudoServerAddress);
+              future.setSuccess();
+            } else {
+              channel.closeNow(future);
+            }
+            break;
+          case INTEREST_OPS:
+            Channels.fireChannelInterestChanged(channel);
+            future.setSuccess();
+            break;
+        }
+      } else if (e instanceof MessageEvent) {
+        // this.messageProcessor.processMessage(channel, myIPCID, (M) ((MessageEvent) e).getMessage());
+        Channels.fireMessageReceived(channel, ((MessageEvent) e).getMessage());
+        future.setSuccess();
+        Channels.fireWriteComplete(channel, 1);
+      } else {
+        if (LOGGER.isErrorEnabled()) {
+          LOGGER.error("Unexpected channel event catched at sink. {}", e);
+        }
+      }
+    } catch (Throwable t) {
+      future.setFailure(t);
+    }
+    if (!future.isDone()) {
+      future.setFailure(null);
+    }
+  }
+}
