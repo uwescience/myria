a22e5cdfd8aeb9544f4794e476ce20a7d8791673
slxu <slxu@cs.washington.edu>
2013-03-15 19:27:52 -0700

    hash partition using whole tuple hash code

---
+++ src/edu/washington/escience/myriad/parallel/WholeTupleHashPartitionFunction.java
@@ -0,0 +1,38 @@
+package edu.washington.escience.myriad.parallel;
+
+import edu.washington.escience.myriad.TupleBatch;
+
+/**
+ * Partition of tuples by the hash code of the whole tuple.
+ */
+public final class WholeTupleHashPartitionFunction extends PartitionFunction<String, Integer> {
+
+  /** Required for Java serialization. */
+  private static final long serialVersionUID = 1L;
+
+  public WholeTupleHashPartitionFunction(final int numPartition) {
+    super(numPartition);
+  }
+
+  /**
+   * @param tb data.
+   * @return partitions.
+   * */
+  @Override
+  public int[] partition(final TupleBatch tb) {
+    final int[] result = new int[tb.numTuples()];
+    for (int i = 0; i < result.length; i++) {
+      int p = tb.hashCode(i) % numPartition;
+      if (p < 0) {
+        p = p + numPartition;
+      }
+      result[i] = p;
+    }
+    return result;
+  }
+
+  @Override
+  public void setAttribute(final String attribute, final Integer value) {
+  }
+
+}
