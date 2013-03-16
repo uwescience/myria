700dbf939792b8343e4825c3ed6195e40e6e91a2
slxu <slxu@cs.washington.edu>
2013-03-15 19:08:24 -0700

    ExchangeMessage for TransportMessage

--- src/edu/washington/escience/myriad/parallel/ExchangeData.java
+++ src/edu/washington/escience/myriad/parallel/ExchangeData.java
@@ -6,40 +6,48 @@ import java.util.Objects;
 import edu.washington.escience.myriad.Schema;
 import edu.washington.escience.myriad.TupleBatch;
 import edu.washington.escience.myriad.column.Column;
-import edu.washington.escience.myriad.parallel.Exchange.ExchangePairID;
 
-public final class ExchangeData {
+public final class ExchangeData implements ExchangeMessage<TupleBatch> {
 
   private final ExchangePairID operatorID;
   private final int fromWorkerID;
   private final TupleBatch dataHolder;
+  public final long seqNum;
 
   private boolean eos;
   private boolean eoi;
 
+  public static enum MetaMessage {
+    EOS, EOI;
+  }
+
   /**
    * @param oID the operator to which this TB should be feed
    * @param workerID the source worker where the TB is generated
-   * @param
+   * @param columns columns.
+   * @param inputSchema inputSchema.
+   * @param numTuples numTuples.
    */
   public ExchangeData(final ExchangePairID oID, final int workerID, final List<Column<?>> columns,
-      final Schema inputSchema, final int numTuples) {
-    dataHolder = new TupleBatch(inputSchema, columns, numTuples);
+      final Schema inputSchema, final int numTuples, final long seqNum) {
+    Objects.requireNonNull(columns);
+    this.seqNum = seqNum;
+    dataHolder = new TupleBatch(inputSchema, columns, numTuples, -1, -1);
     operatorID = oID;
     fromWorkerID = workerID;
-    Objects.requireNonNull(columns);
   }
 
   /**
    * @param oID the operator to which this TB should be feed
    * @param workerID the source worker where the TB is generated
    */
-  public ExchangeData(final ExchangePairID oID, final int workerID, final Schema schema, final int msg) {
+  public ExchangeData(final ExchangePairID oID, final int workerID, final Schema schema, final MetaMessage msg) {
     dataHolder = null;
     operatorID = oID;
     fromWorkerID = workerID;
-    eos = (msg == 0);
-    eoi = (msg == 1);
+    seqNum = -1;
+    eos = (msg == MetaMessage.EOS);
+    eoi = (msg == MetaMessage.EOI);
   }
 
   /**
@@ -47,17 +55,6 @@ public final class ExchangeData {
    */
   public ExchangePairID getOperatorID() {
     return operatorID;
-  }
-
-  public TupleBatch getRealData() {
-    return dataHolder;
-  }
-
-  /**
-   * @return worker id from which the message was sent.
-   */
-  public int getWorkerID() {
-    return fromWorkerID;
   }
 
   public boolean isEos() {
@@ -70,6 +67,25 @@ public final class ExchangeData {
 
   @Override
   public String toString() {
+    if (eos) {
+      return "ExchangeData From Worker:" + fromWorkerID + "; to Operator: " + operatorID + ": EOS.";
+    }
+    if (eoi) {
+      return "ExchangeData From Worker:" + fromWorkerID + "; to Operator: " + operatorID + ": EOI.";
+    }
     return "ExchangeData From Worker:" + fromWorkerID + "; to Operator: " + operatorID + ";\nContents:\n" + dataHolder;
+  }
+
+  /**
+   * @return worker id from which the message was sent.
+   */
+  @Override
+  public int getSourceIPCID() {
+    return fromWorkerID;
+  }
+
+  @Override
+  public TupleBatch getData() {
+    return dataHolder;
   }
 }
