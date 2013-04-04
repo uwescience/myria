08319a641da1aef3b458d9e9e2c6a4e30b0eec7d
slxu <slxu@cs.washington.edu>
2013-04-04 15:19:16 -0700

    serialize port usage

--- test/edu/washington/escience/myriad/daemon/MasterDaemonTest.java
+++ test/edu/washington/escience/myriad/daemon/MasterDaemonTest.java
@@ -3,11 +3,13 @@ package edu.washington.escience.myriad.daemon;
 import java.io.File;
 import java.util.Set;
 
+import org.apache.mina.util.AvailablePortFinder;
 import org.junit.Test;
 import org.restlet.resource.ClientResource;
 
 import com.google.common.io.Files;
 
+import edu.washington.escience.myriad.api.MasterApiServer;
 import edu.washington.escience.myriad.coordinator.catalog.CatalogMaker;
 import edu.washington.escience.myriad.util.FSUtils;
 import edu.washington.escience.myriad.util.ThreadUtils;
@@ -20,10 +22,20 @@ public class MasterDaemonTest {
   public void testStartAndShutdown() throws Exception {
     File tmpFolder = Files.createTempDir();
     try {
+      final int baseMasterPort = 8001;
+      final int baseWorkerPort = 9001;
+      final int n = 0;
+      final String[] args = new String[n + 3];
+      args[0] = tmpFolder.getAbsolutePath();
+      args[1] = Integer.toString(n);
+      args[2] = "localhost:" + baseMasterPort;
+      for (int i = 0; i < n; ++i) {
+        args[i + 3] = "localhost:" + (baseWorkerPort + i);
+      }
+      CatalogMaker.makeNNodesParallelCatalog(args);
+
       /* Remember which threads were there when the test starts. */
       Set<Thread> startThreads = ThreadUtils.getCurrentThreads();
-
-      CatalogMaker.makeNNodesLocalParallelCatalog(tmpFolder.getAbsolutePath(), 2);
 
       /* Start the master. */
       MasterDaemon md = new MasterDaemon(new String[] { tmpFolder.getAbsolutePath() });
@@ -41,6 +53,9 @@ public class MasterDaemonTest {
       }
     } finally {
       FSUtils.deleteFileFolder(tmpFolder);
+      while (!AvailablePortFinder.available(8001) || !AvailablePortFinder.available(MasterApiServer.PORT)) {
+        Thread.sleep(100);
+      }
     }
   }
 
@@ -48,19 +63,28 @@ public class MasterDaemonTest {
   public void testStartAndRestShutdown() throws Exception {
 
     File tmpFolder = Files.createTempDir();
-
     try {
+      final int baseMasterPort = 8001;
+      final int baseWorkerPort = 9001;
+      final int n = 0;
+      final String[] args = new String[n + 3];
+      args[0] = tmpFolder.getAbsolutePath();
+      args[1] = Integer.toString(n);
+      args[2] = "localhost:" + baseMasterPort;
+      for (int i = 0; i < n; ++i) {
+        args[i + 3] = "localhost:" + (baseWorkerPort + i);
+      }
+      CatalogMaker.makeNNodesParallelCatalog(args);
+
       /* Remember which threads were there when the test starts. */
       Set<Thread> startThreads = ThreadUtils.getCurrentThreads();
-
-      CatalogMaker.makeNNodesLocalParallelCatalog(tmpFolder.getAbsolutePath(), 2);
 
       /* Start the master. */
       MasterDaemon md = new MasterDaemon(new String[] { tmpFolder.getAbsolutePath() });
       md.start();
 
       /* Stop the master. */
-      ClientResource shutdownRest = new ClientResource("http://localhost:8753/server/shutdown");
+      ClientResource shutdownRest = new ClientResource("http://localhost:" + MasterApiServer.PORT + "/server/shutdown");
       shutdownRest.get();
       shutdownRest.release();
 
@@ -73,6 +97,9 @@ public class MasterDaemonTest {
       }
     } finally {
       FSUtils.deleteFileFolder(tmpFolder);
+      while (!AvailablePortFinder.available(8001) || !AvailablePortFinder.available(MasterApiServer.PORT)) {
+        Thread.sleep(100);
+      }
     }
   }
 
