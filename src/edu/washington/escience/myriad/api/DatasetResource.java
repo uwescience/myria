uwescience/myria

commit f82ebc04d268e21880ccf6e7b0601987d79c17f5

API: switch from static class variable to Restlet Context
The "right" way to present variables that are globally accessible to
individual Restlets is via the Context given to each Restlet rather than
in static instance variables.

- Store the Server and MasterDaemon in MyriaApiServer
- Change resources to access them from Context instead of through
  statics
- Add some utility functions to make these cleaner
- Add constants so we're not using "magic strings" all over the place

Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

src/edu/washington/escience/myriad/api/DatasetResource.java
@@ -43,7 +43,7 @@ public final class DatasetResource {
   public Schema getDataset(@PathParam("userName") final String userName,
       @PathParam("programName") final String programName, @PathParam("relationName") final String relationName) {
     try {
-      Schema schema = MasterApiServer.getMyriaServer().getSchema(RelationKey.of(userName, programName, relationName));
+      Schema schema = MyriaApiUtils.getServer().getSchema(RelationKey.of(userName, programName, relationName));
       if (schema == null) {
         /* Not found, throw a 404 (Not Found) */
         throw new WebApplicationException(Response.status(Status.NOT_FOUND).build());
@@ -80,7 +80,7 @@ public final class DatasetResource {
 
     /* If we already have a dataset by this name, tell the user there's a conflict. */
     try {
-      if (MasterApiServer.getMyriaServer().getSchema(dataset.relationKey) != null) {
+      if (MyriaApiUtils.getServer().getSchema(dataset.relationKey) != null) {
         /* Found, throw a 409 (Conflict) */
         throw new WebApplicationException(Response.status(Status.CONFLICT).build());
       }
@@ -90,7 +90,7 @@ public final class DatasetResource {
     }
 
     /* If we don't have any workers alive right now, tell the user we're busy. */
-    Set<Integer> workers = MasterApiServer.getMyriaServer().getAliveWorkers();
+    Set<Integer> workers = MyriaApiUtils.getServer().getAliveWorkers();
     if (workers.size() == 0) {
       /* Throw a 503 (Service Unavailable) */
       throw new WebApplicationException(Response.status(Status.SERVICE_UNAVAILABLE).build());
@@ -98,7 +98,7 @@ public final class DatasetResource {
 
     /* Do the work. */
     try {
-      MasterApiServer.getMyriaServer().ingestDataset(dataset.relationKey, dataset.schema, dataset.data);
+      MyriaApiUtils.getServer().ingestDataset(dataset.relationKey, dataset.schema, dataset.data);
     } catch (CatalogException e) {
       throw new WebApplicationException(Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build());
     }
@@ -115,7 +115,7 @@ public final class DatasetResource {
    * A JSON-able wrapper for the expected wire message for a new dataset.
    * 
    */
-  public static class DatasetEncoding {
+  class DatasetEncoding {
     /** The name of the dataset. */
     @JsonProperty("relation_key")
     public RelationKey relationKey;

src/edu/washington/escience/myriad/api/MasterApiServer.java
@@ -1,9 +1,11 @@
 package edu.washington.escience.myriad.api;
 
 import org.restlet.Component;
+import org.restlet.Context;
 import org.restlet.data.Protocol;
 import org.restlet.ext.jaxrs.JaxRsApplication;
 
+import edu.washington.escience.myriad.daemon.MasterDaemon;
 import edu.washington.escience.myriad.parallel.Server;
 
 /**
@@ -16,45 +18,9 @@ public final class MasterApiServer {
 
   /* max time for waiting a server query to complete before throwing a timeout exception */
   private static final String MAX_WAITING_TIME = "86400000";
-  /** The instance. */
-  public static final MasterApiServer INSTANCE = new MasterApiServer();
 
   /** The default port for the server. */
   private static final int PORT = 8753;
-
-  /** The Server for this application. */
-  private static Server myriaServer;
-
-  /**
-   * Used by the API collection classes to access data about system internals.
-   * 
-   * @return the Myriad Server object that handles/knows the distributed system state.
-   */
-  protected static Server getMyriaServer() {
-    return myriaServer;
-  }
-
-  /**
-   * Private method to configure the API Server's pointer to the Myriad Server.
-   * 
-   * @param myriaServer a handle the the Myriad server currently running.
-   */
-  private static void setMyriaServer(final Server myriaServer) {
-    MasterApiServer.myriaServer = myriaServer;
-  }
-
-  /**
-   * Set the local handle to the Myriad Server so that we can serve API calls requesting information.
-   * 
-   * @param myriaServer a handle the the Myriad server currently running.
-   * @throws IllegalStateException if this pointer has already been initialized.
-   */
-  public static void setUp(final Server myriaServer) throws IllegalStateException {
-    if (MasterApiServer.getMyriaServer() != null) {
-      throw new IllegalStateException("MasterServer.catalog is already initialized");
-    }
-    MasterApiServer.setMyriaServer(myriaServer);
-  }
 
   /** The RESTlet Component is the main class that holds multiple servers/hosts for this application. */
   private final Component component;
@@ -64,8 +30,11 @@ public final class MasterApiServer {
 
   /**
    * Constructor for the Master API Server.
+   * 
+   * @param server the Myria server that will handle API requests.
+   * @param daemon the Myria master daemon.
    */
-  private MasterApiServer() {
+  public MasterApiServer(final Server server, final MasterDaemon daemon) {
     System.setProperty("org.restlet.engine.io.timeoutMs", MAX_WAITING_TIME);
     /* create Component (as ever for Restlet) */
     component = new Component();
@@ -73,8 +42,13 @@ public final class MasterApiServer {
     /* Add a server that responds to HTTP on port PORT. */
     restletServer = component.getServers().add(Protocol.HTTP, PORT);
 
+    /* Setup the context variables that will be available to all Restlets. */
+    Context context = component.getContext().createChildContext();
+    context.getAttributes().put(MyriaApiConstants.MYRIA_SERVER_ATTRIBUTE, server);
+    context.getAttributes().put(MyriaApiConstants.MYRIA_MASTER_DAEMON_ATTRIBUTE, daemon);
+
     /* Add a JAX-RS runtime environment, using our MasterApplication implementation. */
-    final JaxRsApplication application = new JaxRsApplication();
+    final JaxRsApplication application = new JaxRsApplication(context);
     application.add(new MasterApplication());
 
     /* Attach the application to the component */

src/edu/washington/escience/myriad/api/MasterResource.java
@@ -4,6 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import edu.washington.escience.myriad.daemon.MasterDaemon;
+
 /**
  * This is the class that handles API calls that return workers.
  * 
@@ -11,6 +13,9 @@ import javax.ws.rs.core.Response;
  */
 @Path("/server")
 public final class MasterResource {
+  /** How long the thread should sleep before shutting down the daemon. This is a HACK! TODO */
+  private static final int SLEEP_BEFORE_SHUTDOWN_MS = 100;
+
   /**
    * Shutdown the server.
    * 
@@ -19,18 +24,24 @@ public final class MasterResource {
   @GET
   @Path("/shutdown")
   public Response shutdown() {
-    MasterApiServer.getMyriaServer().shutdown();
-    Thread shutdownThread = new Thread() {
+    /* Get the daemon and save it to be used in the stopping. */
+    final MasterDaemon daemon =
+        (MasterDaemon) MyriaApiUtils.getContextAttributes().get(MyriaApiConstants.MYRIA_MASTER_DAEMON_ATTRIBUTE);
+
+    /* A thread to stop the daemon after this request finishes. */
+    Thread shutdownThread = new Thread("MasterResource-Shutdown") {
       @Override
       public void run() {
         try {
-          MasterApiServer.INSTANCE.stop();
+          Thread.sleep(SLEEP_BEFORE_SHUTDOWN_MS);
+          daemon.stop();
         } catch (Exception e) {
           e.printStackTrace();
         }
       }
     };
-    shutdownThread.setName("MasterResource-Shutdown");
+
+    /* Start the thread, then return an empty success response. */
     shutdownThread.start();
     return Response.noContent().build();
   }

src/edu/washington/escience/myriad/api/MyriaApiConstants.java
@@ -1,0 +1,20 @@
+package edu.washington.escience.myriad.api;
+
+/**
+ * This class holds constants used in the Myria API server.
+ * 
+ * @author dhalperi
+ * 
+ */
+public final class MyriaApiConstants {
+  /** This class just holds constants. */
+  private MyriaApiConstants() {
+  }
+
+  /** The string identifying the Myria Server in the Restlet Context variable. */
+  public static final String MYRIA_SERVER_ATTRIBUTE = "myria.server";
+  /** The string identifying the Myria MasterDaemon in the Restlet Context variable. */
+  public static final String MYRIA_MASTER_DAEMON_ATTRIBUTE = "myria.master_daemon";
+  /** The string identifying the Myria ApiServer in the Restlet Context variable. */
+  public static final String MYRIA_API_SERVER_ATTRIBUTE = "myria.master_api_server";
+}

src/edu/washington/escience/myriad/api/MyriaApiUtils.java
@@ -1,0 +1,34 @@
+package edu.washington.escience.myriad.api;
+
+import java.util.Map;
+
+import org.restlet.Context;
+
+import edu.washington.escience.myriad.parallel.Server;
+
+/**
+ * Helper functions to simplify Restlet representation-writing code.
+ * 
+ * @author dhalperi
+ * 
+ */
+public final class MyriaApiUtils {
+  /** Utility class, do not construct. */
+  private MyriaApiUtils() {
+  }
+
+  /**
+   * @return the set of attributes in the Restlet Context.
+   */
+  public static Map<String, Object> getContextAttributes() {
+    return Context.getCurrent().getAttributes();
+  }
+
+  /**
+   * @return the Myria Server in the Restlet Context.
+   */
+  public static Server getServer() {
+    return (Server) getContextAttributes().get(MyriaApiConstants.MYRIA_SERVER_ATTRIBUTE);
+  }
+
+}

src/edu/washington/escience/myriad/api/QueryResource.java
@@ -90,14 +90,14 @@ public final class QueryResource {
       /* Remove the server plan if present */
       usingWorkers.remove(0);
       /* Make sure that the requested workers are alive. */
-      if (!MasterApiServer.getMyriaServer().getAliveWorkers().containsAll(usingWorkers)) {
+      if (!MyriaApiUtils.getServer().getAliveWorkers().containsAll(usingWorkers)) {
         /* Throw a 503 (Service Unavailable) */
         throw new WebApplicationException(Response.status(Status.SERVICE_UNAVAILABLE).build());
       }
 
       /* Start the query, and get its Server-assigned Query ID */
       final Long queryId =
-          MasterApiServer.getMyriaServer().startQuery(rawQuery, logicalRa, queryPlan, expectedResultSize);
+          MyriaApiUtils.getServer().startQuery(rawQuery, logicalRa, queryPlan, expectedResultSize);
       /* In the response, tell the client what ID this query was assigned. */
       UriBuilder queryUri = uriInfo.getAbsolutePathBuilder();
       return Response.created(queryUri.path("query-" + queryId.toString()).build()).build();
@@ -234,7 +234,7 @@ public final class QueryResource {
         RelationKey relationKey = RelationKey.of(userName, programName, relationName);
         Schema schema;
         try {
-          schema = MasterApiServer.getMyriaServer().getSchema(relationKey);
+          schema = MyriaApiUtils.getServer().getSchema(relationKey);
         } catch (final CatalogException e) {
           /* Throw a 500 (Internal Server Error) */
           throw new WebApplicationException(Response.status(Status.INTERNAL_SERVER_ERROR).build());

src/edu/washington/escience/myriad/api/WorkerCollection.java
@@ -30,7 +30,7 @@ public final class WorkerCollection {
   @Path("/alive")
   @Produces(MediaType.APPLICATION_JSON)
   public Set<Integer> getAliveWorkers() {
-    return MasterApiServer.getMyriaServer().getAliveWorkers();
+    return MyriaApiUtils.getServer().getAliveWorkers();
   }
 
   /**
@@ -43,7 +43,7 @@ public final class WorkerCollection {
   public String getWorker(@PathParam("workerId") final String workerId) {
     SocketInfo workerInfo;
     try {
-      workerInfo = MasterApiServer.getMyriaServer().getWorkers().get(Integer.parseInt(workerId));
+      workerInfo = MyriaApiUtils.getServer().getWorkers().get(Integer.parseInt(workerId));
     } catch (final NumberFormatException e) {
       /* Parsing failed, throw a 400 (Bad Request) */
       throw new WebApplicationException(Response.status(Status.BAD_REQUEST).build());
@@ -63,7 +63,7 @@ public final class WorkerCollection {
   @Produces(MediaType.APPLICATION_JSON)
   public Map<Integer, String> getWorkers() {
     Map<Integer, SocketInfo> workers;
-    workers = MasterApiServer.getMyriaServer().getWorkers();
+    workers = MyriaApiUtils.getServer().getWorkers();
     final Map<Integer, String> ret = new HashMap<Integer, String>();
     for (final Entry<Integer, SocketInfo> workerInfo : workers.entrySet()) {
       ret.put(workerInfo.getKey(), workerInfo.getValue().toString());

src/edu/washington/escience/myriad/daemon/MasterDaemon.java
@@ -29,13 +29,12 @@ public final class MasterDaemon {
   public static void main(final String[] args) throws Exception {
     final MasterDaemon md = new MasterDaemon(args);
     md.start();
-    // System.out.println("Press enter to stop");
-    // System.in.read();
-    // md.stop();
   }
 
-  /** The Myriad server. */
+  /** The Myria server. */
   private final Server server;
+  /** The Master API server. */
+  private final MasterApiServer apiServer;
 
   /**
    * Instantiates a MasterDaemon object. Includes the API server and the Myriad server.
@@ -46,7 +45,7 @@ public final class MasterDaemon {
   public MasterDaemon(final String[] args) throws Exception {
     processArguments(args);
     server = new Server(FilenameUtils.concat(args[0], "master.catalog"));
-    MasterApiServer.setUp(server);
+    apiServer = new MasterApiServer(server, this);
   }
 
   /**
@@ -68,7 +67,7 @@ public final class MasterDaemon {
    * @throws Exception if there is an issue starting either server.
    */
   public void start() throws Exception {
-    MasterApiServer.INSTANCE.start();
+    apiServer.start();
     server.start();
   }
 
@@ -78,7 +77,7 @@ public final class MasterDaemon {
    * @throws Exception if there is an issue stopping either server.
    */
   public void stop() throws Exception {
-    MasterApiServer.INSTANCE.stop();
+    apiServer.stop();
     server.shutdown();
   }
 

commit 132fd68ca8561c0e51f964f793d349ba6b0448ba

api: fix bug introduced in f82ebc04d268e21880ccf6e7b0601987d79c17f5
Apparently making this class not public static breaks deserialization.

Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

src/edu/washington/escience/myriad/api/DatasetResource.java
@@ -115,7 +115,7 @@ public final class DatasetResource {
    * A JSON-able wrapper for the expected wire message for a new dataset.
    * 
    */
-  class DatasetEncoding {
+  public static class DatasetEncoding {
     /** The name of the dataset. */
     @JsonProperty("relation_key")
     public RelationKey relationKey;