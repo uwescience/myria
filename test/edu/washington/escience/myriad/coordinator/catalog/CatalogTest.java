53bf84a9a92909e07801a226d9fdd2419c4d71b1
Daniel Halperin <dhalperi@cs.washington.edu>
2013-04-18 10:16:31 -0700

    CatalogTest: cleanup and add a CatalogMakerTest
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- test/edu/washington/escience/myriad/coordinator/catalog/CatalogTest.java
+++ test/edu/washington/escience/myriad/coordinator/catalog/CatalogTest.java
@@ -1,8 +1,10 @@
 package edu.washington.escience.myriad.coordinator.catalog;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +18,7 @@ import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 import org.slf4j.LoggerFactory;
 
+import edu.washington.escience.myriad.MyriaConstants;
 import edu.washington.escience.myriad.parallel.SocketInfo;
 
 public class CatalogTest {
@@ -34,9 +37,11 @@ public class CatalogTest {
 
   /**
    * TODO All this test does is create an in-memory Catalog and make sure its description matches. Flesh it out.
+   * 
+   * @throws CatalogException if there is an error creating the Catalog.
    */
   @Test
-  public void testCatalogCreation() {
+  public void testCatalogCreation() throws CatalogException {
     /* Constants needed for this test. */
     final String DESCRIPTION = "local-worker-test";
     final String SERVER = "localhost:8001";
@@ -48,40 +53,43 @@ public class CatalogTest {
     Catalog catalog = null;
 
     /* Set and test the description */
-    try {
-      catalog = Catalog.createInMemory(DESCRIPTION);
-      assertTrue(catalog.getDescription().equals(DESCRIPTION));
-    } catch (final CatalogException e) {
-      e.printStackTrace();
-      fail(e.toString());
-    }
+    catalog = Catalog.createInMemory(DESCRIPTION);
+    assertTrue(catalog.getDescription().equals(DESCRIPTION));
 
     /* Set and test the server */
-    try {
-      catalog.addMaster(SERVER);
-      final List<SocketInfo> servers = catalog.getMasters();
-      assertTrue(servers.size() == 1);
-      assertTrue(servers.get(0).toString().equals(SERVER));
-    } catch (final CatalogException e) {
-      e.printStackTrace();
-      fail(e.toString());
-    }
+    catalog.addMaster(SERVER);
+    final List<SocketInfo> servers = catalog.getMasters();
+    assertTrue(servers.size() == 1);
+    assertTrue(servers.get(0).toString().equals(SERVER));
 
     /* Set and test the workers */
-    try {
-      for (final String worker : WORKERS) {
-        catalog.addWorker(worker);
-      }
-      final Map<Integer, SocketInfo> workers = catalog.getWorkers();
-      assertTrue(workers.size() == WORKERS.length);
-      /* Slightly annoying equality check here */
-      final Collection<SocketInfo> values = workers.values();
-      for (final String worker : WORKERS) {
-        assertTrue(values.contains(SocketInfo.valueOf(worker)));
-      }
-    } catch (final CatalogException e) {
-      e.printStackTrace();
-      fail(e.toString());
+    for (final String worker : WORKERS) {
+      catalog.addWorker(worker);
     }
+    final Map<Integer, SocketInfo> workers = catalog.getWorkers();
+    assertTrue(workers.size() == WORKERS.length);
+    /* Slightly annoying equality check here */
+    final Collection<SocketInfo> values = workers.values();
+    for (final String worker : WORKERS) {
+      assertTrue(values.contains(SocketInfo.valueOf(worker)));
+    }
+  }
+
+  /**
+   * Test creating a catalog using CatalogMaker functionality.
+   * 
+   * @throws Exception if there is an error making the Catalog.
+   */
+  @Test
+  public void testCatalogMaker() throws Exception {
+    /* Turn off SQLite logging, it's annoying. */
+    Logger.getLogger("com.almworks.sqlite4java").setLevel(Level.OFF);
+    Path path = Files.createTempDirectory(MyriaConstants.SYSTEM_NAME + "_CatalogTest");
+    final int numWorkers = 5;
+    final String masterCatalogPath = path.toString() + File.separatorChar + "master.catalog";
+    CatalogMaker.makeNNodesLocalParallelCatalog(path.toFile().getAbsolutePath(), 5);
+    Catalog c = Catalog.open(masterCatalogPath);
+    assertTrue(c.getWorkers().size() == numWorkers);
+    c.close();
   }
 }
