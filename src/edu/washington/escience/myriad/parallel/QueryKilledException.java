b7076b8e82396386cc9e8abc37e182514815b38c
slxu <slxu@cs.washington.edu>
2013-04-20 23:20:14 -0700

    make a dedicated exception for the query gets killed cause

---
+++ src/edu/washington/escience/myriad/parallel/QueryKilledException.java
@@ -0,0 +1,14 @@
+package edu.washington.escience.myriad.parallel;
+
+public class QueryKilledException extends InterruptedException {
+
+  public QueryKilledException() {
+    // TODO Auto-generated constructor stub
+  }
+
+  public QueryKilledException(String s) {
+    super(s);
+    // TODO Auto-generated constructor stub
+  }
+
+}
