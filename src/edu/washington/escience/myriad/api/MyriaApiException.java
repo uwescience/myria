0e8c675419f387116b876c95f570de1f8b982502
Daniel Halperin <dhalperi@cs.washington.edu>
2013-04-30 16:51:37 -0700

    API: add MyriaApiException
    This is a helper class to wrap WebApplicationException, but use better
    error messages.
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

---
+++ src/edu/washington/escience/myriad/api/MyriaApiException.java
@@ -0,0 +1,77 @@
+package edu.washington.escience.myriad.api;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
+
+/**
+ * A helper function to easily create HTTP error responses in the API server.
+ * 
+ * @author dhalperi
+ * 
+ */
+public final class MyriaApiException extends WebApplicationException {
+
+  /** Required for Java serialization. */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Construct a MyriaApiException from the given status and cause. The entity of the HTTP Response includes the given
+   * HTTP status code and the body is the exception cause. If the status code is 500 INTERNAL SERVER ERROR then we
+   * return the entire stack trace. Otherwise, we just return the message.
+   * 
+   * @param status the HTTP status code used for the HTTP response.
+   * @param cause the Exception, whose message is used to explain the exception in the HTTP response.
+   */
+  public MyriaApiException(final Status status, final Throwable cause) {
+    super(getResponse(status, cause));
+  }
+
+  /**
+   * Construct a MyriaApiException from the given status and explanation. The entity of the HTTP Response includes the
+   * given HTTP status code and the body is the provided explanation string.
+   * 
+   * @param status the HTTP status code used for the HTTP response.
+   * @param explanation the message is used to explain the exception in the HTTP response.
+   */
+  public MyriaApiException(final Status status, final String explanation) {
+    super(getResponse(status, explanation));
+  }
+
+  /**
+   * Get the HTTP response for this status and cause. If the Status code is 500 (Internal Server Error) then we return
+   * the entire stack trace. Otherwise, we just return the message.
+   * 
+   * @param status the HTTP status code to be returned.
+   * @param cause the reason for the exception.
+   * @return the HTTP Response.
+   */
+  private static Response getResponse(final Status status, final Throwable cause) {
+    ResponseBuilder ret = Response.status(status);
+    if (status.equals(Status.INTERNAL_SERVER_ERROR)) {
+      StringWriter stringWriter = new StringWriter();
+      cause.printStackTrace(new PrintWriter(stringWriter));
+      String stackTrace = stringWriter.toString();
+      ret.entity(stackTrace);
+    } else {
+      ret.entity(cause.getMessage());
+    }
+    return ret.build();
+  }
+
+  /**
+   * Get the HTTP response for this status and explanation.
+   * 
+   * @param status the HTTP status code of the response.
+   * @param explanation the explanation of the HTTP error.
+   * @return the Response.
+   */
+  private static Response getResponse(final Status status, final String explanation) {
+    Response ret = Response.status(status).entity(explanation).build();
+    return ret;
+  }
+}
