3d4d2b4f12cdd898b6aef99c2664316e0e8b3d51
Daniel Halperin <dhalperi@cs.washington.edu>
2013-04-30 17:13:57 -0700

    DatasetEncoding: implement MyriaApiEncoding interface
    And corresponding validate() function.
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- src/edu/washington/escience/myriad/api/encoding/DatasetEncoding.java
+++ src/edu/washington/escience/myriad/api/encoding/DatasetEncoding.java
@@ -1,17 +1,33 @@
 package edu.washington.escience.myriad.api.encoding;
 
+import javax.ws.rs.core.Response.Status;
+
+import com.google.common.base.Preconditions;
+
 import edu.washington.escience.myriad.RelationKey;
 import edu.washington.escience.myriad.Schema;
+import edu.washington.escience.myriad.api.MyriaApiException;
 
 /**
  * A JSON-able wrapper for the expected wire message for a new dataset.
  * 
  */
-public class DatasetEncoding {
+public class DatasetEncoding implements MyriaApiEncoding {
   /** The name of the dataset. */
   public RelationKey relationKey;
   /** The Schema of its tuples. */
   public Schema schema;
   /** The data it contains. */
   public byte[] data;
+
+  @Override
+  public void validate() throws MyriaApiException {
+    try {
+      Preconditions.checkNotNull(relationKey);
+      Preconditions.checkNotNull(schema);
+      Preconditions.checkNotNull(data);
+    } catch (Exception e) {
+      throw new MyriaApiException(Status.BAD_REQUEST, "required fields: relation_key, schema, data");
+    }
+  }
 }
