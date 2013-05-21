85be3fd872f789d7675b02e01ca2a378ab3015ce
Daniel Halperin <dhalperi@cs.washington.edu>
2013-05-21 14:32:14 -0700

    Dataset: add POST /dataset/tipsy for a new Tipsy dataset
    Add an API to ingest Tipsy data. JSON POSTed to the `/dataset/tipsy` URL
    will be treated as a TipsyDatasetEncoding and the server will attempt to
    ingest those files.
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

---
+++ src/edu/washington/escience/myriad/api/encoding/TipsyDatasetEncoding.java
@@ -0,0 +1,28 @@
+package edu.washington.escience.myriad.api.encoding;
+
+import javax.ws.rs.core.Response.Status;
+
+import com.google.common.base.Preconditions;
+
+import edu.washington.escience.myriad.RelationKey;
+import edu.washington.escience.myriad.api.MyriaApiException;
+
+public class TipsyDatasetEncoding implements MyriaApiEncoding {
+  public RelationKey relationKey;
+  public String tipsyFilename;
+  public String grpFilename;
+  public String iorderFilename;
+
+  @Override
+  public void validate() throws MyriaApiException {
+    try {
+      Preconditions.checkNotNull(relationKey);
+      Preconditions.checkArgument(tipsyFilename != null);
+      Preconditions.checkArgument(grpFilename != null);
+      Preconditions.checkArgument(iorderFilename != null);
+    } catch (Exception e) {
+      throw new MyriaApiException(Status.BAD_REQUEST,
+          "required fields: relation_key, tipsy_filename, grp_filename, iorder_filename");
+    }
+  }
+}
