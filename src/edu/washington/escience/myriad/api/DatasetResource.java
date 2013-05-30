a2201316c04fd76e3b59565441d930363112dae6
Daniel Halperin <dhalperi@cs.washington.edu>
2013-05-30 16:43:43 -0700

    Dataset: add POST /dataset/tipsy for a new Tipsy dataset
    Add an API to ingest Tipsy data. JSON POSTed to the `/dataset/tipsy` URL
    will be treated as a TipsyDatasetEncoding and the server will attempt to
    ingest those files.
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- src/edu/washington/escience/myriad/api/DatasetResource.java
+++ src/edu/washington/escience/myriad/api/DatasetResource.java
@@ -21,9 +21,11 @@ import edu.washington.escience.myriad.DbException;
 import edu.washington.escience.myriad.RelationKey;
 import edu.washington.escience.myriad.Schema;
 import edu.washington.escience.myriad.api.encoding.DatasetEncoding;
+import edu.washington.escience.myriad.api.encoding.TipsyDatasetEncoding;
 import edu.washington.escience.myriad.coordinator.catalog.CatalogException;
 import edu.washington.escience.myriad.operator.FileScan;
 import edu.washington.escience.myriad.operator.Operator;
+import edu.washington.escience.myriad.operator.TipsyFileScan;
 
 /**
  * This is the class that handles API calls that return relation schemas.
@@ -105,6 +107,59 @@ public final class DatasetResource {
       MyriaApiUtils.getServer().ingestDataset(dataset.relationKey, dataset.workers, source);
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
+    }
+
+    /* In the response, tell the client the path to the relation. */
+    UriBuilder queryUri = uriInfo.getBaseUriBuilder();
+    return Response.created(
+        queryUri.path("dataset").path("user-" + dataset.relationKey.getUserName()).path(
+            "program-" + dataset.relationKey.getProgramName())
+            .path("relation-" + dataset.relationKey.getRelationName()).build()).build();
+  }
+
+  /**
+   * @param payload the request payload.
+   * @param uriInfo information about the current URL.
+   * @return the created dataset resource.
+   * @throws DbException if there is an error in the database.
+   */
+  @POST
+  @Path("/tipsy")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response newTipsyDataset(final byte[] payload, @Context final UriInfo uriInfo) throws DbException {
+    /* Attempt to deserialize the object. */
+    TipsyDatasetEncoding dataset = MyriaApiUtils.deserialize(payload, TipsyDatasetEncoding.class);
+
+    /* If we already have a dataset by this name, tell the user there's a conflict. */
+    try {
+      if (MyriaApiUtils.getServer().getSchema(dataset.relationKey) != null) {
+        /* Found, throw a 409 (Conflict) */
+        throw new MyriaApiException(Status.CONFLICT, "That dataset already exists.");
+      }
+    } catch (CatalogException e) {
+      throw new DbException(e);
+    }
+
+    /* If we don't have any workers alive right now, tell the user we're busy. */
+    Set<Integer> workers = MyriaApiUtils.getServer().getAliveWorkers();
+    if (workers.size() == 0) {
+      /* Throw a 503 (Service Unavailable) */
+      throw new MyriaApiException(Status.SERVICE_UNAVAILABLE, "There are no alive workers to receive this dataset.");
+    }
+
+    /* Moreover, check whether all requested workers are alive. */
+    if (dataset.workers != null && !MyriaApiUtils.getServer().getAliveWorkers().containsAll(dataset.workers)) {
+      /* Throw a 503 (Service Unavailable) */
+      throw new MyriaApiException(Status.SERVICE_UNAVAILABLE, "Not all requested workers are alive");
+    }
+
+    /* Do the work. */
+    try {
+      MyriaApiUtils.getServer().ingestDataset(dataset.relationKey, dataset.workers,
+          new TipsyFileScan(dataset.tipsyFilename, dataset.grpFilename, dataset.iorderFilename));
+    } catch (InterruptedException ee) {
+      Thread.currentThread().interrupt();
+      ee.printStackTrace();
     }
 
     /* In the response, tell the client the path to the relation. */
