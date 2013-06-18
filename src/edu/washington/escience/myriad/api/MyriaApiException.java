f91577bd89440aae5f3417d1208514f9e96a8780
Daniel Halperin <dhalperi@cs.washington.edu>
2013-06-18 14:18:05 -0700

    api: add MyriaExceptionMapper
    This is the class for Jersey that maps exceptions to HTTP results.
    
    Also move the standard exception->response functions here.
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- src/edu/washington/escience/myriad/api/MyriaApiException.java
+++ src/edu/washington/escience/myriad/api/MyriaApiException.java
@@ -1,11 +1,6 @@
 package edu.washington.escience.myriad.api;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
 
 import org.slf4j.LoggerFactory;
@@ -32,7 +27,7 @@ public final class MyriaApiException extends WebApplicationException {
    * @param cause the Exception, whose message is used to explain the exception in the HTTP response.
    */
   public MyriaApiException(final Status status, final Throwable cause) {
-    super(getResponse(status, cause));
+    super(MyriaExceptionMapper.getResponse(status, cause));
     LOGGER.trace("In Status:Throwable (log will be after getResponse)");
   }
 
@@ -44,47 +39,7 @@ public final class MyriaApiException extends WebApplicationException {
    * @param explanation the message is used to explain the exception in the HTTP response.
    */
   public MyriaApiException(final Status status, final String explanation) {
-    super(getResponse(status, explanation));
+    super(MyriaExceptionMapper.getResponse(status, explanation));
     LOGGER.trace("In Status:String (log will be after getResponse)");
-  }
-
-  /**
-   * Get the HTTP response for this status and cause. If the Status code is 500 (Internal Server Error) then we return
-   * the entire stack trace. Otherwise, we just return the message.
-   * 
-   * @param status the HTTP status code to be returned.
-   * @param cause the reason for the exception.
-   * @return the HTTP Response.
-   */
-  private static Response getResponse(final Status status, final Throwable cause) {
-    /* Pass MyriaApiException objects right on through. */
-    if (cause instanceof MyriaApiException) {
-      LOGGER.trace("Here in getResponse::Status:Throwable");
-      return ((MyriaApiException) cause).getResponse();
-    }
-    ResponseBuilder ret = Response.status(status);
-    if (status.equals(Status.INTERNAL_SERVER_ERROR) || cause.getMessage() == null || cause.getMessage().length() == 0) {
-      StringWriter stringWriter = new StringWriter();
-      cause.printStackTrace(new PrintWriter(stringWriter));
-      String stackTrace = stringWriter.toString();
-      ret.entity(stackTrace);
-    } else {
-      ret.entity(cause.getMessage());
-    }
-    LOGGER.trace("In getResponse::Status:Throwable");
-    return ret.build();
-  }
-
-  /**
-   * Get the HTTP response for this status and explanation.
-   * 
-   * @param status the HTTP status code of the response.
-   * @param explanation the explanation of the HTTP error.
-   * @return the Response.
-   */
-  private static Response getResponse(final Status status, final String explanation) {
-    Response ret = Response.status(status).entity(explanation).build();
-    LOGGER.trace("In getResponse::Status:String");
-    return ret;
   }
 }
