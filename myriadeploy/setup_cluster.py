7ed344c286e4132149fd22f5a1a23b93be776452
Jingjing Wang <jwang@cs.washington.edu>
2013-07-03 20:18:27 -0700

    Move deployment functions into Myria, let python scripts call them directly. For recovery, Myria needs to have the ability to make catalogs, copy files, start a new worker inside itself. These jobs were done by python scripts outside of Myria, now they are moved inside.

--- myriadeploy/setup_cluster.py
+++ myriadeploy/setup_cluster.py
@@ -5,104 +5,27 @@ import myriadeploy
 import subprocess
 import sys
 
-def host_port_list(workers):
-    return [str(worker[0]) + ':' + str(worker[1]) for worker in workers]
-
 def make_catalog(config_file):
     """Creates a Myria catalog (running the Java program to do so) from the
 given deployment configuration."""
-
-    # Create a new one.
     args = ["./run_catalog_maker.sh", config_file]
     if subprocess.call(args):
-        print >> sys.stderr, "error making the Catalog"
-        sys.exit(1)
+        raise Exception("Error making the catalogs");
 
-def remote_mkdir(hostname, dirname, username):
-    if hostname != 'localhost':
-        args = ["ssh", "%s@%s" % (username, hostname), "mkdir", "-p", dirname]
-    else:
-        args = ["mkdir", "-p", dirname]
-    return subprocess.call(args)
-
-def copy_master_catalog(hostname, dirname, path, username):
-    local_path = "%s/%s" % (dirname, "master.catalog")
-    if hostname != 'localhost':
-        remote_path = "%s@%s:%s/%s-files/%s" % (username, hostname, path, dirname, dirname)
-    else:
-        remote_path = "%s/%s-files/%s" % (path, dirname, dirname)
-    args = ["rsync", "-aLvz", local_path, remote_path]
-    return subprocess.call(args)
-
-def copy_worker_catalog(hostname, dirname, path, i, username):
-    local_path = "%s/worker_%d" % (dirname, i)
-    if hostname != 'localhost':
-        remote_path = "%s@%s:%s/%s-files/%s" % (username, hostname, path, dirname, dirname)
-    else:
-        remote_path = "%s/%s-files/%s" % (path, dirname, dirname)
-    args = ["rsync", "-aLvz", local_path, remote_path]
-    return subprocess.call(args)
-
-def get_host_port_path(node, default_path):
-    if len(node) == 2:
-        (hostname, port) = node
-        if default_path is None:
-            raise Exception("Path not specified for node %s" % str(node))
-        else:
-            path = default_path
-    else:
-        (hostname, port, path) = node
-    return (hostname, port, path)
-
-def copy_catalogs(config):
+def copy_catalogs(config_file):
     """Copies the master and worker catalogs to the remote hosts."""
-    description = config['description']
-    default_path = config['path']
-    master = config['master']
-    workers = config['workers']
-    username = config['username']
+    args = ["./using_deployment_utils.sh", config_file, "-copy_master_catalog"]
+    if subprocess.call(args):
+        raise Exception("Error copying master catalog");
+    args = ["./using_deployment_utils.sh", config_file, "-copy_worker_catalogs"]
+    if subprocess.call(args):
+        raise Exception("Error copying worker catalogs");
 
-    # Make directories on master
-    (hostname, _, path) = get_host_port_path(master, default_path)
-    if remote_mkdir(hostname, "%s/%s-files/%s" \
-            % (path, description, description), username):
-        raise Exception("Error making directory on master %s" \
-                % (hostname,))
-    # Copy files to master
-    if copy_master_catalog(hostname, description, path, username):
-        raise Exception("Error copying master.catalog to %s" % (hostname,))
-
-    for (i, worker) in enumerate(workers):
-        # Workers are numbered from 1, not 0
-        worker_id = i + 1
-    
-        # Make directories on the worker
-        (hostname, _, path) = get_host_port_path(worker, default_path)
-        if remote_mkdir(hostname, "%s/%s-files/%s" \
-                % (path, description, description), username):
-            raise Exception("Error making directory on worker %d %s" \
-                    % (worker_id, hostname))
-        # Copy the files to the worker
-        if copy_worker_catalog(hostname, description, path, worker_id, username):
-            raise Exception("Error copying worker.catalog to %s " % (hostname,))
-
-def copy_distribution(config):
+def copy_distribution(config_file):
     "Copy the distribution (jar and libs and conf) to compute nodes."
-    nodes = config['nodes']
-    default_path = config['path']
-    description = config['description']
-    username = config['username']
-
-    for node in nodes:
-        (hostname, _, path) = get_host_port_path(node, default_path)
-        if hostname != 'localhost':
-            remote_path = "%s@%s:%s/%s-files" % (username, hostname, path, description)
-        else:
-            remote_path = "%s/%s-files" % (path, description)
-        to_copy = ["libs", "conf", "sqlite4java-282"]
-        args = ["rsync", "-aLvz"] + to_copy + [remote_path]
-        if subprocess.call(args):
-            raise Exception("Error copying distribution to %s" % (hostname,))
+    args = ["./using_deployment_utils.sh", config_file, "-copy_distribution"]
+    if subprocess.call(args):
+        raise Exception("Error copying distribution to %s" % (hostname,))
 
 def main(argv):
     # Usage
@@ -111,16 +34,14 @@ def main(argv):
         print >> sys.stderr, "       deployment.cfg: a configuration file modeled after deployment.cfg.sample"
         sys.exit(1)
 
-    config = myriadeploy.read_config_file(argv[1])
-
     # Step 1: make the Catalog
     make_catalog(argv[1])
 
     # Step 2: Copy each catalog over
-    copy_catalogs(config)
+    copy_catalogs(argv[1])
 
     # Step 3: Copy over java, libs, myriad, conf
-    copy_distribution(config)
+    copy_distribution(argv[1])
 
 if __name__ == "__main__":
     main(sys.argv)
---
+++ src/edu/washington/escience/myriad/util/DeploymentUtils.java
@@ -0,0 +1,272 @@
+package edu.washington.escience.myriad.util;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.LoggerFactory;
+
+import edu.washington.escience.myriad.tool.MyriaConfigurationReader;
+
+/**
+ * Deployment util methods.
+ * */
+public final class DeploymentUtils {
+
+  /** usage. */
+  public static final String USAGE =
+      "java DeploymentUtils <config_file> <-copy_master_catalog | -copy_worker_catalogs | -copy_distribution | -start_master | -start_workers>";
+  /** The reader. */
+  private static final MyriaConfigurationReader READER = new MyriaConfigurationReader();
+  /** The logger. */
+  private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(DeploymentUtils.class);
+
+  /**
+   * entry point.
+   * 
+   * @param args args.
+   * @throws IOException if file system error occurs.
+   * */
+  public static void main(final String[] args) throws IOException {
+    if (args.length != 2) {
+      System.out.println(USAGE);
+    }
+
+    Map<String, HashMap<String, String>> config = READER.load(args[0]);
+    String workingDir = config.get("deployment").get("path");
+    String description = config.get("deployment").get("name");
+    String username = config.get("deployment").get("username");
+
+    final String action = args[1];
+    if (action.equals("-copy_master_catalog")) {
+      String remotePath = workingDir + "/" + description + "-files" + "/" + description;
+      // Although we have only one master now
+      HashMap<String, String> masters = config.get("master");
+      for (String masterId : masters.keySet()) {
+        String hostname = getHostname(masters.get(masterId));
+        if (username != null) {
+          hostname = username + "@" + hostname;
+        }
+        String localPath = description + "/" + "master.catalog";
+        mkdir(hostname, remotePath);
+        rsyncFileToRemote(localPath, hostname, remotePath);
+      }
+    } else if (action.equals("-copy_worker_catalogs")) {
+      String remotePath = workingDir + "/" + description + "-files" + "/" + description;
+      HashMap<String, String> workers = config.get("workers");
+      for (String workerId : workers.keySet()) {
+        String hostname = getHostname(workers.get(workerId));
+        if (username != null) {
+          hostname = username + "@" + hostname;
+        }
+        String localPath = description + "/" + "worker_" + workerId;
+        mkdir(hostname, remotePath);
+        rsyncFileToRemote(localPath, hostname, remotePath);
+      }
+    } else if (action.equals("-copy_distribution")) {
+      String remotePath = workingDir + "/" + description + "-files";
+      HashMap<String, String> masters = config.get("master");
+      for (String masterId : masters.keySet()) {
+        String hostname = getHostname(masters.get(masterId));
+        if (username != null) {
+          hostname = username + "@" + hostname;
+        }
+        rsyncFileToRemote("libs", hostname, remotePath);
+        rsyncFileToRemote("conf", hostname, remotePath);
+        rsyncFileToRemote("sqlite4java-282", hostname, remotePath);
+      }
+      HashMap<String, String> workers = config.get("workers");
+      for (String workerId : workers.keySet()) {
+        String hostname = getHostname(workers.get(workerId));
+        if (username != null) {
+          hostname = username + "@" + hostname;
+        }
+        rsyncFileToRemote("libs", hostname, remotePath);
+        rsyncFileToRemote("conf", hostname, remotePath);
+        rsyncFileToRemote("sqlite4java-282", hostname, remotePath);
+      }
+    } else if (action.equals("-start_master")) {
+      String restPort = config.get("deployment").get("rest_port");
+      String maxHeapSize = config.get("deployment").get("max_heap_size");
+      if (maxHeapSize == null) {
+        maxHeapSize = "";
+      }
+      HashMap<String, String> masters = config.get("master");
+      for (String masterId : masters.keySet()) {
+        String hostname = getHostname(masters.get(masterId));
+        if (username != null) {
+          hostname = username + "@" + hostname;
+        }
+        startMaster(hostname, workingDir, description, maxHeapSize, restPort);
+      }
+
+    } else if (action.equals("-start_workers")) {
+      String maxHeapSize = config.get("deployment").get("max_heap_size");
+      if (maxHeapSize == null) {
+        maxHeapSize = "";
+      }
+      HashMap<String, String> workers = config.get("workers");
+      for (String workerId : workers.keySet()) {
+        String hostname = getHostname(workers.get(workerId));
+        if (username != null) {
+          hostname = username + "@" + hostname;
+        }
+        startWorker(hostname, workingDir, description, maxHeapSize, workerId);
+      }
+    } else {
+      System.out.println(USAGE);
+    }
+  }
+
+  /**
+   * start a worker process on a remote machine.
+   * 
+   * @param address e.g. beijing.cs.washington.edu
+   * @param workingDir the same meaning as path in deployment.cfg
+   * @param description the same meaning as name in deployment.cfg
+   * @param maxHeapSize the same meaning as max_heap_size in deployment.cfg
+   * @param workerId the worker id.
+   */
+  public static void startWorker(final String address, final String workingDir, final String description,
+      final String maxHeapSize, final String workerId) {
+    StringBuilder builder = new StringBuilder();
+    builder.append("ssh " + address);
+    builder.append(" cd " + workingDir + "/" + description + "-files;");
+    builder.append(" nohup java -cp 'libs/*'");
+    builder.append(" -Djava.util.logging.config.file=conf/logging.properties");
+    builder.append(" -Dlog4j.configuration=log4j.properties");
+    builder.append(" -Djava.library.path=sqlite4java-282");
+    builder.append(" " + maxHeapSize);
+    builder.append(" edu.washington.escience.myriad.parallel.Worker");
+    builder.append(" --workingDir " + description + "/worker_" + workerId);
+    builder.append(" 0</dev/null");
+    builder.append(" 1>worker_" + workerId + "_stdout");
+    builder.append(" 2>worker_" + workerId + "_stderr");
+    builder.append(" &");
+    System.out.println(address);
+    startAProcess(builder.toString());
+  }
+
+  /**
+   * start a master process on a remote machine.
+   * 
+   * @param address e.g. beijing.cs.washington.edu
+   * @param workingDir the same meaning as path in deployment.cfg
+   * @param description the same meaning as name in deployment.cfg
+   * @param maxHeapSize the same meaning as max_heap_size in deployment.cfg
+   * @param restPort the port number for restlet.
+   */
+  public static void startMaster(final String address, final String workingDir, final String description,
+      final String maxHeapSize, final String restPort) {
+    StringBuilder builder = new StringBuilder();
+    builder.append("ssh " + address);
+    builder.append(" cd " + workingDir + "/" + description + "-files;");
+    builder.append(" nohup java -cp 'libs/*'");
+    builder.append(" -Djava.util.logging.config.file=conf/logging.properties");
+    builder.append(" -Dlog4j.configuration=log4j.properties");
+    builder.append(" -Djava.library.path=sqlite4java-282");
+    builder.append(" " + maxHeapSize);
+    builder.append(" edu.washington.escience.myriad.daemon.MasterDaemon");
+    builder.append(" " + description + " " + restPort);
+    builder.append(" 0</dev/null");
+    builder.append(" 1>master_stdout");
+    builder.append(" 2>master_stderr");
+    builder.append(" &");
+    System.out.println(address);
+    startAProcess(builder.toString());
+
+    String hostname = address;
+    if (hostname.indexOf('@') != -1) {
+      hostname = address.substring(hostname.indexOf('@') + 1);
+    }
+    URL masterAliveUrl;
+    try {
+      masterAliveUrl = new URL("http://" + hostname + ":" + restPort + "/workers/alive");
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
+    }
+    long start = System.currentTimeMillis();
+    while (true) {
+      try {
+        HttpURLConnection request = (HttpURLConnection) masterAliveUrl.openConnection();
+        if (request.getResponseCode() == 200) {
+          break;
+        }
+      } catch (IOException e) {
+        // pass
+      }
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+      }
+      if (System.currentTimeMillis() - start > 20000) {
+        throw new RuntimeException("after 20s master " + address + " is not alive");
+      }
+    }
+  }
+
+  /**
+   * Call mkdir on a remote machine.
+   * 
+   * @param address e.g. beijing.cs.washington.edu
+   * @param remotePath e.g. /tmp/test
+   */
+  public static void mkdir(final String address, final String remotePath) {
+    StringBuilder builder = new StringBuilder();
+    builder.append("ssh");
+    builder.append(" " + address);
+    builder.append(" mkdir -p");
+    builder.append(" " + remotePath);
+    startAProcess(builder.toString());
+  }
+
+  /**
+   * Copy a local file to a location on a remote machine, using rsync.
+   * 
+   * @param localPath path to the local file that you want to copy from
+   * @param address e.g. beijing.cs.washington.edu
+   * @param remotePath e.g. /tmp/test
+   */
+  public static void rsyncFileToRemote(final String localPath, final String address, final String remotePath) {
+    StringBuilder builder = new StringBuilder();
+    builder.append("rsync");
+    builder.append(" -aLvz");
+    builder.append(" " + localPath);
+    builder.append(" " + address + ":" + remotePath);
+    startAProcess(builder.toString());
+  }
+
+  /**
+   * start a process by ProcessBuilder.
+   * 
+   * @param cmd the command.
+   */
+  private static void startAProcess(final String cmd) {
+    LOGGER.debug(cmd);
+    try {
+      new ProcessBuilder().inheritIO().command(cmd.split(" ")).start();
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * Helper function to get the hostname from hostname:port.
+   * 
+   * @param s the string hostname:port
+   * @return the hostname.
+   * */
+  private static String getHostname(final String s) {
+    return s.split(":")[0];
+  }
+
+  /**
+   * util classes are not instantiable.
+   * */
+  private DeploymentUtils() {
+  }
+}
