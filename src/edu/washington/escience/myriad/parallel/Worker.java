a265757208ace73795a13bcded4c2332189665ee
slxu <slxu@cs.washington.edu>
2013-07-10 16:08:14 -0700

    Add ShutdownThreadCleaner to Worker
    When worker shutdown is requested, ShutdownThreadCleaner will monitor
    the status of the running user threads, if there is any blocking user
    threads, it'll interrupt them. The ShutdownThreadCleaner makes sure
    that the Worker will get sucessfully shutdown within some pre-defined
    waiting time interval.
    
    Maybe the master also need one. May transplant it to master if needed
    in the future.

--- src/edu/washington/escience/myriad/parallel/Worker.java
+++ src/edu/washington/escience/myriad/parallel/Worker.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
@@ -27,6 +28,7 @@ import org.jboss.netty.handler.execution.OrderedMemoryAwareThreadPoolExecutor;
 
 import com.almworks.sqlite4java.SQLiteConnection;
 import com.almworks.sqlite4java.SQLiteException;
+import com.google.common.collect.Sets;
 
 import edu.washington.escience.myriad.DbException;
 import edu.washington.escience.myriad.MyriaConstants;
@@ -213,16 +215,126 @@ public final class Worker {
   private class ShutdownChecker extends TimerTask {
     @Override
     public final synchronized void run() {
-      try {
-        if (toShutdown) {
-          shutdown();
+      if (toShutdown) {
+        shutdown();
+      }
+    }
+  }
+
+  /**
+   * Periodically detect whether the {@link Worker} should be shutdown.
+   * */
+  private class ShutdownThreadCleaner extends Thread {
+
+    /**
+     * In wait state for at most 5 seconds.
+     * */
+    static final int WAIT_MAXIMUM_MS = 5 * 1000;
+    /**
+     * Interrupt an unresponding thread for at most 3 times.
+     * */
+    static final int MAX_INTERRUPT_TIMES = 3;
+
+    /**
+     * for setting not daemon.
+     * */
+    ShutdownThreadCleaner() {
+      super.setDaemon(true);
+    }
+
+    /**
+     * How many milliseconds a thread have been waited to get finish.
+     * */
+    private final HashMap<Thread, Integer> waitedForMS = new HashMap<Thread, Integer>();
+    /**
+     * How many times a thread has been interrupted.
+     * */
+    private final HashMap<Thread, Integer> interruptTimes = new HashMap<Thread, Integer>();
+    /**
+     * The set of threads we have been waiting for the maximum MS, and so have decided to kill them directly.
+     * */
+    private final Set<Thread> abandonThreads = Sets.newSetFromMap(new HashMap<Thread, Boolean>());
+
+    /**
+     * utility method, add an integer v to the value of m[t] and return the new value. null key and value are taken ca
+     * of.
+     * 
+     * @return the new value
+     * @param m a map
+     * @param t a thread
+     * @param v the value
+     * */
+    private int addToMap(final Map<Thread, Integer> m, final Thread t, final int v) {
+      Integer tt = m.get(t);
+      if (tt == null) {
+        tt = 0;
+      }
+      m.put(t, tt + v);
+      return tt + v;
+    }
+
+    /**
+     * utility method, get the value of m[t] . null key and value are taken care of.
+     * 
+     * @param m a map
+     * @param t a thread
+     * @return the value
+     * */
+    private int getFromMap(final Map<Thread, Integer> m, final Thread t) {
+      Integer tt = m.get(t);
+      if (tt == null) {
+        tt = 0;
+      }
+      return tt;
+    }
+
+    @Override
+    public final void run() {
+
+      while (true) {
+        Set<Thread> allThreads = Thread.getAllStackTraces().keySet();
+        HashMap<Thread, Integer> nonSystemThreads = new HashMap<Thread, Integer>();
+        for (final Thread t : allThreads) {
+          if (t.getThreadGroup() != null && t.getThreadGroup() != mainThreadGroup
+              && t.getThreadGroup() != mainThreadGroup.getParent() && t != Thread.currentThread()
+              && !abandonThreads.contains(t)) {
+            nonSystemThreads.put(t, 0);
+          }
         }
-      } catch (Throwable ee) {
-        if (LOGGER.isErrorEnabled()) {
-          LOGGER.error("Unknown exception caught at shutdown checker.", ee);
+
+        if (nonSystemThreads.isEmpty()) {
+          if (abandonThreads.isEmpty()) {
+            return;
+          } else {
+            JVMUtils.shutdownVM();
+          }
+        }
+
+        try {
+          Thread.sleep(MyriaConstants.SHORT_WAITING_INTERVAL_100_MS);
+        } catch (InterruptedException e) {
+          JVMUtils.shutdownVM();
+        }
+
+        for (final Thread t : nonSystemThreads.keySet()) {
+          if (addToMap(waitedForMS, t, MyriaConstants.SHORT_WAITING_INTERVAL_100_MS) > WAIT_MAXIMUM_MS) {
+            waitedForMS.put(t, 0);
+            if (addToMap(interruptTimes, t, 1) > MAX_INTERRUPT_TIMES) {
+              if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Thread {} have been interrupted for {} times. Kill it directly.", t, getFromMap(
+                    interruptTimes, t) - 1);
+              }
+              abandonThreads.add(t);
+            } else {
+              if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Waited Thread {} to finish for {} seconds. I'll try interrupting it.", t,
+                    TimeUnit.MILLISECONDS.toSeconds(WAIT_MAXIMUM_MS) * getFromMap(interruptTimes, t));
+              }
+              t.interrupt();
+            }
+          }
         }
       }
-
     }
   }
 
@@ -332,6 +444,11 @@ public final class Worker {
   private final ConcurrentHashMap<String, Object> execEnvVars;
 
   /**
+   * The thread group of the main thread.
+   * */
+  private static volatile ThreadGroup mainThreadGroup;
+
+  /**
    * Worker process entry point.
    * 
    * @param args command line arguments.
@@ -346,10 +463,10 @@ public final class Worker {
         JVMUtils.shutdownVM();
       }
 
-      String workingDir = null;
+      String workingDirTmp = System.getProperty("user.dir");
       if (args.length >= 2) {
         if (args[0].equals("--workingDir")) {
-          workingDir = args[1];
+          workingDirTmp = args[1];
         } else {
           if (LOGGER.isErrorEnabled()) {
             LOGGER.error("Invalid arguments.\n" + USAGE);
@@ -358,21 +475,39 @@ public final class Worker {
         }
       }
 
+      final String workingDir = workingDirTmp;
       if (LOGGER.isInfoEnabled()) {
         LOGGER.info("workingDir: " + workingDir);
       }
-      // Instantiate a new worker
-      final Worker w = new Worker(workingDir, QueryExecutionMode.NON_BLOCKING);
-      // int port = w.port;
+      mainThreadGroup = Thread.currentThread().getThreadGroup();
+      ThreadGroup workerThreadGroup = new ThreadGroup(mainThreadGroup, "MyriaWorkerThreadGroup");
+      Thread myriaWorkerMain = new Thread(workerThreadGroup, "MyriaWorkerMain") {
 
-      // Start the actual message handler by binding
-      // the acceptor to a network socket
-      // Now the worker can accept messages
-      w.start();
+        @Override
+        public void run() {
+          try {
+            // Instantiate a new worker
+            final Worker w = new Worker(workingDir, QueryExecutionMode.NON_BLOCKING);
+            // int port = w.port;
 
-      if (LOGGER.isInfoEnabled()) {
-        LOGGER.info("Worker started at:" + w.catalog.getWorkers().get(w.myID));
-      }
+            // Start the actual message handler by binding
+            // the acceptor to a network socket
+            // Now the worker can accept messages
+            w.start();
+
+            if (LOGGER.isInfoEnabled()) {
+              LOGGER.info("Worker started at:" + w.catalog.getWorkers().get(w.myID));
+            }
+          } catch (Throwable e) {
+            if (LOGGER.isErrorEnabled()) {
+              LOGGER.error("Unknown error occurs at Worker. Quit directly.", e);
+            }
+          }
+
+        }
+      };
+      myriaWorkerMain.start();
+
     } catch (Throwable e) {
       if (LOGGER.isErrorEnabled()) {
         LOGGER.error("Unknown error occurs at Worker. Quit directly.", e);
@@ -596,35 +731,40 @@ public final class Worker {
   /**
    * This method should be called whenever the system is going to shutdown.
    * 
-   * @throws InterruptedException if the shutdown process is interrupted.
    */
-  public void shutdown() throws InterruptedException {
-    if (LOGGER.isInfoEnabled()) {
-      LOGGER.info("Shutdown requested. Please wait when cleaning up...");
-    }
-    if (!connectionPool.isShutdown()) {
-      if (!abruptShutdown) {
-        connectionPool.shutdown().await();
-      } else {
-        connectionPool.shutdownNow().await();
+  void shutdown() {
+    try {
+      if (LOGGER.isInfoEnabled()) {
+        LOGGER.info("Shutdown requested. Please wait when cleaning up...");
       }
-    }
-    connectionPool.releaseExternalResources();
 
-    if (pipelineExecutor != null && !pipelineExecutor.isShutdown()) {
-      pipelineExecutor.shutdown();
-    }
+      if (!connectionPool.isShutdown()) {
+        if (!abruptShutdown) {
+          connectionPool.shutdown();
+        } else {
+          connectionPool.shutdownNow();
+        }
+      }
+      connectionPool.releaseExternalResources();
 
-    if (LOGGER.isInfoEnabled()) {
-      LOGGER.info("shutdown IPC completed");
-    }
-    // must use shutdownNow here because the query queue processor and the control message processor are both blocking.
-    // We have to interrupt them at shutdown.
-    messageProcessingExecutor.shutdownNow();
-    queryExecutor.shutdown();
-    scheduledTaskExecutor.shutdown();
-    if (LOGGER.isInfoEnabled()) {
-      LOGGER.info("Worker #" + myID + " shutdown completed");
+      if (pipelineExecutor != null && !pipelineExecutor.isShutdown()) {
+        pipelineExecutor.shutdown();
+      }
+
+      if (LOGGER.isInfoEnabled()) {
+        LOGGER.info("shutdown IPC completed");
+      }
+      // must use shutdownNow here because the query queue processor and the control message processor are both
+      // blocking.
+      // We have to interrupt them at shutdown.
+      messageProcessingExecutor.shutdownNow();
+      queryExecutor.shutdown();
+      scheduledTaskExecutor.shutdown();
+      if (LOGGER.isInfoEnabled()) {
+        LOGGER.info("Worker #" + myID + " shutdown completed");
+      }
+    } finally {
+      new Thread(mainThreadGroup, new ShutdownThreadCleaner(), "ShutdownThreadCleaner").start();
     }
   }
 
