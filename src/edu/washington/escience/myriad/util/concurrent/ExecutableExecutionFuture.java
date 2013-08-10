88e0fb6ae22d539ca31a6f5ca16967a960856ef4
slxu <slxu@cs.washington.edu>
2013-08-09 21:08:04 -0700

    Add ThreadAffinityExecutor.
    This executor runs the same Runnable or Callable in the same thread
    when the Runnable or Callable is submitted repeatedly. This executor
    won't have the SQLite thread confinement problem.

---
+++ src/edu/washington/escience/myriad/util/concurrent/ExecutableExecutionFuture.java
@@ -0,0 +1,221 @@
+package edu.washington.escience.myriad.util.concurrent;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.google.common.base.Preconditions;
+
+import edu.washington.escience.myriad.DbException;
+
+/**
+ * The {@link ExecutionFuture} implementation that is Callable and Runnable. The state of the future is set
+ * automatically by the execution of this task.
+ * 
+ * @param <T> The return type.
+ */
+public class ExecutableExecutionFuture<T> extends OperationFutureBase<T> implements ExecutionFuture<T>, Callable<T>,
+    Runnable {
+
+  /**
+   * The {@link Callable} who caused the creation of this future.
+   * */
+  private final Callable<T> callable;
+
+  /**
+   * The thread who is executing the task.
+   * */
+  private volatile Thread executingThread;
+
+  /**
+   * The lock to make sure the thread interrupt action is correct in {@link this#cancel(boolean)}.
+   * */
+  private final Object interruptedCheckingLock = new Object();
+
+  /**
+   * Creates a new instance.
+   * 
+   * @param callable the {@link Callable} who caused the creation of this future.
+   * 
+   * @param cancellable {@code true} if and only if this future can be canceled
+   */
+  public ExecutableExecutionFuture(final Callable<T> callable, final boolean cancellable) {
+    super(cancellable);
+    Preconditions.checkNotNull(callable);
+    this.callable = callable;
+  }
+
+  /**
+   * Creates a new instance.
+   * 
+   * @param runnable the {@link Runnable} who caused the creation of this future.
+   * 
+   * @param cancellable {@code true} if and only if this future can be canceled
+   */
+  public ExecutableExecutionFuture(final Runnable runnable, final boolean cancellable) {
+    this(Executors.callable(runnable, (T) null), cancellable);
+  }
+
+  @Override
+  public final ExecutionFuture<T> sync() throws InterruptedException, DbException {
+    sync0();
+    return this;
+  }
+
+  @Override
+  public final ExecutionFuture<T> syncUninterruptibly() throws DbException {
+    super.syncUninterruptibly0();
+    return this;
+  }
+
+  @Override
+  public final ExecutionFuture<T> await() throws InterruptedException {
+    super.await0();
+    return this;
+  }
+
+  @Override
+  public final ExecutionFuture<T> awaitUninterruptibly() {
+    super.awaitUninterruptibly0();
+    return this;
+  }
+
+  /**
+   * running state.
+   * */
+  protected static final int RUNNING = 2;
+
+  @Override
+  public boolean cancel(final boolean mayInterruptIfRunning) {
+    if (!isCancellable()) {
+      return false;
+    }
+
+    if (isDone()) {
+      return isCancelled();
+    }
+
+    if (!super.compareAndSetState(READY, CANCELL)) {
+      // already running or already done
+      if (mayInterruptIfRunning) {
+        Thread t = executingThread;
+        if (t != null) {
+          // cancel in running
+          if (t == Thread.currentThread()) {
+            cancel();
+          } else if (super.compareAndSetState(RUNNING, CANCELL)) {
+            synchronized (interruptedCheckingLock) {
+              t = executingThread;
+              if (t != null) {
+                t.interrupt();
+              }
+            }
+            this.awaitUninterruptibly();
+            wakeupWaitersAndNotifyListeners();
+          }
+        }
+      }
+    } else {
+      wakeupWaitersAndNotifyListeners();
+    }
+    return isCancelled();
+  }
+
+  @Override
+  public T get() throws InterruptedException, ExecutionException {
+    this.await();
+    return getResult();
+  }
+
+  @Override
+  public T get(final long timeout, final TimeUnit unit) throws InterruptedException, ExecutionException,
+      TimeoutException {
+    this.await(timeout, unit);
+    if (isDone()) {
+      return getResult();
+    }
+    return null;
+  }
+
+  @Override
+  public Callable<T> getCallable() {
+    return callable;
+  }
+
+  @Override
+  public ExecutionFuture<T> addListener(final OperationFutureListener listener) {
+    super.addListener0(listener);;
+    return this;
+  }
+
+  @Override
+  public ExecutionFuture<T> removeListener(final OperationFutureListener listener) {
+    super.removeListener0(listener);
+    return this;
+  }
+
+  @Override
+  public T call() throws Exception {
+    if (!compareAndSetState(READY, RUNNING)) {
+      if (isDone()) {
+        return this.get();
+      } else {
+        throw new RejectedExecutionException("Another thread (" + this.executingThread + ") is executing this task");
+      }
+    }
+    this.executingThread = Thread.currentThread();
+    try {
+      T result = null;
+      try {
+        if (Thread.interrupted()) {
+          throw new InterruptedException();
+        }
+        result = this.callable.call();
+        if (Thread.interrupted()) {
+          throw new InterruptedException();
+        }
+      } catch (InterruptedException e) {
+        if (isCancelled()) {
+          // interrupted because the operation is cancelled.
+          return null;
+        } else {
+          // unexpected interrupted, for example caused by shutdownNow
+          throw e;
+        }
+      }
+      if (setSuccess0(result)) {
+        return result;
+      } else {
+        return null;
+      }
+    } catch (Throwable e) {
+      setFailure0(e);
+      if (e instanceof Exception) {
+        throw e;
+      }
+      return this.get();
+    } finally {
+
+      synchronized (interruptedCheckingLock) {
+        this.executingThread = null;
+        if (Thread.currentThread().isInterrupted()) {
+          if (!isCancelled()) {
+            throw new InterruptedException();
+          }
+        }
+      }
+    }
+  }
+
+  @Override
+  public void run() {
+    try {
+      this.call();
+    } catch (Exception e) {
+      throw new Error(e);
+    }
+  }
+}
---
+++ src/edu/washington/escience/myriad/util/concurrent/ExecutionFuture.java
@@ -0,0 +1,54 @@
+/*
+ * 
+ * This file is a modification to the ChannelFuture interface in Netty.
+ * 
+ * Copyright 2012 The Netty Project
+ * 
+ * The Netty Project licenses this file to you under the Apache License, version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package edu.washington.escience.myriad.util.concurrent;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+import edu.washington.escience.myriad.DbException;
+import edu.washington.escience.myriad.util.Attachmentable;
+
+/**
+ * The handle of an asynchronous {@link Callable} operation.
+ * 
+ * @param <T> the return type of the {@link Callable}
+ */
+public interface ExecutionFuture<T> extends Attachmentable, Future<T>, OperationFuture {
+
+  /**
+   * @return the {@link Callable} object associated with this future.
+   */
+  Callable<T> getCallable();
+
+  @Override
+  ExecutionFuture<T> addListener(OperationFutureListener listener);
+
+  @Override
+  ExecutionFuture<T> removeListener(final OperationFutureListener listener);
+
+  @Override
+  ExecutionFuture<T> sync() throws InterruptedException, DbException;
+
+  @Override
+  ExecutionFuture<T> syncUninterruptibly() throws DbException;
+
+  @Override
+  ExecutionFuture<T> await() throws InterruptedException;
+
+  @Override
+  ExecutionFuture<T> awaitUninterruptibly();
+
+}
