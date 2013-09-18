fd3c6b4434651ff51adfd49067ac5806407f71c8
jingjingwang <jwang@cs.washington.edu>
2013-09-18 12:22:54 -0700

    Add 3 StreamingStateUpdater: DupElim, KeepMinValue, SimpleAppender.
    DupElim used to be an operator but not anymore. It's a
    StreamingStateUpdater that should be attached to an operator which
    implements StreamingAggregate, for example, an IDBInput or a
    StreamingAggregateAdaptor.
    
    KeepMinValue always keeps the tuple with smaller value under the same
    key.
    
    SimpleAppender simply just keep appending tuples with no replacement.

--- src/edu/washington/escience/myria/operator/KeepMinValue.java
+++ src/edu/washington/escience/myria/operator/KeepMinValue.java
@@ -13,12 +13,13 @@ import com.google.common.collect.ImmutableMap;
 import edu.washington.escience.myria.Schema;
 import edu.washington.escience.myria.TupleBatch;
 import edu.washington.escience.myria.TupleBuffer;
+import edu.washington.escience.myria.Type;
 
 /**
- * Duplicate elimination. It adds newly meet unique tuples into a buffer so that the source TupleBatches are not
- * referenced. This implementation reduces memory consumption.
+ * Keeps min vaule. It adds newly meet unique tuples into a buffer so that the source TupleBatches are not referenced.
+ * This implementation reduces memory consumption.
  * */
-public final class DupElim extends StreamingStateUpdater {
+public final class KeepMinValue extends StreamingStateUpdater {
 
   /** Required for Java serialization. */
   private static final long serialVersionUID = 1L;
@@ -26,7 +27,7 @@ public final class DupElim extends StreamingStateUpdater {
   /**
    * The logger for this class.
    * */
-  private static final Logger LOGGER = LoggerFactory.getLogger(DupElim.class.getName());
+  private static final Logger LOGGER = LoggerFactory.getLogger(KeepMinValue.class.getName());
 
   /**
    * Indices to unique tuples.
@@ -37,6 +38,21 @@ public final class DupElim extends StreamingStateUpdater {
    * The buffer for stroing unique tuples.
    * */
   private transient TupleBuffer uniqueTuples = null;
+
+  /** column indices of the key. */
+  private final int[] keyColIndices;
+  /** column indices of the value. */
+  private final int valueColIndex;
+
+  /**
+   * 
+   * @param keyColIndices column indices of the key
+   * @param valueColIndex column index of the value
+   */
+  public KeepMinValue(final int[] keyColIndices, final int valueColIndex) {
+    this.keyColIndices = keyColIndices;
+    this.valueColIndex = valueColIndex;
+  }
 
   @Override
   public void cleanup() {
@@ -52,7 +68,7 @@ public final class DupElim extends StreamingStateUpdater {
    * @return true if equals.
    * */
   private boolean tupleEquals(final int index, final List<Object> cntTuple) {
-    for (int i = 0; i < cntTuple.size(); ++i) {
+    for (int i : keyColIndices) {
       if (!(uniqueTuples.get(i, index)).equals(cntTuple.get(i))) {
         return false;
       }
@@ -61,12 +77,35 @@ public final class DupElim extends StreamingStateUpdater {
   }
 
   /**
+   * Check if a tuple in uniqueTuples equals to the comparing tuple (cntTuple).
+   * 
+   * @param index the index in uniqueTuples
+   * @param cntTuple a list representation of a tuple to compare
+   * @return true if equals.
+   * */
+  private boolean replace(final int index, final List<Object> cntTuple) {
+    Type t = uniqueTuples.getSchema().getColumnType(valueColIndex);
+    switch (t) {
+      case INT_TYPE:
+        return ((Integer) cntTuple.get(valueColIndex)) < ((Integer) uniqueTuples.get(valueColIndex, index));
+      case FLOAT_TYPE:
+        return ((Float) cntTuple.get(valueColIndex)) < ((Float) uniqueTuples.get(valueColIndex, index));
+      case DOUBLE_TYPE:
+        return ((Double) cntTuple.get(valueColIndex)) < ((Double) uniqueTuples.get(valueColIndex, index));
+      case LONG_TYPE:
+        return ((Long) cntTuple.get(valueColIndex)) < ((Long) uniqueTuples.get(valueColIndex, index));
+      default:
+        throw new IllegalStateException("type " + t + " is not supported in KeepMinValue.replace()");
+    }
+  }
+
+  /**
    * Do duplicate elimination for tb.
    * 
    * @param tb the TupleBatch for performing DupElim.
    * @return the duplicate eliminated TB.
    * */
-  protected TupleBatch doDupElim(final TupleBatch tb) {
+  protected TupleBatch keepMinValue(final TupleBatch tb) {
     final int numTuples = tb.numTuples();
     if (numTuples <= 0) {
       return tb;
@@ -79,7 +118,7 @@ public final class DupElim extends StreamingStateUpdater {
         cntTuple.add(tb.getObject(j, i));
       }
       final int nextIndex = uniqueTuples.numTuples();
-      final int cntHashCode = tb.hashCode(i);
+      final int cntHashCode = tb.hashCode(i, keyColIndices);
       List<Integer> tupleIndexList = uniqueTupleIndices.get(cntHashCode);
       if (tupleIndexList == null) {
         for (int j = 0; j < tb.numColumns(); ++j) {
@@ -94,6 +133,11 @@ public final class DupElim extends StreamingStateUpdater {
       for (final int oldTupleIndex : tupleIndexList) {
         if (tupleEquals(oldTupleIndex, cntTuple)) {
           unique = false;
+          if (replace(oldTupleIndex, cntTuple)) {
+            uniqueTuples.replace(valueColIndex, oldTupleIndex, cntTuple.get(valueColIndex));
+          } else {
+            toRemove.set(i);
+          }
           break;
         }
       }
@@ -102,8 +146,6 @@ public final class DupElim extends StreamingStateUpdater {
           uniqueTuples.put(j, cntTuple.get(j));
         }
         tupleIndexList.add(nextIndex);
-      } else {
-        toRemove.set(i);
       }
     }
     return tb.remove(toRemove);
@@ -122,7 +164,7 @@ public final class DupElim extends StreamingStateUpdater {
 
   @Override
   public TupleBatch update(final TupleBatch tb) {
-    TupleBatch newtb = doDupElim(tb);
+    TupleBatch newtb = keepMinValue(tb);
     if (newtb.numTuples() > 0) {
       return newtb;
     }
