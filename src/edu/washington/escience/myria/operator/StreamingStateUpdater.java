ca93907c0d5a8c7a16f88de540cbee42aaf6f5ca
jingjingwang <jwang@cs.washington.edu>
2013-09-18 12:21:27 -0700

    Add StreamingAggregate and StreamingStateUpdater.
    A StreamingStateUpdater should maintain its internal states while keep
    processing incoming tuple batches. It has an update() method, which
    should be called by a StreamingAggregate. A StreamingAggregate takes a StreamingStateUpdater and sends
    tuples to the updater by calling its update() method.

---
+++ src/edu/washington/escience/myria/operator/StreamingStateUpdater.java
@@ -0,0 +1,52 @@
+package edu.washington.escience.myria.operator;
+
+import java.io.Serializable;
+
+import com.google.common.collect.ImmutableMap;
+
+import edu.washington.escience.myria.Schema;
+import edu.washington.escience.myria.TupleBatch;
+
+/**
+ * */
+public abstract class StreamingStateUpdater implements Serializable {
+  /** Required for Java serialization. */
+  private static final long serialVersionUID = 1L;
+
+  /** the operator that it belongs to. */
+  protected Operator op;
+
+  /**
+   * set its attached operator for inferring schema.
+   * 
+   * @param op the operator that it's attached on.
+   */
+  public void setAttachedOperator(final Operator op) {
+    this.op = op;
+  }
+
+  /**
+   * initialization, as what we have in Operator.
+   * 
+   * @param execEnvVars environment variables.
+   */
+  public abstract void init(final ImmutableMap<String, Object> execEnvVars);
+
+  /**
+   * cleanup, as what we have in Operator.
+   */
+  public abstract void cleanup();
+
+  /**
+   * takes a TB, updates its internel states, and return a TB if applicable.
+   * 
+   * @param tb the input tuple batch.
+   * @return the generated tuple batch.
+   * */
+  public abstract TupleBatch update(TupleBatch tb);
+
+  /**
+   * @return its output schema.
+   */
+  public abstract Schema getSchema();
+}
