c362a24479fe0345c2ae2524c0cab8d215a2a885
Dominik Moritz <domoritz@gmail.com>
2013-10-03 10:31:00 -0700

    Write comments in n-ary operator

--- src/edu/washington/escience/myria/operator/NAryOperator.java
+++ src/edu/washington/escience/myria/operator/NAryOperator.java
@@ -6,13 +6,24 @@ import com.google.common.base.Preconditions;
 
 import edu.washington.escience.myria.Schema;
 
+/**
+ * 
+ * @author dominik
+ * 
+ */
 public abstract class NAryOperator extends Operator {
+
+  /** Required for Java serialization. */
+  private static final long serialVersionUID = 1L;
 
   /**
    * The children of the n-ary operator.
    * */
   protected Operator[] children;
 
+  /**
+   * Default, empty constructor.
+   */
   public NAryOperator() {
     super();
   }
