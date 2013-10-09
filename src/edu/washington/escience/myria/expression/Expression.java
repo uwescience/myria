7af7018abd3a1c97673e8a83dee0d897799bae7d
Dominik Moritz <domoritz@gmail.com>
2013-10-09 15:18:15 -0700

    Stub methods in Expression

--- src/edu/washington/escience/myria/expression/Expression.java
+++ src/edu/washington/escience/myria/expression/Expression.java
@@ -10,11 +10,58 @@ import edu.washington.escience.myria.TupleBatch;
  * 
  */
 public class Expression {
-  private String outputName;
-  private String javaExpression;
-  List<Variable> indexes;
+  /**
+   * Name of the column that the result will be written to.
+   */
+  private final String outputName;
 
-  public Object eval(TupleBatch tb, int rowId) {
+  /**
+   * The java expression to be evaluated in {@link #eval}.
+   */
+  private final String javaExpression;
+
+  /**
+   * List of variables used in {@link #javaExpression}.
+   */
+  private final List<Variable> indexes;
+
+  /**
+   * Constructs the Expression object.
+   * 
+   * @param outputName the name of the resulting element
+   * @param javaExpression the expression to be evaluated
+   * @param indexes variables that are used in the javaExpression
+   */
+  public Expression(final String outputName, final String javaExpression, final List<Variable> indexes) {
+    this.outputName = outputName;
+    this.javaExpression = javaExpression;
+    this.indexes = indexes;
+
+    compile();
+  }
+
+  /**
+   * Compiles the {@link #javaExpression}.
+   */
+  private void compile() {
+    // TODO
+  }
+
+  /**
+   * Evaluates the expression.
+   * 
+   * @param tb a tuple batch
+   * @param rowId the row that should be used for input data
+   * @return the result from the evaluation
+   */
+  public Object eval(final TupleBatch tb, final int rowId) {
     return null;
+  }
+
+  /**
+   * @return the output name
+   */
+  public String getOutputName() {
+    return outputName;
   }
 }
