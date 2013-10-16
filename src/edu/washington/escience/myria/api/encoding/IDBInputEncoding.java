7e2fa8646967b4bcad1e8b5d58f295d5ea38b35f
Daniel Halperin <dhalperi@cs.washington.edu>
2013-10-16 12:45:19 -0700

    More serialization fixes for ExchangePairID and IDBInputEncoding
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- src/edu/washington/escience/myria/api/encoding/IDBInputEncoding.java
+++ src/edu/washington/escience/myria/api/encoding/IDBInputEncoding.java
@@ -3,6 +3,7 @@ package edu.washington.escience.myria.api.encoding;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableList;
 
 import edu.washington.escience.myria.operator.IDBInput;
@@ -11,12 +12,19 @@ import edu.washington.escience.myria.parallel.ExchangePairID;
 import edu.washington.escience.myria.parallel.Server;
 
 public class IDBInputEncoding extends OperatorEncoding<IDBInput> {
+  @JsonProperty
   public Integer argSelfIdbId;
+  @JsonProperty
   public String argControllerOperatorId;
+  @JsonProperty
   public String argInitialInput;
+  @JsonProperty
   public String argIterationInput;
+  @JsonProperty
   public String argEosControllerInput;
+  @JsonProperty
   public ExchangePairID realControllerOperatorId;
+  @JsonProperty
   public Integer realControllerWorkerId;
   private static final List<String> requiredArguments = ImmutableList.of("argSelfIdbId", "argControllerOperatorId",
       "argInitialInput", "argIterationInput", "argEosControllerInput");
