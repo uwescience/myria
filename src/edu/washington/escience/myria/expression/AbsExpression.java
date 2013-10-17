b29a20eaca3fa4d44167d0893888b1204e1dc4c7
Daniel Halperin <dhalperi@cs.washington.edu>
2013-10-17 10:04:33 -0700

    Apply: add AbsExpression and test
    Also make test slightly simplier (i vs curI) and add missing
    verification of data output by "third".
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

---
+++ src/edu/washington/escience/myria/expression/AbsExpression.java
@@ -0,0 +1,62 @@
+package edu.washington.escience.myria.expression;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+
+import edu.washington.escience.myria.Schema;
+import edu.washington.escience.myria.Type;
+
+/**
+ * Take the absolute value of the operand.
+ */
+public class AbsExpression extends UnaryExpression {
+  /***/
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * This is not really unused, it's used automagically by Jackson deserialization.
+   */
+  @SuppressWarnings("unused")
+  private AbsExpression() {
+    super();
+  }
+
+  /**
+   * Take the absolute value of the operand.
+   * 
+   * @param operand the operand.
+   */
+  public AbsExpression(final ExpressionOperator operand) {
+    super(operand);
+  }
+
+  @Override
+  public Type getOutputType(final Schema schema) {
+    Type operandType = getOperand().getOutputType(schema);
+    ImmutableList<Type> validTypes = ImmutableList.of(Type.DOUBLE_TYPE, Type.FLOAT_TYPE, Type.LONG_TYPE, Type.INT_TYPE);
+    int operandIdx = validTypes.indexOf(operandType);
+    Preconditions.checkArgument(operandIdx != -1, "AbsExpression cannot handle operand [%s] of Type %s", getOperand(),
+        operandType);
+    return validTypes.get(operandIdx);
+  }
+
+  @Override
+  public String getJavaString(final Schema schema) {
+    return getFunctionCallUnaryString("Math.abs", schema);
+  }
+
+  @Override
+  public int hashCode() {
+    return defaultHashCode();
+  }
+
+  @Override
+  public boolean equals(final Object other) {
+    if (other == null || !(other instanceof AbsExpression)) {
+      return false;
+    }
+    AbsExpression otherExpr = (AbsExpression) other;
+    return Objects.equal(getOperand(), otherExpr.getOperand());
+  }
+}
--- test/edu/washington/escience/myria/operator/apply/ApplyTest.java
+++ test/edu/washington/escience/myria/operator/apply/ApplyTest.java
@@ -1,7 +1,6 @@
 package edu.washington.escience.myria.operator.apply;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -13,6 +12,7 @@ import edu.washington.escience.myria.TupleBatch;
 import edu.washington.escience.myria.TupleBatchBuffer;
 import edu.washington.escience.myria.Type;
 import edu.washington.escience.myria.api.encoding.ExpressionEncoding;
+import edu.washington.escience.myria.expression.AbsExpression;
 import edu.washington.escience.myria.expression.ConstantExpression;
 import edu.washington.escience.myria.expression.Expression;
 import edu.washington.escience.myria.expression.ExpressionOperator;
@@ -31,7 +31,7 @@ public class ApplyTest {
   private final int NUM_TUPLES = 2 * TupleBatch.BATCH_SIZE;
 
   @Test
-  public void testApplySqrt() throws DbException {
+  public void testApply() throws DbException {
     final Schema schema =
         new Schema(ImmutableList.of(Type.LONG_TYPE, Type.LONG_TYPE, Type.INT_TYPE, Type.STRING_TYPE), ImmutableList.of(
             "a", "b", "c", "d"));
@@ -95,6 +95,15 @@ public class ApplyTest {
       expressions.add(exprEnc.construct());
     }
 
+    {
+      // Expression: Math.abs(b-a)
+
+      ExpressionOperator abs = new AbsExpression(new MinusExpression(varb, vara));
+
+      ExpressionEncoding exprEnc = new ExpressionEncoding("fifth", abs);
+      expressions.add(exprEnc.construct());
+    }
+
     Apply apply = new Apply(new TupleSource(tbb), expressions.build());
 
     apply.open(null);
@@ -103,22 +112,29 @@ public class ApplyTest {
     while (!apply.eos()) {
       result = apply.nextReady();
       if (result != null) {
-        assertEquals(4, result.getSchema().numColumns());
+        assertEquals(5, result.getSchema().numColumns());
         assertEquals(Type.DOUBLE_TYPE, result.getSchema().getColumnType(0));
         assertEquals(Type.LONG_TYPE, result.getSchema().getColumnType(1));
         assertEquals(Type.DOUBLE_TYPE, result.getSchema().getColumnType(2));
         assertEquals(Type.STRING_TYPE, result.getSchema().getColumnType(3));
+        assertEquals(Type.LONG_TYPE, result.getSchema().getColumnType(4));
 
         assertEquals("first", result.getSchema().getColumnName(0));
         assertEquals("second", result.getSchema().getColumnName(1));
         assertEquals("third", result.getSchema().getColumnName(2));
         assertEquals("fourth", result.getSchema().getColumnName(3));
-        for (int i = 0; i < result.numTuples(); i++) {
-          assertEquals(i + resultSize, result.getDouble(0, i), 0.0000001);
-          long b = i + resultSize + 1;
-          int c = i + resultSize;
-          assertEquals((b + c) * (b - c), result.getLong(1, i));
-          assertTrue(("FOO" + (i + resultSize)).equals(result.getString(3, i)));
+        assertEquals("fifth", result.getSchema().getColumnName(4));
+        for (int curI = 0; curI < result.numTuples(); curI++) {
+          long i = curI + resultSize;
+          assertEquals(i, result.getDouble(0, curI), 0.0000001);
+          long a = (long) Math.pow(i, 2);
+          long b = i + 1;
+          int c = (int) i;
+          String d = ("Foo" + i).toUpperCase();
+          assertEquals((b + c) * (b - c), result.getLong(1, curI));
+          assertEquals(Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)), result.getDouble(2, curI), 0.0000001);
+          assertEquals(d, result.getString(3, curI));
+          assertEquals(Math.abs(b - a), result.getLong(4, curI));
         }
         resultSize += result.numTuples();
       }
