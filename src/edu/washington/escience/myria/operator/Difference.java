727febed9c8f93ec058a28e040ee733f82759407
Andrew Whitaker <whitaker@cs.washington.edu>
2013-11-18 12:39:50 -0800

    Scaffolding for set difference operator

---
+++ src/edu/washington/escience/myria/operator/Difference.java
@@ -0,0 +1,37 @@
+package edu.washington.escience.myria.operator;
+
+import edu.washington.escience.myria.Schema;
+import edu.washington.escience.myria.TupleBatch;
+
+/**
+ * Implementation of set difference. Duplicates are not preserved.
+ * 
+ * @author whitaker
+ */
+public class Difference extends BinaryOperator {
+
+  /**
+   * Instantiate a set difference operator.
+   * 
+   * @param left the operator being subtracted from.
+   * @param right the operator to be subtracted.
+   */
+  public Difference(final Operator left, final Operator right) {
+    super(left, right);
+  }
+
+  /** Required for Java serialization. */
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected TupleBatch fetchNextReady() throws Exception {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Schema generateSchema() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}
