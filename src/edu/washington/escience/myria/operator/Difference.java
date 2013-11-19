d0c370a4d6f0d1475b924320d8d32397d19ab8d4
Andrew Whitaker <whitaker@cs.washington.edu>
2013-11-18 16:44:39 -0800

    Add processing for right tuple batch

--- src/edu/washington/escience/myria/operator/Difference.java
+++ src/edu/washington/escience/myria/operator/Difference.java
@@ -1,10 +1,18 @@
 package edu.washington.escience.myria.operator;
 
+import java.util.List;
+
 import com.google.common.collect.ImmutableMap;
 
 import edu.washington.escience.myria.DbException;
 import edu.washington.escience.myria.Schema;
 import edu.washington.escience.myria.TupleBatch;
+import edu.washington.escience.myria.TupleBuffer;
+import edu.washington.escience.myria.column.Column;
+import gnu.trove.list.TIntList;
+import gnu.trove.list.array.TIntArrayList;
+import gnu.trove.map.TIntObjectMap;
+import gnu.trove.map.hash.TIntObjectHashMap;
 
 /**
  * Implementation of set difference. Duplicates are not preserved.
@@ -17,6 +25,16 @@ public class Difference extends BinaryOperator {
   private static final long serialVersionUID = 1L;
 
   /**
+   * This buffer stores tuples to remove from the left operator.
+   */
+  private transient TupleBuffer tuplesToRemove = null;
+
+  /**
+   * Mapping from tuple hash code to indices in the tuplesToRemove buffer.
+   * */
+  private transient TIntObjectMap<TIntList> tupleIndices;
+
+  /**
    * Instantiate a set difference operator.
    * 
    * @param left the operator being subtracted from.
@@ -26,13 +44,57 @@ public class Difference extends BinaryOperator {
     super(left, right);
   }
 
-  private TupleBatch processLeftChildTB(TupleBatch leftTB) {
-    // TODO Auto-generated method stub
+  private TupleBatch processLeftChildTB(final TupleBatch leftTB) {
     return null;
   }
 
-  private void processRightChildTB(TupleBatch rightTB) {
+  /**
+   * Mark a particular tuple as seen.
+   * 
+   * @param batch A tuple batch
+   * @param rowNum The index of the tuple among the valid tuples in batch.
+   * 
+   * @return true if this is the first time this tuple has been encountered.
+   */
+  private boolean markAsSeen(final TupleBatch batch, final int rowNum) {
+    final int tupleHash = batch.hashCode(rowNum);
 
+    TIntList tupleIndexList = tupleIndices.get(tupleHash);
+
+    if (tupleIndexList == null) {
+      tupleIndexList = new TIntArrayList();
+      tupleIndices.put(tupleHash, tupleIndexList);
+    }
+
+    // Check whether we've seen this tuple before
+    for (int i = 0; i < tupleIndexList.size(); i++) {
+      if (batch.tupleEquals(rowNum, tuplesToRemove, tupleIndexList.get(i))) {
+        return false;
+      }
+    }
+
+    // This is a new tuple: add it to the toRemove tuple buffer
+    final int nextToRemoveRow = tuplesToRemove.numTuples();
+    final int absoluteRowNum = batch.getValidIndices().get(rowNum);
+    final List<Column<?>> columns = batch.getDataColumns();
+
+    for (int columnNum = 0; columnNum < batch.numColumns(); columnNum++) {
+      tuplesToRemove.put(columnNum, columns.get(columnNum), absoluteRowNum);
+    }
+    tupleIndexList.add(nextToRemoveRow);
+    return true;
+  }
+
+  /**
+   * Process a batch of tuples that are removed from the final result.
+   * 
+   * @param batch A tuple batch
+   */
+  private void processRightChildTB(final TupleBatch batch) {
+    final int numValidTuples = batch.numTuples();
+    for (int row = 0; row < numValidTuples; row++) {
+      markAsSeen(batch, row);
+    }
   }
 
   @Override
@@ -70,9 +132,13 @@ public class Difference extends BinaryOperator {
 
   @Override
   public void init(final ImmutableMap<String, Object> execEnvVars) throws DbException {
+    tupleIndices = new TIntObjectHashMap<TIntList>();
+    tuplesToRemove = new TupleBuffer(getSchema());
   }
 
   @Override
   protected void cleanup() throws DbException {
+    tuplesToRemove = null;
+    tupleIndices = null;
   }
 }
