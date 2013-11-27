53940c5d8840ecfdefbcddd5b42287857a8ed10c
Daniel Halperin <dhalperi@cs.washington.edu>
2013-11-27 11:34:23 -0800

    myria: add io package with DataSource interface
    DataSource is an interface for any source of data. The key property it
    has is that it provides an input stream.
    
    Add three implementations, namely ByteArraySource, InputStreamSource,
    and UriSource. These wrap a byte[], a pre-existing InputStream, and a
    String (URI) respectively.
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

---
+++ src/edu/washington/escience/myria/io/DataSource.java
@@ -0,0 +1,26 @@
+package edu.washington.escience.myria.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+/**
+ * An interface for any source of bits. This interface should be the principal parameter to any operator that produces
+ * tuples from input data, e.g., files, the web, or other.
+ */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "data_type")
+@JsonSubTypes({
+    @Type(name = "Bytes", value = ByteArraySource.class), @Type(name = "File", value = FileSource.class),
+    @Type(name = "URI", value = UriSource.class), })
+public interface DataSource {
+  /**
+   * Returns an {@link InputStream} providing read access to the bits in the specified data source.
+   * 
+   * @return an {@link InputStream} providing read access to the bits in the specified data source.
+   * @throws IOException if there is an error producing the input stream.
+   */
+  InputStream getInputStream() throws IOException;
+}
