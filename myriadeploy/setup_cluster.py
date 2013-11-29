e8ad4f9a6524ce781147981b00d798e24588ece8
Shumo Chu <shumo.chu@gmail.com>
2013-11-28 19:00:00 -0800

    Clean up deployment python program
    * make `update_jar_only.py` update other necessary files as well
    * clean used variable/import in `setup_cluster.py`

--- myriadeploy/setup_cluster.py
+++ myriadeploy/setup_cluster.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python
 
-import myriadeploy
-
 import subprocess
 import sys
+
 
 def make_catalog(config_file):
     """Creates a Myria catalog (running the Java program to do so) from the
 given deployment configuration."""
     args = ["./run_catalog_maker.sh", config_file]
     if subprocess.call(args):
-        raise Exception("Error making the catalogs");
+        raise Exception("Error making the catalogs")
+
 
 def copy_catalogs(config_file):
     """Copies the master and worker catalogs to the remote hosts."""
     args = ["./using_deployment_utils.sh", config_file, "-copy_master_catalog"]
     if subprocess.call(args):
-        raise Exception("Error copying master catalog");
+        raise Exception("Error copying master catalog")
     args = ["./using_deployment_utils.sh", config_file, "-copy_worker_catalogs"]
     if subprocess.call(args):
-        raise Exception("Error copying worker catalogs");
+        raise Exception("Error copying worker catalogs")
+
 
 def copy_distribution(config_file):
     "Copy the distribution (jar and libs and conf) to compute nodes."
     args = ["./using_deployment_utils.sh", config_file, "-copy_distribution"]
     if subprocess.call(args):
-        raise Exception("Error copying distribution to %s" % (hostname,))
+        raise Exception("Error copying distribution")
+
 
 def main(argv):
     # Usage
     if len(argv) != 2:
         print >> sys.stderr, "Usage: %s <deployment.cfg>" % (argv[0])
-        print >> sys.stderr, "       deployment.cfg: a configuration file modeled after deployment.cfg.sample"
+        print >> sys.stderr, \
+            "       deployment.cfg: \
+            a configuration file modeled after deployment.cfg.sample"
         sys.exit(1)
 
     # Step 1: make the Catalog
--- myriadeploy/update_myria_jar_only.py
+++ myriadeploy/update_myria_jar_only.py
@@ -1,53 +1,27 @@
 #!/usr/bin/env python
 
-import myriadeploy
-
 import subprocess
 import sys
 
-def host_port_list(workers):
-    return [str(worker[0]) + ':' + str(worker[1]) for worker in workers]
 
-def get_host_port_path(node, default_path):
-    if len(node) == 2:
-        (hostname, port) = node
-        if default_path is None:
-            raise Exception("Path not specified for node %s" % str(node))
-        else:
-            path = default_path
-    else:
-        (hostname, port, path) = node
-    return (hostname, port, path)
-
-def copy_distribution(config):
+def copy_distribution(config_file):
     "Copy the distribution (jar and libs and conf) to compute nodes."
-    nodes = config['nodes']
-    description = config['description']
-    default_path = config['path']
-    username = config['username']
+    args = ["./using_deployment_utils.sh", config_file, "-copy_distribution"]
+    if subprocess.call(args):
+        raise Exception("Error copying distribution")
 
-    for node in nodes:
-        (hostname, _, path) = get_host_port_path(node, default_path)
-        if hostname != 'localhost':
-            remote_path = "%s@%s:%s/%s-files" % (username, hostname, path, description)
-        else:
-            remote_path = "%s/%s-files" % (path, description)
-        to_copy = ["libs", "conf"]
-        args = ["rsync", "--del", "-rLDvz"] + to_copy + [remote_path]
-        if subprocess.call(args):
-            raise Exception("Error copying distribution to %s" % (hostname,))
 
 def main(argv):
     # Usage
     if len(argv) != 2:
         print >> sys.stderr, "Usage: %s <deployment.cfg>" % (argv[0])
-        print >> sys.stderr, "       deployment.cfg: a configuration file modeled after deployment.cfg.sample"
+        print >> sys.stderr, \
+            "       deployment.cfg: \
+            a configuration file modeled after deployment.cfg.sample"
         sys.exit(1)
 
-    config = myriadeploy.read_config_file(argv[1])
-
     # Step 1: Copy over libs, "conf", myria
-    copy_distribution(config)
+    copy_distribution(argv[1])
 
 if __name__ == "__main__":
     main(sys.argv)
