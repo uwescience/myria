8fc81f6d34ab7ed17889c0a3ef1e14423dcabdb9
Daniel Halperin <dhalperi@cs.washington.edu>
2014-01-29 17:11:03 -0800

    JdbcAccessMethodTest: add a postgres test when Travis is running.
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- systemtest/edu/washington/escience/myria/accessmethod/JdbcAccessMethodTest.java
+++ systemtest/edu/washington/escience/myria/accessmethod/JdbcAccessMethodTest.java
@@ -5,7 +5,10 @@ package edu.washington.escience.myria.accessmethod;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Objects;
+
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
 
 import edu.washington.escience.myria.DbException;
 import edu.washington.escience.myria.MyriaConstants;
@@ -22,36 +25,48 @@ import edu.washington.escience.myria.util.TestUtils;
  * @author dhalperi
  * 
  */
-public class MysqlJdbcAccessMethodTest {
+public class JdbcAccessMethodTest {
+  /** The logger for this class. */
+  protected static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(JdbcAccessMethod.class);
+
   private static final String MYSQL_DRIVER_CLASS = "com.mysql.jdbc.Driver";
   private static final int MYSQL_PORT = 3306;
   private static final String MYSQL_DATABASE_NAME = "myria_test";
+  private static final String POSTGRES_DRIVER_CLASS = "org.postgresql.Driver";
+  private static final int POSTGRES_PORT = 5432;
+  private static final String POSTGRES_DATABASE_NAME = "myria_test";
 
-  private JdbcInfo getJdbcInfo() {
+  private JdbcInfo getJdbcInfo(final String dbms) {
     if (TestUtils.inTravis()) {
       /* Return localhost using Travis' default credentials. */
-      return JdbcInfo.of(MYSQL_DRIVER_CLASS, MyriaConstants.STORAGE_SYSTEM_MYSQL, "localhost", MYSQL_PORT,
-          MYSQL_DATABASE_NAME, "travis", "");
-    } else {
-      /* Return the MyriaTest AWS instance */
-      final String host = "54.213.118.143";
-      final String user = "myria";
-      final String password = "nays26[shark";
-      final String dbms = "mysql";
-      return JdbcInfo.of(MYSQL_DRIVER_CLASS, dbms, host, MYSQL_PORT, MYSQL_DATABASE_NAME, user, password);
+      if (dbms.equals(MyriaConstants.STORAGE_SYSTEM_POSTGRESQL)) {
+        return JdbcInfo.of(POSTGRES_DRIVER_CLASS, MyriaConstants.STORAGE_SYSTEM_POSTGRESQL, "localhost", POSTGRES_PORT,
+            POSTGRES_DATABASE_NAME, "postgres", "");
+      } else {
+        return JdbcInfo.of(MYSQL_DRIVER_CLASS, MyriaConstants.STORAGE_SYSTEM_MYSQL, "localhost", MYSQL_PORT,
+            MYSQL_DATABASE_NAME, "travis", "");
+      }
     }
+
+    /* Return the MyriaTest AWS MySQL instance */
+    final String host = "54.213.118.143";
+    final String user = "myria";
+    final String password = "nays26[shark";
+    return JdbcInfo.of(MYSQL_DRIVER_CLASS, MyriaConstants.STORAGE_SYSTEM_MYSQL, host, MYSQL_PORT, MYSQL_DATABASE_NAME,
+        user, password);
   }
 
-  @Test
-  public void testNumberResultsAndMultipleBatches() throws DbException, InterruptedException {
+  private void testInsertTuplesAndCountThem(final String dbms) throws DbException {
+    Objects.requireNonNull(dbms, "dbms");
+
     /* Connection information */
-    final JdbcInfo jdbcInfo = getJdbcInfo();
+    final JdbcInfo jdbcInfo = getJdbcInfo(dbms);
 
     /* First, insert tuples into the database. */
     final int expectedNumResults = 250;
     TupleRangeSource source = new TupleRangeSource(expectedNumResults, Type.INT_TYPE);
     final Schema schema = source.getSchema();
-    final RelationKey relation = RelationKey.of("myria", "test", "mysql");
+    final RelationKey relation = RelationKey.of("myria", "test", dbms);
     DbInsert insert = new DbInsert(source, relation, jdbcInfo, true);
     /* Run to completion. */
     insert.open(null);
@@ -80,4 +95,17 @@ public class MysqlJdbcAccessMethodTest {
     assertEquals(expectedNumResults, count);
   }
 
+  @Test
+  public void testInsertTuplesAndCountThemMySQL() throws DbException {
+    testInsertTuplesAndCountThem(MyriaConstants.STORAGE_SYSTEM_MYSQL);
+  }
+
+  @Test
+  public void testInsertTuplesAndCountThemPostgreSQL() throws DbException {
+    if (!TestUtils.inTravis()) {
+      LOGGER.warn("Skipping PostgreSQL test since not in Travis.");
+      return;
+    }
+    testInsertTuplesAndCountThem(MyriaConstants.STORAGE_SYSTEM_POSTGRESQL);
+  }
 }
