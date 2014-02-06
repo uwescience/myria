d1de23c6c8ab1e9019c62e4249ff41b5b128def5
Shumo Chu <shumo.chu@gmail.com>
2014-02-06 00:16:10 -0800

    fix the bug that `collect_worker_running_time.py` will be erroneous when the query runs less than 1 hr.

--- visualization/query_execution/collect_worker_running_time.py
+++ visualization/query_execution/collect_worker_running_time.py
@@ -1,39 +1,50 @@
 #!/usr/bin/env python
 
 import re
-import subprocess
 import sys
+import argparse
 
-def collect_worker_time(path, query_id, worker_id):           
 
-    regex_operator = re.compile(r'INFO  \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d* .Nonblocking query executor#\d*. WorkerQueryPartition\$\d* - Query #(\d*) executed for (\d*)h (\d*)m (\d*\.\d+|\d+)s')
+#parse args
+parser = argparse.ArgumentParser(
+    description='collect running time of workers of a query')
+parser.add_argument("log", type=str, help="log file path")
+parser.add_argument("worker_number", type=int, help="number of workers")
+parser.add_argument("query", type=int, help="query id")
+args = parser.parse_args()
 
-    lines = [ line.strip() for line in open(path)]
-    lines = [ line for line in lines if regex_operator.match(line) ]
-    tuples = [ re.findall(r'INFO  \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d* .Nonblocking query executor#\d*. WorkerQueryPartition\$\d* - Query #(\d*) executed for (\d*)h (\d*)m (\d*\.\d+|\d+)s', line) 
-                for line in lines] 
+
+def collect_worker_time(path, query_id, worker_id):
+
+    regex_operator = re.compile(r'INFO  \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d* .Nonblocking query executor#\d*. WorkerQueryPartition\$\d* - Query #(\d*) executed for ([\w\W]*)')
+
+    lines = [line.strip() for line in open(path)]
+    lines = [line for line in lines if regex_operator.match(line)]
+    tuples = [re.findall(r'INFO  \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d* .Nonblocking query executor#\d*. WorkerQueryPartition\$\d* - Query #(\d*) executed for (\d*h )?(\d*m )?(\d*\.\d+|\d+)s', line)
+              for line in lines]
     tuples = [i[0] for i in tuples if i[0][0] == str(query_id)]
-    if len(tuples) == 0: 
-        print >> sys.stderr, "can not find running time of query %d"%query_id
+    if len(tuples) == 0:
+        print >> sys.stderr, "can not find running time of query %d" % query_id
         sys.exit(1)
     elif len(tuples) > 1:
-        print >> sys.stderr, "find running time of query %d more than once"%query_id
+        print >> sys.stderr, "find running time of query %d\
+         more than once" % query_id
         sys.exit(1)
-    print "%d,%f"%(worker_id, int(tuples[0][1])*3600+int(tuples[0][2])*60+float(tuples[0][3]))
+    time = 0.0
+    if tuples[0][1]:
+        time = time + int(tuples[0][1][:-2]) * 3600
+    if tuples[0][2]:
+        time = time + int(tuples[0][2][:-2]) * 60
+    if tuples[0][3]:
+        time = time + float(tuples[0][3])
+    print "%d, %f" % (worker_id, time)
 
-def main(argv):
-# Usage
-    if len(argv) != 4:
-        print >> sys.stderr, "Usage: %s <log_file_path> <# of workers> <query id>" % (argv[0])
-        print >> sys.stderr, "       log_file_path "
-        print >> sys.stderr, "       # of workers "
-        print >> sys.stderr, "       query id "
-        sys.exit(1)
 
+def main():
     print "worker_id,running_time"
-    number_of_workers = int(argv[2])
-    for i in range(1,number_of_workers+1):
-        collect_worker_time(argv[1]+"/worker_%d_stdout"%i,int(argv[3]),i)
+    number_of_workers = args.worker_number
+    for i in range(1, number_of_workers + 1):
+        collect_worker_time(args.log + "/worker_%d_stdout" % i, args.query, i)
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
