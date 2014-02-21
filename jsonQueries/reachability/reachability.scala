bd48790f2bf103971c138353f6feb27905f49780
slxu <slxu@cs.washington.edu>
2014-02-21 13:02:54 -0800

    Fix wrong order of Join result columns
    In reachability.scala

--- jsonQueries/reachability/reachability.scala
+++ jsonQueries/reachability/reachability.scala
@@ -17,13 +17,13 @@ import edu.washington.escience.myria.client.JsonQueryBaseBuilder
 import edu.washington.escience.myria.RelationKey
 import edu.washington.escience.myria.parallel.SingleFieldHashPartitionFunction
 
-val b = new JsonQueryBaseBuilder().workers(Array(1,2,3))
+val b = new JsonQueryBaseBuilder().workers(Array(1,2))
 val pf = new SingleFieldHashPartitionFunction(null,0)
 val a = RelationKey.of("jwang","reachability","a0")
 val g = RelationKey.of("jwang","reachability","g")
 
 val iterateInput = b.scan(a).setName("scan2a").shuffle(pf).setName("shuffle_a").beginIterate()
-b.scan(g).setName("scan2g").shuffle(pf).setName("shuffle_2g").hashEquiJoin(iterateInput,Array(0), Array(), Array(0), Array(1)).setName("join").shuffle(pf).setName("shuffle_j").endIterate(iterateInput)
+b.scan(g).setName("scan2g").shuffle(pf).setName("shuffle_2g").hashEquiJoin(iterateInput,Array(0), Array(1), Array(0), Array()).setName("join").shuffle(pf).setName("shuffle_j").endIterate(iterateInput)
 val finalResult = iterateInput.count().setName("count").masterCollect
 print(finalResult.buildJson())
 
