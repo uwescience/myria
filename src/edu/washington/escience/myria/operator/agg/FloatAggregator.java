c98c22511694139f37dfa27d44ac165d0ada8917
Daniel Halperin <dhalperi@cs.washington.edu>
2014-03-27 10:48:10 -0700

    Float,DoubleAggregator: cleanup

--- src/edu/washington/escience/myria/operator/agg/FloatAggregator.java
+++ src/edu/washington/escience/myria/operator/agg/FloatAggregator.java
@@ -56,6 +56,7 @@ public final class FloatAggregator implements Aggregator<Float> {
    * @param aggOps the aggregate operation to simultaneously compute.
    * */
   public FloatAggregator(final String aFieldName, final int aggOps) {
+    Objects.requireNonNull(aFieldName, "aFieldName");
     if (aggOps <= 0) {
       throw new IllegalArgumentException("No aggregation operations are selected");
     }
@@ -99,8 +100,54 @@ public final class FloatAggregator implements Aggregator<Float> {
   }
 
   @Override
+  public void add(final Float value) {
+    addFloat(Objects.requireNonNull(value, "value"));
+  }
+
+  @Override
+  public void add(final ReadableColumn from) {
+    Objects.requireNonNull(from, "from");
+    final int numTuples = from.size();
+    if (numTuples == 0) {
+      return;
+    }
+
+    if (AggUtils.needsCount(aggOps)) {
+      count = LongMath.checkedAdd(count, numTuples);
+    }
+
+    if (!AggUtils.needsStats(aggOps)) {
+      return;
+    }
+    for (int i = 0; i < numTuples; i++) {
+      addFloatStats(from.getFloat(i));
+    }
+  }
+
+  @Override
   public void add(final ReadableTable from, final int fromColumn) {
+    Objects.requireNonNull(from, "from");
     add(from.asColumn(fromColumn));
+  }
+
+  @Override
+  public void add(final ReadableTable table, final int column, final int row) {
+    Objects.requireNonNull(table, "table");
+    addFloat(table.getFloat(column, row));
+  }
+
+  /**
+   * Add the specified value to this aggregator.
+   * 
+   * @param value the value to be added
+   */
+  public void addFloat(final float value) {
+    if (AggUtils.needsCount(aggOps)) {
+      count = LongMath.checkedAdd(count, 1);
+    }
+    if (AggUtils.needsStats(aggOps)) {
+      addFloatStats(value);
+    }
   }
 
   /**
@@ -124,26 +171,8 @@ public final class FloatAggregator implements Aggregator<Float> {
   }
 
   @Override
-  public void add(final Float value) {
-    addFloat(Objects.requireNonNull(value, "value"));
-  }
-
-  /**
-   * Add the specified value to this aggregator.
-   * 
-   * @param value the value to be added
-   */
-  public void addFloat(final float value) {
-    if (AggUtils.needsCount(aggOps)) {
-      count = LongMath.checkedAdd(count, 1);
-    }
-    if (AggUtils.needsStats(aggOps)) {
-      addFloatStats(value);
-    }
-  }
-
-  @Override
   public void getResult(final AppendableTable dest, final int destColumn) {
+    Objects.requireNonNull(dest, "dest");
     int idx = destColumn;
     if ((aggOps & AGG_OP_COUNT) != 0) {
       dest.putLong(idx, count);
@@ -178,31 +207,7 @@ public final class FloatAggregator implements Aggregator<Float> {
   }
 
   @Override
-  public void add(final ReadableTable table, final int column, final int row) {
-    addFloat(table.getFloat(column, row));
-  }
-
-  @Override
   public Type getType() {
     return Type.FLOAT_TYPE;
-  }
-
-  @Override
-  public void add(final ReadableColumn from) {
-    final int numTuples = from.size();
-    if (numTuples == 0) {
-      return;
-    }
-
-    if (AggUtils.needsCount(aggOps)) {
-      count = LongMath.checkedAdd(count, numTuples);
-    }
-
-    if (!AggUtils.needsStats(aggOps)) {
-      return;
-    }
-    for (int i = 0; i < numTuples; i++) {
-      addFloatStats(from.getFloat(i));
-    }
   }
 }
