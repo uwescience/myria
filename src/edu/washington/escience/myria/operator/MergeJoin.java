0034c71583540b53fe0afac32a10e2aa6990ce63
Dominik Moritz <domoritz@gmail.com>
2014-04-01 17:57:58 -0700

    Refactor merge join.

--- src/edu/washington/escience/myria/operator/MergeJoin.java
+++ src/edu/washington/escience/myria/operator/MergeJoin.java
@@ -333,7 +333,7 @@ public final class MergeJoin extends BinaryOperator {
       } else {
         leftIsLess();
       }
-      nexttb = ans.popAnyUsingTimeout();
+      nexttb = ans.popFilled();
       if (nexttb != null) {
         return nexttb;
       }
@@ -455,16 +455,18 @@ public final class MergeJoin extends BinaryOperator {
 
     final boolean atLast = leftRowIndex == leftBatches.getLast().numTuples() - 1;
     if (atLast) {
-      if (!left.eos() && leftNotProcessed == null) {
-        TupleBatch tb = left.nextReady();
-        if (tb != null) {
-          leftNotProcessed = tb;
-        } else if (left.eos()) {
-          deferredEOS = true;
-        }
-      }
       if (leftNotProcessed != null) {
         leftMoveFromNotProcessed();
+      } else {
+        if (!left.eos()) {
+          TupleBatch tb = left.nextReady();
+          if (tb != null) {
+            leftNotProcessed = tb;
+          }
+        }
+        if (left.eos()) {
+          deferredEOS = true;
+        }
       }
     } else {
       leftRowIndex++;
@@ -480,16 +482,18 @@ public final class MergeJoin extends BinaryOperator {
 
     final boolean atLast = rightRowIndex == rightBatches.getLast().numTuples() - 1;
     if (atLast) {
-      if (!right.eos() && rightNotProcessed == null) {
-        TupleBatch tb = right.nextReady();
-        if (tb != null) {
-          rightNotProcessed = tb;
-        } else if (right.eos()) {
-          deferredEOS = true;
-        }
-      }
       if (rightNotProcessed != null) {
         rightMoveFromNotProcessed();
+      } else {
+        if (!right.eos()) {
+          TupleBatch tb = right.nextReady();
+          if (tb != null) {
+            rightNotProcessed = tb;
+          }
+        }
+        if (right.eos()) {
+          deferredEOS = true;
+        }
       }
     } else {
       rightRowIndex++;
