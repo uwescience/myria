884d1eee78383365419821f2d4d6e48755ff68c4
Daniel Halperin <dhalperi@cs.washington.edu>
2014-04-03 00:07:44 -0700

    SingletonRelationTest: assert that SingletonRelation is a singleton

---
+++ test/edu/washington/escience/myria/operator/SingletonRelationTest.java
@@ -0,0 +1,29 @@
+package edu.washington.escience.myria.operator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import edu.washington.escience.myria.DbException;
+import edu.washington.escience.myria.storage.TupleBatch;
+import edu.washington.escience.myria.util.TestEnvVars;
+
+public class SingletonRelationTest {
+
+  @Test
+  public void test() throws DbException {
+    SingletonRelation singleton = new SingletonRelation();
+    singleton.open(TestEnvVars.get());
+    long count = 0;
+    while (!singleton.eos()) {
+      TupleBatch tb = singleton.nextReady();
+      if (tb == null) {
+        continue;
+      }
+      count += tb.numTuples();
+    }
+    singleton.close();
+    assertEquals(1, count);
+  }
+
+}
