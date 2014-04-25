ec5ee8ad7404e49ab42062cda87a8faefacdee0b
Daniel Halperin <dhalperi@cs.washington.edu>
2014-04-25 01:02:00 -0700

    update myria version and branch every compile
    Only works for gradle compiles.

---
+++ update_commit_id.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cur_commit="$(git rev-parse HEAD)"
+cur_branch="$(git rev-parse --abbrev-ref HEAD)"
+
+echo "package edu.washington.escience.myria;
+
+/**
+ * Version info about the source code at build time.
+ */
+public final class MyriaCommit {
+  /** Utility class cannot be constructed. */
+  private MyriaCommit() {
+  }
+
+  /** The git commit id at build time. */
+  public static final String COMMIT = \"$cur_commit\";
+  /** The git branch at build time. */
+  public static final String BRANCH = \"$cur_branch\";
+}" > src/edu/washington/escience/myria/MyriaCommit.java
