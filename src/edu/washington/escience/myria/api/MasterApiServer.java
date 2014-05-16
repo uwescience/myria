77d7ead13139de468b72ca6c91045b29c6eb7bf9
jingjingwang <jwang@cs.washington.edu>
2014-05-15 20:16:56 -0700

    add the function to register to the watchdog everytime when master starts

--- src/edu/washington/escience/myria/api/MasterApiServer.java
+++ src/edu/washington/escience/myria/api/MasterApiServer.java
@@ -1,7 +1,12 @@
 package edu.washington.escience.myria.api;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.Inet4Address;
 import java.net.URI;
+import java.net.URL;
+import java.util.Map;
 
 import javax.ws.rs.core.UriBuilder;
 
@@ -12,8 +17,10 @@ import org.slf4j.LoggerFactory;
 import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import com.sun.jersey.api.core.ResourceConfig;
 
+import edu.washington.escience.myria.MyriaSystemConfigKeys;
 import edu.washington.escience.myria.daemon.MasterDaemon;
 import edu.washington.escience.myria.parallel.Server;
+import edu.washington.escience.myria.tool.MyriaConfigurationReader;
 
 /**
  * The main class for the Myria API server.
@@ -41,6 +48,62 @@ public final class MasterApiServer {
     URI baseUri = UriBuilder.fromUri("http://0.0.0.0/").port(port).build();
     ResourceConfig masterApplication = new MasterApplication(server, daemon);
     webServer = GrizzlyServerFactory.createHttpServer(baseUri, masterApplication);
+
+    registerToWatchdog(server, port);
+  }
+
+  /**
+   * Register this deployment to the watchdog.
+   * 
+   * @param server the Myria server that will handle API requests.
+   * @param port the port the Myria API server will listen on.
+   * @throws IOException if registration failed.
+   */
+  public void registerToWatchdog(final Server server, final int port) throws IOException {
+    final String deploymentFile = server.getConfiguration(MyriaSystemConfigKeys.DEPLOYMENT_FILE);
+    final String description = server.getConfiguration(MyriaSystemConfigKeys.DESCRIPTION);
+    final String workingDir =
+        server.getConfiguration(MyriaSystemConfigKeys.WORKING_DIRECTORY) + "/" + description + "-files/" + description;
+    final String masterApiSocket = Inet4Address.getLocalHost().getHostAddress() + ":" + port;
+
+    final Map<String, Map<String, String>> config = new MyriaConfigurationReader().load(deploymentFile);
+    final String watchdog = config.get("deployment").get("watchdog");
+    String secretCode = config.get("deployment").get("secret_code");
+    if (secretCode == null) {
+      secretCode = "";
+    }
+    if (watchdog != null) {
+      if (LOGGER.isTraceEnabled()) {
+        LOGGER.trace("Registering myself to the watchdog " + watchdog);
+      }
+
+      String urlParameters =
+          "master=" + masterApiSocket + "&" + "working_dir=" + workingDir + "&" + "deployment_file=" + deploymentFile
+              + "&" + "secret_code=" + secretCode;
+      HttpURLConnection connection = null;
+      try {
+        URL url = new URL("http://" + watchdog + "/register");
+        connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setRequestProperty("Content-Length", "" + urlParameters.getBytes().length);
+        connection.setUseCaches(false);
+
+        DataOutputStream output = new DataOutputStream(connection.getOutputStream());
+        output.writeBytes(urlParameters);
+        output.flush();
+        output.close();
+        if (connection.getResponseCode() != 200) {
+          throw new RuntimeException("return code: " + connection.getResponseCode());
+        }
+      } catch (Exception e) {
+        LOGGER.error("Cannot POST to the watchdog, cause: " + e);
+      } finally {
+        connection.disconnect();
+      }
+    }
   }
 
   /**
