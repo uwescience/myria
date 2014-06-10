bfde133a2aacfda339d8c7b73aa0b3836adf1713
Daniel Halperin <dhalperi@cs.washington.edu>
2014-06-10 10:20:07 -0700

    Server: fix bug in start time
    We were updating the Query start time every time a new SubQuery was started. Add checks to ExecutionStatistics
    to catch this and also fix the Server to only update the query statistics for the first SubQuery.

--- src/edu/washington/escience/myria/parallel/ExecutionStatistics.java
+++ src/edu/washington/escience/myria/parallel/ExecutionStatistics.java
@@ -3,6 +3,7 @@ package edu.washington.escience.myria.parallel;
 import javax.annotation.Nullable;
 
 import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
 
 import edu.washington.escience.myria.proto.QueryProto;
 import edu.washington.escience.myria.util.DateTimeUtils;
@@ -35,6 +36,7 @@ public class ExecutionStatistics {
    * Set the start time to now.
    */
   public final void markStart() {
+    Preconditions.checkArgument(startTime == null, "can't re-mark start");
     startAtInNano = System.nanoTime();
     startTime = DateTimeUtils.nowInISO8601();
   }
@@ -43,6 +45,7 @@ public class ExecutionStatistics {
    * Set the end time to now.
    */
   public final void markEnd() {
+    Preconditions.checkArgument(endTime == null, "can't re-mark end");
     endAtInNano = System.nanoTime();
     endTime = DateTimeUtils.nowInISO8601();
   }
