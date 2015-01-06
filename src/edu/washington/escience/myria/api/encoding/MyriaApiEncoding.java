604ec19cfe4ace127508d72d549912387706be65
Daniel Halperin <dhalperi@cs.washington.edu>
2015-01-05 16:03:27 -0800

    MyriaApiEncoding: list all missing fields at the same time
    Previously, we would return 1 missing field, then you fix and resubmit
    and get another error. List them all at once. Also clean up some
    redundant code and messages.

--- src/edu/washington/escience/myria/api/encoding/MyriaApiEncoding.java
+++ src/edu/washington/escience/myria/api/encoding/MyriaApiEncoding.java
@@ -9,7 +9,7 @@ import javax.ws.rs.core.Response.Status;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.base.Joiner;
-import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 
 import edu.washington.escience.myria.api.MyriaApiException;
 
@@ -21,16 +21,16 @@ import edu.washington.escience.myria.api.MyriaApiException;
  */
 public abstract class MyriaApiEncoding {
   /**
-   * @return the list of names of required fields.
+   * @return a list of required fields.
    */
   @JsonIgnore
-  private final List<String> getRequiredFields() {
+  private final List<Field> getRequiredFields() {
     Field[] fs = this.getClass().getFields();
-    List<String> requiredFields = new LinkedList<>();
+    List<Field> requiredFields = new LinkedList<>();
     for (Field f : fs) {
       Annotation a = f.getAnnotation(Required.class);
       if (a != null) {
-        requiredFields.add(f.getName());
+        requiredFields.add(f);
       }
     }
     return requiredFields;
@@ -44,22 +44,21 @@ public abstract class MyriaApiEncoding {
    * @throws MyriaApiException if the validation checks fail.
    */
   public final void validate() throws MyriaApiException {
-    /* Fetch the list of required fields. */
-    final List<String> fields = getRequiredFields();
-    String current = "";
+    final List<String> missing = Lists.newLinkedList();
     /* Check using Java reflection to see that every field is there. */
     try {
-      for (final String f : fields) {
-        current = f;
-        Preconditions.checkNotNull(getClass().getField(f).get(this));
+      for (final Field f : getRequiredFields()) {
+        if (null == f.get(this)) {
+          missing.add(f.getName());
+        }
       }
-    } catch (NoSuchFieldException | IllegalAccessException e) {
+    } catch (IllegalAccessException e) {
       throw new MyriaApiException(Status.INTERNAL_SERVER_ERROR, e);
-    } catch (final NullPointerException e1) {
+    }
+    if (!missing.isEmpty()) {
       /* Some field is missing; throw a 400 (BAD REQUEST) exception with the list of required fields. */
-      final StringBuilder sb = new StringBuilder(getClass().getName()).append(" has required fields: ");
-      sb.append(Joiner.on(", ").join(fields));
-      sb.append(". Missing ").append(current);
+      final StringBuilder sb = new StringBuilder(getClass().getName()).append(" is missing required fields: ");
+      sb.append(Joiner.on(", ").join(missing)).append(".\n");
       throw new MyriaApiException(Status.BAD_REQUEST, sb.toString());
     }
     validateExtra();
