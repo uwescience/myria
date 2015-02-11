b81e24b4aa9aaeeaac6d9252d03e62054220d34b
Daniel Halperin <dhalperi@cs.washington.edu>
2015-02-10 22:29:45 -0800

    JsonBadPlanTests: test with duplicate producers

--- systemtest/edu/washington/escience/myria/systemtest/JsonBadPlanTests.java
+++ systemtest/edu/washington/escience/myria/systemtest/JsonBadPlanTests.java
@@ -173,4 +173,49 @@ public class JsonBadPlanTests extends SystemTestBase {
       assertThat(e.getMessage()).contains("Producer 1 only supports a single consumer, not 2");
     }
   }
+
+  @Test
+  public void fragmentDuplicateProducerTest() throws Exception {
+    EmptyRelationEncoding empty = new EmptyRelationEncoding();
+    AbstractProducerEncoding<?> prod = new CollectProducerEncoding();
+    empty.opId = 0;
+    empty.schema = Schema.ofFields("x", Type.LONG_TYPE);
+    prod.opId = 1;
+    prod.argChild = empty.opId;
+    PlanFragmentEncoding prodFrag = PlanFragmentEncoding.of(empty, prod);
+
+    EmptyRelationEncoding empty2 = new EmptyRelationEncoding();
+    AbstractProducerEncoding<?> prod2 = new CollectProducerEncoding();
+    empty2.opId = 2;
+    empty2.schema = Schema.ofFields("x", Type.LONG_TYPE);
+    prod2.opId = 1;
+    prod2.argChild = empty2.opId;
+    PlanFragmentEncoding prodFrag2 = PlanFragmentEncoding.of(empty2, prod2);
+
+    AbstractConsumerEncoding<?> cons1 = new CollectConsumerEncoding();
+    cons1.opId = 4;
+    cons1.argOperatorId = prod.opId;
+    AbstractConsumerEncoding<?> cons2 = new CollectConsumerEncoding();
+    cons2.opId = 5;
+    cons2.argOperatorId = 3;
+    UnionAllEncoding ua = new UnionAllEncoding();
+    ua.argChildren = new Integer[] { cons1.opId, cons2.opId };
+    ua.opId = 6;
+    SinkRootEncoding sink = new SinkRootEncoding();
+    sink.opId = 7;
+    sink.argChild = ua.opId;
+    PlanFragmentEncoding consFrag = PlanFragmentEncoding.of(cons1, cons2, ua, sink);
+
+    QueryEncoding query = new QueryEncoding();
+    query.plan = new SubQueryEncoding(ImmutableList.of(prodFrag, prodFrag2, consFrag));
+    query.logicalRa = "Fragment duplicate producer test";
+    query.rawQuery = query.logicalRa;
+
+    try {
+      submitQuery(query);
+      fail();
+    } catch (IllegalStateException e) {
+      assertThat(e.getMessage()).contains("Two different operators cannot produce the same opId 1.");
+    }
+  }
 }
