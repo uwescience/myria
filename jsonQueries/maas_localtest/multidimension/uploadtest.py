579643136248770805d65d88edc40d64a99a0c1a
Ryan Maas <rwmaas@gmail.com>
2015-03-15 23:43:09 -0700

    fixed uploadtest)

--- jsonQueries/maas_localtest/multidimension/uploadtest.py
+++ jsonQueries/maas_localtest/multidimension/uploadtest.py
@@ -16,7 +16,7 @@ def upload_parallel(filename, workers=2):
 
     work = []
     for i in range(1, workers + 1):
-        work.append((i, 'http://s3-us-west-2.amazonaws.com/myria-sdss/astronomy/partioned/%s/p2/%s' % (name_root, filename + '-part-' + str(i).zfill(5))))
+        work.append((i, 'http://s3-us-west-2.amazonaws.com/myria-sdss/astronomy/partioned/%s/%s/%s' % (name_root, workers,filename + '-part-' + str(i).zfill(5))))
 
 
     #'https://s3-us-west-2.amazonaws.com/myria-sdss/astronomy/partioned/%/p2/%s' % (name_root, filename + '-part-' + str(1).zfill(5))
