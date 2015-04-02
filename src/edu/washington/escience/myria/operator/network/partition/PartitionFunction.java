fe36fdaae964e70090c951cdabda5bfb84c1873d
Dan Radion <dan.radion@gmail.com>
2015-04-02 16:39:46 -0700

    added a partition function that decides based on a raw integer value

--- src/edu/washington/escience/myria/operator/network/partition/PartitionFunction.java
+++ src/edu/washington/escience/myria/operator/network/partition/PartitionFunction.java
@@ -22,6 +22,7 @@ import edu.washington.escience.myria.storage.TupleBatch;
 @JsonSubTypes({
     @Type(value = RoundRobinPartitionFunction.class, name = "RoundRobin"),
     @Type(value = SingleFieldHashPartitionFunction.class, name = "SingleFieldHash"),
+    @Type(value = RawValuePartitionFunction.class, name = "RawValue"),
     @Type(value = MultiFieldHashPartitionFunction.class, name = "MultiFieldHash"),
     @Type(value = WholeTupleHashPartitionFunction.class, name = "WholeTupleHash") })
 public abstract class PartitionFunction implements Serializable {
