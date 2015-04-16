fc436f0f258c434509564b108731a393184b4c46
Dan Radion <dan.radion@gmail.com>
2015-04-15 23:39:41 -0700

    updated sampling encodings to support randomSeed

--- src/edu/washington/escience/myria/api/encoding/SampleWREncoding.java
+++ src/edu/washington/escience/myria/api/encoding/SampleWREncoding.java
@@ -5,8 +5,11 @@ import edu.washington.escience.myria.operator.SampleWR;
 
 public class SampleWREncoding extends BinaryOperatorEncoding<SampleWR> {
 
+  /** Used to make results deterministic. Null if no specified value. */
+  public Long randomSeed;
+
   @Override
   public SampleWR construct(final ConstructArgs args) {
-    return new SampleWR(null, null);
+    return new SampleWR(null, null, randomSeed);
   }
 }
