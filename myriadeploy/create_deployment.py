ad1d1d101f1aa348555292c645fa778298b1f10b
Brandon Haynes <bhaynes@cs.washington.edu>
2015-05-12 08:33:56 -0700

    Added worker directory/database name to parameters; removed 2g heap size default, switched coord/worker default ports

--- myriadeploy/create_deployment.py
+++ myriadeploy/create_deployment.py
@@ -9,19 +9,21 @@ from itertools import groupby
 
 def get_deployment(path, coordinator_hostname, worker_hostnames, name='myria',
                    rest_port=8753, database_type='postgresql',
-                   database_port=5432, heap='2g', debug=False,
+                   database_port=5432, heap=None, debug=False,
                    database_username=None, database_password=None,
                    coordinator_port=9001, worker_ports=None,
-                   worker_base_port=8001):
+                   worker_base_port=8001, worker_directories=None,
+                   worker_databases=None):
     """ Generates a Myria deployment file with the given configuration """
     return (_get_header(path, name, rest_port, database_type, database_port,
                         heap, debug, database_username, database_password) +
             _get_coordinator(coordinator_hostname, coordinator_port) +
-            _get_workers(worker_hostnames, worker_ports, worker_base_port))
+            _get_workers(worker_hostnames, worker_ports, worker_base_port,
+                         worker_directories, worker_databases))
 
 
 def _get_header(path, name='myria', rest_port=8753, database_type='postgresql',
-                database_port=5432, heap='2g', debug=False,
+                database_port=5432, heap=None, debug=False,
                 database_username=None, database_password=None):
     """ Generates the header section of a Myria deployment file """
     header = ('[deployment]\n'
@@ -51,7 +53,8 @@ def _get_coordinator(hostname, port=9001):
            '0 = {}:{}\n\n'.format(hostname, port)
 
 
-def _get_workers(hostnames, ports=None, base_port=8001):
+def _get_workers(hostnames, ports=None, base_port=8001,
+                 directories=None, database_names=None):
     """ Generates the worker section of a Myria deployment file """
     workers = '[workers]\n'
 
@@ -61,9 +64,14 @@ def _get_workers(hostnames, ports=None, base_port=8001):
                  for hostname, group in groupby(hostnames)
                  for offset in xrange(len(list(group)))]
 
-    # TODO: add support for specifying different directories and database names for workers
-    for index, (hostname, port) in enumerate(zip(hostnames, ports)):
-        workers += '{} = {}:{}\n'.format(index + 1, hostname, port)
+    # Make sure these lists are at least as long as hostnames
+    directories = (directories or []) + [''] * len(hostnames)
+    database_names = (database_names or []) + [''] * len(hostnames)
+
+    for index, (hostname, port, directory, database_name) in enumerate(zip(
+            hostnames, ports, directories, database_names)):
+        workers += '{} = {}:{}:{}:{}\n'.format(index + 1, hostname, port,
+                                               directory, database_name)
 
     return workers
 
@@ -81,9 +89,7 @@ def main(argv):
     parser.add_argument(
         'worker_hostnames', metavar='workers', type=str, nargs='+',
         help='One or more worker hostnames')
-    parser.add_argument(
-        '--worker-ports', dest='worker_ports', type=int, nargs='*',
-        default=None, help='One or more ports for worker communication')
+
     parser.add_argument(
         '--name', type=str, default='myria',
         help='Name identifying this installation')
@@ -104,13 +110,27 @@ def main(argv):
         default=None, help='Password for connecting to storage layer')
     parser.add_argument(
         '--coordinator-port', dest='coordinator_port', type=int,
-        default=9001, help='Port for coordinator communication')
+        default=8001, help='Port for coordinator communication')
+
+    parser.add_argument(
+        '--worker-ports', dest='worker_ports', type=int, nargs='*',
+        default=None, help='One or more ports for worker communication')
     parser.add_argument(
         '--worker-base-port', dest='worker_base_port', type=int,
-        default=8001, help='Base port for worker communication '
+        default=9001, help='Base port for worker communication '
                            '(when worker-ports not specified)')
     parser.add_argument(
-        '--heap', type=str, default='2g', help='Java VM heap size')
+        '--worker-directories', dest='worker_directories', type=str, nargs='*',
+        default=None, help='One or more worker directories '
+                           '(default is [path])')
+    parser.add_argument(
+        '--worker-databases', dest='worker_databases', type=str, nargs='*',
+        default=None, help='One or more worker database names '
+                           '(default is [--name])')
+
+    parser.add_argument(
+        '--heap', type=str, help='Java VM heap size (e.g., "-Xmx2g") '
+                                 'and/or other parameters')
     parser.add_argument(
         '--debug', default=False, action='store_true',
         help='Enable debugging support')
