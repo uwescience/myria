5ac6702be686aac680c53d7ab614ab640e36bb94
jortiz16 <jortiz16@uw.edu>
2015-05-18 14:38:48 -0700

    workaround and shuffleConsumer handling

--- src/edu/washington/escience/myria/operator/network/CacheShuffleConsumer.java
+++ src/edu/washington/escience/myria/operator/network/CacheShuffleConsumer.java
@@ -5,7 +5,8 @@ package edu.washington.escience.myria.operator.network;
 
 import edu.washington.escience.myria.DbException;
 import edu.washington.escience.myria.Schema;
-import edu.washington.escience.myria.parallel.CacheController;
+import edu.washington.escience.myria.operator.CacheLeaf;
+import edu.washington.escience.myria.operator.CacheRoot;
 import edu.washington.escience.myria.parallel.ExchangePairID;
 import edu.washington.escience.myria.storage.TupleBatch;
 
@@ -13,7 +14,6 @@ import edu.washington.escience.myria.storage.TupleBatch;
  * 
  */
 public class CacheShuffleConsumer extends GenericShuffleConsumer {
-
   /**
    * @param schema input/output data schema
    * @param operatorID my operatorID
@@ -21,15 +21,34 @@ public class CacheShuffleConsumer extends GenericShuffleConsumer {
    * */
   public CacheShuffleConsumer(final Schema schema, final ExchangePairID operatorID, final int[] is) {
     super(schema, operatorID, is);
+    openCacheOperators();
   }
 
+  /**
+   * Starting to open the necessary cache operators. (probably re-think this over)
+   * */
+  public void openCacheOperators() {
+    // open a new root and make this it's child... now CSC has two parents
+    CacheRoot cacheRootOperator = new CacheRoot(this);
+    CacheLeaf cacheLeafOperator = new CacheLeaf();
+    try {
+      /* problem is that "this" is already open */
+      cacheRootOperator.open(getExecEnvVars());
+      cacheLeafOperator.open(getExecEnvVars());
+    } catch (DbException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /* this is what the CacheShuffleConsumer receives and sends to the parent */
   @Override
   protected final TupleBatch fetchNextReady() throws DbException {
     try {
       /* adding the tuple batch to the worker cache */
-      CacheController workerCacheController = getWorker().getCacheController();
-      workerCacheController.addTupleBatch(getTuplesNormal(!nonBlockingExecution));
+      // CacheController workerCacheController = getWorker().getCacheController();
+      // workerCacheController.addTupleBatch(getTuplesNormal(!nonBlockingExecution));
 
+      /* here, it would also be receiving tuples from the cache as well */
       /* returning the tuples as usual */
       return getTuplesNormal(!nonBlockingExecution);
     } catch (final InterruptedException e) {
@@ -37,5 +56,4 @@ public class CacheShuffleConsumer extends GenericShuffleConsumer {
     }
     return null;
   }
-  // This class needs to open the CacheRoot operator and make this operator it's child
 }
