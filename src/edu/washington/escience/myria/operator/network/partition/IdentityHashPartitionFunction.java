1cd372d01e730ee63dff1cd6721ab1e8abb7bb89
Dan Radion <dan.radion@gmail.com>
2015-05-24 10:57:03 -0700

    renamed RawValue to IdentityHash

--- src/edu/washington/escience/myria/operator/network/partition/IdentityHashPartitionFunction.java
+++ src/edu/washington/escience/myria/operator/network/partition/IdentityHashPartitionFunction.java
@@ -8,9 +8,10 @@ import edu.washington.escience.myria.Type;
 import edu.washington.escience.myria.storage.TupleBatch;
 
 /**
- * Implementation of a PartitionFunction that decides based on the raw values of an integer field.
+ * Implementation of a PartitionFunction that use the trivial identity hash.
+ * i.e. a --> a
  */
-public final class RawValuePartitionFunction extends PartitionFunction {
+public final class IdentityHashPartitionFunction extends PartitionFunction {
 
   /** Required for Java serialization. */
   private static final long serialVersionUID = 1L;
@@ -23,12 +24,12 @@ public final class RawValuePartitionFunction extends PartitionFunction {
    * @param index the index of the partition field.
    */
   @JsonCreator
-  public RawValuePartitionFunction(
+  public IdentityHashPartitionFunction(
       @JsonProperty(value = "index", required = true) final Integer index) {
     super(null);
     this.index = java.util.Objects.requireNonNull(index, "missing property index");
     Preconditions.checkArgument(this.index >= 0,
-        "RawValue field index cannot take negative value %s", this.index);
+        "IdentityHash field index cannot take negative value %s", this.index);
   }
 
   /**
@@ -45,7 +46,7 @@ public final class RawValuePartitionFunction extends PartitionFunction {
   @Override
   public int[] partition(final TupleBatch tb) {
     Preconditions.checkArgument(tb.getSchema().getColumnType(index) == Type.INT_TYPE,
-        "RawValue index column must be of type INT");
+        "IdentityHash index column must be of type INT");
     final int[] result = new int[tb.numTuples()];
     for (int i = 0; i < result.length; i++) {
       // Offset by -1 because WorkerIDs are 1-indexed.
