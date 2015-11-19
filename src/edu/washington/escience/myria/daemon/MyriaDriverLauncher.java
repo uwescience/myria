16b3386584ea18f4c1ab178abd584d71f4f31163
Tobin Baker <tdbaker@cs.washington.edu>
2015-11-19 10:48:36 -0800

    first pass at driver-launcher communication

--- src/edu/washington/escience/myria/daemon/MyriaDriverLauncher.java
+++ src/edu/washington/escience/myria/daemon/MyriaDriverLauncher.java
@@ -1,6 +1,7 @@
 package edu.washington.escience.myria.daemon;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,10 +9,20 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.concurrent.GuardedBy;
+import javax.inject.Inject;
+
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.reef.client.ClientConfiguration;
+import org.apache.reef.client.CompletedJob;
 import org.apache.reef.client.DriverConfiguration;
+import org.apache.reef.client.FailedJob;
+import org.apache.reef.client.FailedRuntime;
+import org.apache.reef.client.JobMessage;
+import org.apache.reef.client.LauncherStatus;
 import org.apache.reef.client.REEF;
+import org.apache.reef.client.RunningJob;
 import org.apache.reef.runtime.local.client.LocalRuntimeConfiguration;
 // import org.apache.reef.runtime.yarn.client.YarnClientConfiguration;
 import org.apache.reef.tang.Configuration;
@@ -20,31 +31,30 @@ import org.apache.reef.tang.Injector;
 import org.apache.reef.tang.Tang;
 import org.apache.reef.tang.annotations.Name;
 import org.apache.reef.tang.annotations.NamedParameter;
+import org.apache.reef.tang.annotations.Unit;
 import org.apache.reef.tang.exceptions.BindException;
 import org.apache.reef.tang.exceptions.InjectionException;
 import org.apache.reef.tang.formats.AvroConfigurationSerializer;
 import org.apache.reef.tang.formats.CommandLine;
 import org.apache.reef.tang.formats.ConfigurationModule;
+import org.apache.reef.util.Optional;
+import org.apache.reef.wake.EventHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import edu.washington.escience.myria.MyriaConstants;
 import edu.washington.escience.myria.coordinator.ConfigFileException;
-import edu.washington.escience.myria.daemon.MyriaDriver.ActiveContextHandler;
-import edu.washington.escience.myria.daemon.MyriaDriver.CompletedEvaluatorHandler;
-import edu.washington.escience.myria.daemon.MyriaDriver.CompletedTaskHandler;
-import edu.washington.escience.myria.daemon.MyriaDriver.ContextFailureHandler;
-import edu.washington.escience.myria.daemon.MyriaDriver.EvaluatorAllocatedHandler;
-import edu.washington.escience.myria.daemon.MyriaDriver.EvaluatorFailureHandler;
-import edu.washington.escience.myria.daemon.MyriaDriver.RunningTaskHandler;
-import edu.washington.escience.myria.daemon.MyriaDriver.StartHandler;
-import edu.washington.escience.myria.daemon.MyriaDriver.StopHandler;
-import edu.washington.escience.myria.daemon.MyriaDriver.TaskFailureHandler;
-import edu.washington.escience.myria.daemon.MyriaDriver.TaskMessageHandler;
 import edu.washington.escience.myria.tools.MyriaConfigurationParser;
 
+@Unit
 public final class MyriaDriverLauncher {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(MyriaDriverLauncher.class);
 
   private static final String USAGE_STRING =
       "Usage: MyriaDriverLauncher -configPath <configPath> -javaLibPath <javaLibPath> -nativeLibPath <nativeLibPath>";
+
+  private static final long DRIVER_PING_TIMEOUT_MILLIS = 60 * 1000;
 
   /**
    * @param args full path to the configuration directory.
@@ -57,17 +67,27 @@ public final class MyriaDriverLauncher {
     }
     // final Configuration runtimeConfiguration = YarnClientConfiguration.CONF.build();
     final Configuration runtimeConfiguration = LocalRuntimeConfiguration.CONF.build();
-    launchDriver(runtimeConfiguration, args);
+    run(runtimeConfiguration, args);
   }
 
-  /**
-   * @param configPath path to directory of containing configuration files
-   * @return Configuration object.
-   */
-  private static Configuration getMyriaGlobalConf(final String configPath) throws IOException,
-      BindException, ConfigFileException {
-    final String configFile = FilenameUtils.concat(configPath, MyriaConstants.DEPLOYMENT_CONF_FILE);
-    return MyriaConfigurationParser.loadConfiguration(configFile);
+  private final REEF reef;
+  @GuardedBy("this")
+  private Optional<RunningJob> driver = Optional.empty();
+  @GuardedBy("this")
+  private LauncherStatus status = LauncherStatus.INIT;
+
+  @Inject
+  private MyriaDriverLauncher(final REEF reef) {
+    this.reef = reef;
+  }
+
+  private final static Configuration getClientConf() {
+    return ClientConfiguration.CONF
+        .set(ClientConfiguration.ON_JOB_RUNNING, RunningJobHandler.class)
+        .set(ClientConfiguration.ON_JOB_MESSAGE, JobMessageHandler.class)
+        .set(ClientConfiguration.ON_JOB_COMPLETED, CompletedJobHandler.class)
+        .set(ClientConfiguration.ON_JOB_FAILED, FailedJobHandler.class)
+        .set(ClientConfiguration.ON_RUNTIME_ERROR, RuntimeErrorHandler.class).build();
   }
 
   /**
@@ -77,18 +97,22 @@ public final class MyriaDriverLauncher {
   private final static Configuration getDriverConf(final String[] libPaths, final String[] filePaths)
       throws IOException {
     ConfigurationModule driverConf =
-        DriverConfiguration.CONF.set(DriverConfiguration.DRIVER_IDENTIFIER, "MyriaDriver")
-            .set(DriverConfiguration.ON_DRIVER_STARTED, StartHandler.class)
-            .set(DriverConfiguration.ON_DRIVER_STOP, StopHandler.class)
-            .set(DriverConfiguration.ON_EVALUATOR_ALLOCATED, EvaluatorAllocatedHandler.class)
-            .set(DriverConfiguration.ON_EVALUATOR_COMPLETED, CompletedEvaluatorHandler.class)
-            .set(DriverConfiguration.ON_EVALUATOR_FAILED, EvaluatorFailureHandler.class)
-            .set(DriverConfiguration.ON_CONTEXT_ACTIVE, ActiveContextHandler.class)
-            .set(DriverConfiguration.ON_CONTEXT_FAILED, ContextFailureHandler.class)
-            .set(DriverConfiguration.ON_TASK_RUNNING, RunningTaskHandler.class)
-            .set(DriverConfiguration.ON_TASK_COMPLETED, CompletedTaskHandler.class)
-            .set(DriverConfiguration.ON_TASK_FAILED, TaskFailureHandler.class)
-            .set(DriverConfiguration.ON_TASK_MESSAGE, TaskMessageHandler.class);
+        DriverConfiguration.CONF
+            .set(DriverConfiguration.DRIVER_IDENTIFIER, "MyriaDriver")
+            .set(DriverConfiguration.ON_DRIVER_STARTED, MyriaDriver.StartHandler.class)
+            .set(DriverConfiguration.ON_DRIVER_STOP, MyriaDriver.StopHandler.class)
+            .set(DriverConfiguration.ON_EVALUATOR_ALLOCATED,
+                MyriaDriver.EvaluatorAllocatedHandler.class)
+            .set(DriverConfiguration.ON_EVALUATOR_COMPLETED,
+                MyriaDriver.CompletedEvaluatorHandler.class)
+            .set(DriverConfiguration.ON_EVALUATOR_FAILED, MyriaDriver.EvaluatorFailureHandler.class)
+            .set(DriverConfiguration.ON_CONTEXT_ACTIVE, MyriaDriver.ActiveContextHandler.class)
+            .set(DriverConfiguration.ON_CONTEXT_FAILED, MyriaDriver.ContextFailureHandler.class)
+            .set(DriverConfiguration.ON_TASK_RUNNING, MyriaDriver.RunningTaskHandler.class)
+            .set(DriverConfiguration.ON_TASK_COMPLETED, MyriaDriver.CompletedTaskHandler.class)
+            .set(DriverConfiguration.ON_TASK_FAILED, MyriaDriver.TaskFailureHandler.class)
+            .set(DriverConfiguration.ON_TASK_MESSAGE, MyriaDriver.TaskMessageHandler.class)
+            .set(DriverConfiguration.ON_CLIENT_MESSAGE, MyriaDriver.ClientMessageHandler.class);
 
     for (String dirPath : libPaths) {
       for (String filePath : getFileNamesInDirectory(Paths.get(dirPath))) {
@@ -101,6 +125,16 @@ public final class MyriaDriverLauncher {
       }
     }
     return driverConf.build();
+  }
+
+  /**
+   * @param configPath path to directory of containing configuration files
+   * @return Configuration object.
+   */
+  private static Configuration getMyriaGlobalConf(final String configPath) throws IOException,
+      BindException, ConfigFileException {
+    final String configFile = FilenameUtils.concat(configPath, MyriaConstants.DEPLOYMENT_CONF_FILE);
+    return MyriaConfigurationParser.loadConfiguration(configFile);
   }
 
   private static List<String> getFileNamesInDirectory(final Path root) throws IOException {
@@ -132,7 +166,7 @@ public final class MyriaDriverLauncher {
    * @throws ParseException
    * @throws ConfigFileException
    */
-  public static void launchDriver(final Configuration runtimeConf, final String[] args)
+  public static LauncherStatus run(final Configuration runtimeConf, final String[] args)
       throws InjectionException, IOException, ParseException, ConfigFileException {
     final Tang tang = Tang.Factory.getTang();
     // TODO: implement ClientConfiguration event handlers so we can get messages from the Driver
@@ -153,8 +187,44 @@ public final class MyriaDriverLauncher {
         Configurations.merge(
             getDriverConf(new String[] {javaLibPath}, new String[] {nativeLibPath}),
             globalConfWrapper);
-    final REEF reef = tang.newInjector(runtimeConf).getInstance(REEF.class);
+
+    return tang.newInjector(runtimeConf, getClientConf()).getInstance(MyriaDriverLauncher.class)
+        .run(driverConf);
+  }
+
+  private LauncherStatus run(final Configuration driverConf) {
+    LOGGER.info("Submitting Myria driver to REEF...");
     reef.submit(driverConf);
+
+    synchronized (this) {
+      while (!status.isDone()) {
+        LOGGER.info("Waiting for Myria driver to finish...");
+        try {
+          this.wait(DRIVER_PING_TIMEOUT_MILLIS);
+          if (driver.isPresent()) {
+            final byte[] driverMsg = MyriaDriver.DRIVER_PING_MSG.getBytes(StandardCharsets.UTF_8);
+            LOGGER.info("Sending message to Myria driver: {}", MyriaDriver.DRIVER_PING_MSG);
+            driver.get().send(driverMsg);
+          }
+        } catch (final InterruptedException ex) {
+          LOGGER.info("Interrupted while waiting for Myria driver to finish, exiting...");
+          close(); // this sets status to FORCE_CLOSED
+        }
+      }
+    }
+
+    reef.close();
+    return status;
+  }
+
+  public synchronized void close() {
+    if (status.isRunning()) {
+      status = LauncherStatus.FORCE_CLOSED;
+    }
+    if (driver.isPresent()) {
+      driver.get().close();
+    }
+    notify();
   }
 
   /**
@@ -186,4 +256,56 @@ public final class MyriaDriverLauncher {
   public static final class SerializedGlobalConf implements Name<String> {
   }
 
+  final class JobMessageHandler implements EventHandler<JobMessage> {
+    @Override
+    public void onNext(final JobMessage message) {
+      final byte[] msg = message.get();
+      final String msgStr = new String(msg, StandardCharsets.UTF_8);
+      LOGGER.info("Message from Myria driver: {}", msgStr);
+    }
+  }
+
+  final class RunningJobHandler implements EventHandler<RunningJob> {
+    @Override
+    public void onNext(final RunningJob job) {
+      LOGGER.info("Myria driver is running...");
+      synchronized (MyriaDriverLauncher.this) {
+        status = LauncherStatus.RUNNING;
+        driver = Optional.of(job);
+      }
+    }
+  }
+
+  final class CompletedJobHandler implements EventHandler<CompletedJob> {
+    @Override
+    public void onNext(final CompletedJob job) {
+      LOGGER.info("Myria driver exited");
+      synchronized (MyriaDriverLauncher.this) {
+        status = LauncherStatus.COMPLETED;
+        MyriaDriverLauncher.this.notify();
+      }
+    }
+  }
+
+  final class FailedJobHandler implements EventHandler<FailedJob> {
+    @Override
+    public void onNext(final FailedJob job) {
+      LOGGER.error("Myria driver failed: {}", job.getReason().orElse(null));
+      synchronized (MyriaDriverLauncher.this) {
+        status = LauncherStatus.failed(job.getReason());
+        MyriaDriverLauncher.this.notify();
+      }
+    }
+  }
+
+  final class RuntimeErrorHandler implements EventHandler<FailedRuntime> {
+    @Override
+    public void onNext(final FailedRuntime error) {
+      LOGGER.error("Myria driver runtime error {}: {}", error, error.getReason().orElse(null));
+      synchronized (MyriaDriverLauncher.this) {
+        status = LauncherStatus.failed(error.getReason());
+        MyriaDriverLauncher.this.notify();
+      }
+    }
+  }
 }
