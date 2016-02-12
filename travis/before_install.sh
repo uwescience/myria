db50db0a82bc3a306f1ecf70778f3cb6c9602dfe
Tobin Baker <tdbaker@cs.washington.edu>
2016-02-12 10:51:01 -0800

    add IAM user to travis with temp S3 test

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -9,3 +9,5 @@ cat ~/.ssh/id_rsa_localhost.pub >> ~/.ssh/authorized_keys
 ssh-keyscan -t rsa localhost >> ~/.ssh/known_hosts
 echo "IdentityFile ~/id_rsa" >> ~/.ssh/config
 echo "IdentityFile ~/id_rsa_localhost" >> ~/.ssh/config
+
+python -c 'import boto, boto.s3.key, time, os; b = boto.connect_s3(os.environ["AWS_ACCESS_KEY_ID"], os.environ["AWS_SECRET_ACCESS_KEY"]).get_bucket("myria-test"); boto.s3.key.Key(bucket=b, name="myria-write-test-%d" % int(time.time())).set_contents_from_string("Myria S3 write test")'
