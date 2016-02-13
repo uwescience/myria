6e15a79fe3c72d15551397a359ec68e6f35c5a2b
jortiz16 <jortiz16@uw.edu>
2016-02-12 17:35:44 -0800

    test s3

--- test/edu/washington/escience/myria/operator/DataOutputTest.java
+++ test/edu/washington/escience/myria/operator/DataOutputTest.java
@@ -3,6 +3,8 @@
  */
 package edu.washington.escience.myria.operator;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableList;
@@ -22,27 +24,28 @@ import edu.washington.escience.myria.util.TestEnvVars;
 public class DataOutputTest {
 
   @Test
-  public void readCSVTest() throws Exception {
+  public void DataOutputTest() throws Exception {
     /* Read a CSV and construct the query */
     final String filename = "testdata/twitter/TwitterK.csv";
     final Schema schema = new Schema(ImmutableList.of(Type.INT_TYPE, Type.INT_TYPE));
 
+    /* Build the query and verify */
     FileScan scanCSV = new FileScan(new FileSource(filename), schema);
     DataSink byteSink = new ByteSink();
     DataOutput dataOutput = new DataOutput(scanCSV, new CsvTupleWriter(), byteSink);
 
     dataOutput.open(TestEnvVars.get());
-
-    /* Read the data */
-    String result = null;
-    TupleBatch tb = null;
+    int actualTupleCount = 0;
     while (!dataOutput.eos()) {
-      tb = dataOutput.nextReady();
+      TupleBatch tb = dataOutput.nextReady();
       if (tb != null) {
-        // add to result
+        actualTupleCount += tb.numTuples();
       }
     }
+    dataOutput.close();
 
-    /* Verify results */
+    int expectedTupleCount = 2715;
+    assertEquals(actualTupleCount, expectedTupleCount);
+
   }
 }
