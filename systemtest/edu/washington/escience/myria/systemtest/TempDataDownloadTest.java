c51313e36462351511acca4ab19f7ace56dfe396
jortiz16 <jortiz16@uw.edu>
2016-02-19 10:08:26 -0800

    compile

--- systemtest/edu/washington/escience/myria/systemtest/TempDataDownloadTest.java
+++ systemtest/edu/washington/escience/myria/systemtest/TempDataDownloadTest.java
@@ -6,6 +6,8 @@ package edu.washington.escience.myria.systemtest;
 import org.junit.Test;
 
 import edu.washington.escience.myria.RelationKey;
+import edu.washington.escience.myria.Schema;
+import edu.washington.escience.myria.Type;
 import edu.washington.escience.myria.io.DataSource;
 import edu.washington.escience.myria.io.UriSource;
 import edu.washington.escience.myria.operator.network.partition.RoundRobinPartitionFunction;
@@ -21,6 +23,7 @@ public class TempDataDownloadTest extends SystemTestBase {
     /* Read the data back in from S3 into one worker */
     RelationKey relationKeyDownload = RelationKey.of("public", "adhoc", "download");
     DataSource relationSourceS3 = new UriSource("s3://myria-test/test.txt");
+    Schema relationSchema = Schema.ofFields("x", Type.INT_TYPE, "y", Type.INT_TYPE);
     JsonAPIUtils.ingestData("localhost", masterDaemonPort, ingest(relationKeyDownload, relationSchema,
         relationSourceS3, ' ', new RoundRobinPartitionFunction(2)));
 
