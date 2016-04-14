a86faad7cd5a756a438ff8d6d57f384ddd41c4c8
jortiz16 <jortiz16@uw.edu>
2016-04-14 00:30:10 -0700

    added CSVFragment and test

--- src/edu/washington/escience/myria/io/UriSource.java
+++ src/edu/washington/escience/myria/io/UriSource.java
@@ -16,6 +16,12 @@ import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3URI;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -51,24 +57,20 @@ public class UriSource implements DataSource, Serializable {
     parsedUri =
         URI.create(Objects.requireNonNull(uri, "Parameter uri to UriSource may not be null"));
     /* Force using the Hadoop S3A FileSystem */
-    if (parsedUri.getScheme().equals("s3")) {
-      parsedUri =
-          new URI(
-              "s3a",
-              parsedUri.getUserInfo(),
-              parsedUri.getHost(),
-              parsedUri.getPort(),
-              parsedUri.getPath(),
-              parsedUri.getQuery(),
-              parsedUri.getFragment());
-    }
+    // if (parsedUri.getScheme().equals("s3")) {
+    // parsedUri = new URI("s3a", parsedUri.getUserInfo(), parsedUri.getHost(), parsedUri.getPort(),
+    // parsedUri.getPath(),
+    // parsedUri.getQuery(), parsedUri.getFragment());
+    // }
   }
 
   @Override
   public InputStream getInputStream() throws IOException {
-    return (parsedUri.getScheme().equals("http") || parsedUri.getScheme().equals("https"))
-        ? parsedUri.toURL().openConnection().getInputStream()
-        : getHadoopFileSystemInputStream(parsedUri);
+    if (parsedUri.getScheme().equals("http") || parsedUri.getScheme().equals("https")) {
+      return parsedUri.toURL().openConnection().getInputStream();
+    } else {
+      return getHadoopFileSystemInputStream(parsedUri);
+    }
   }
 
   /**
@@ -93,5 +95,31 @@ public class UriSource implements DataSource, Serializable {
     }
 
     return new SequenceInputStream(java.util.Collections.enumeration(streams));
+  }
+
+  /**
+   * Modifies the request to get the input stream
+   */
+  public InputStream getChunkInputStream(final long startRange, final long endRange, final boolean lastWorker)
+      throws IOException {
+    AmazonS3URI s3URI = new AmazonS3URI(parsedUri);
+    AmazonS3 s3Client = new AmazonS3Client(new DefaultAWSCredentialsProviderChain());
+    GetObjectRequest s3Request = new GetObjectRequest(s3URI.getBucket(), s3URI.getKey());
+    S3Object s3Object = s3Client.getObject(s3Request);
+    if (lastWorker) {
+      s3Request.setRange(startRange);
+    } else {
+      s3Request.setRange(startRange, endRange);
+    }
+    s3Object = s3Client.getObject(s3Request);
+    return s3Object.getObjectContent();
+  }
+
+  public long getFileSize() {
+    AmazonS3URI s3URI = new AmazonS3URI(parsedUri);
+    AmazonS3 s3Client = new AmazonS3Client(new DefaultAWSCredentialsProviderChain());
+    GetObjectRequest s3Request = new GetObjectRequest(s3URI.getBucket(), s3URI.getKey());
+    S3Object s3Object = s3Client.getObject(s3Request);
+    return s3Object.getObjectMetadata().getContentLength();
   }
 }
