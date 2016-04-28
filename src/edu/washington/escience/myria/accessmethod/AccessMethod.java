3a6988674c4d7ecc7c53155339801e18b0989b88
jortiz16 <jortiz16@uw.edu>
2016-04-28 10:27:26 -0700

    db view and index operators

--- src/edu/washington/escience/myria/accessmethod/AccessMethod.java
+++ src/edu/washington/escience/myria/accessmethod/AccessMethod.java
@@ -189,6 +189,15 @@ public abstract class AccessMethod {
       throws DbException;
 
   /**
+   * Creates a view
+   * 
+   * @param viewName
+   * @param viewQuery
+   * @throws DbException
+   */
+  public abstract void createViewIfNotExists(String viewName, String viewQuery) throws DbException;
+
+  /**
    * Holds a reference to a column and whether it is ascending or descending.
    */
   public static final class IndexRef implements Serializable {
