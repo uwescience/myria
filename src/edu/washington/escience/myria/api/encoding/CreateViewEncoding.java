e9b96724d7d126adc7ad7c89345ae00e5760348d
jortiz16 <jortiz16@uw.edu>
2016-05-16 14:39:02 -0700

    fix catalog bug and make create views idempotent

--- src/edu/washington/escience/myria/api/encoding/CreateViewEncoding.java
+++ src/edu/washington/escience/myria/api/encoding/CreateViewEncoding.java
@@ -9,7 +9,9 @@ import java.util.Set;
  * 
  */
 public class CreateViewEncoding extends MyriaApiEncoding {
+  @Required
   public String viewName;
+  @Required
   public String viewDefinition;
   public Set<Integer> workers;
 }
