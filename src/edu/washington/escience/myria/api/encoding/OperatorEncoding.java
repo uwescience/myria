34b6dee103720dd665bf779f872622c7025226fe
Tobin Baker <tdbaker@cs.washington.edu>
2016-09-03 13:43:33 -0700

    remove Split operator since we have SplitExpression

--- src/edu/washington/escience/myria/api/encoding/OperatorEncoding.java
+++ src/edu/washington/escience/myria/api/encoding/OperatorEncoding.java
@@ -68,7 +68,6 @@ import edu.washington.escience.myria.operator.Operator;
   @Type(name = "Singleton", value = SingletonEncoding.class),
   @Type(name = "CrossWithSingleton", value = CrossWithSingletonEncoding.class),
   @Type(name = "SinkRoot", value = SinkRootEncoding.class),
-  @Type(name = "Split", value = SplitEncoding.class),
   @Type(name = "StatefulApply", value = StatefulApplyEncoding.class),
   @Type(name = "SymmetricHashJoin", value = SymmetricHashJoinEncoding.class),
   @Type(name = "SymmetricHashCountingJoin", value = SymmetricHashCountingJoinEncoding.class),
