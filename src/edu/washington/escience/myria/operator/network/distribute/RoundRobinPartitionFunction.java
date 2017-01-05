e33ccac00156f863704169e5dc6152f88d1762be
Tobin Baker <tdbaker@cs.washington.edu>
2017-01-05 15:30:11 -0800

    logging

--- src/edu/washington/escience/myria/operator/network/distribute/RoundRobinPartitionFunction.java
+++ src/edu/washington/escience/myria/operator/network/distribute/RoundRobinPartitionFunction.java
@@ -6,11 +6,15 @@ import javax.annotation.Nonnull;
 
 import edu.washington.escience.myria.storage.TupleBatch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * A partition function that simply sends one tuple to each output in turn.
  */
 public final class RoundRobinPartitionFunction extends PartitionFunction {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(RoundRobinPartitionFunction.class);
   /** Required for Java serialization. */
   private static final long serialVersionUID = 1L;
   /** The current partition to use. */
@@ -20,6 +24,7 @@ public final class RoundRobinPartitionFunction extends PartitionFunction {
   public TupleBatch[] partition(@Nonnull final TupleBatch tb) {
     BitSet[] partitions = new BitSet[numPartitions()];
     for (int i = 0; i < tb.numTuples(); i++) {
+      LOGGER.info("Current partition: " + curPartition);
       partitions[curPartition].set(i);
       curPartition = (curPartition + 1) % numPartitions();
     }
