654be579f7bc75983ff53a0b656b4d6442a4b1e6
Tobin Baker <tdbaker@cs.washington.edu>
2017-01-05 17:58:25 -0800

    fix NPE

--- src/edu/washington/escience/myria/api/encoding/GenericShuffleProducerEncoding.java
+++ src/edu/washington/escience/myria/api/encoding/GenericShuffleProducerEncoding.java
@@ -10,14 +10,9 @@ import edu.washington.escience.myria.operator.network.GenericShuffleProducer;
 import edu.washington.escience.myria.operator.network.distribute.DistributeFunction;
 import edu.washington.escience.myria.parallel.ExchangePairID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /** JSON wrapper for GenericShuffleProducer encoding. */
 public class GenericShuffleProducerEncoding
     extends AbstractProducerEncoding<GenericShuffleProducer> {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(GenericShuffleProducerEncoding.class);
 
   /** The distribute function. */
   @Required public DistributeFunction distributeFunction;
@@ -28,9 +23,7 @@ public class GenericShuffleProducerEncoding
   @Override
   public GenericShuffleProducer construct(final ConstructArgs args) {
     Set<Integer> workerIds = getRealWorkerIds();
-    LOGGER.info("worker IDs: " + workerIds.toString());
     List<ExchangePairID> operatorIds = getRealOperatorIds();
-    LOGGER.info("operator IDs: " + operatorIds.toString());
     distributeFunction.setDestinations(workerIds.size(), operatorIds.size());
     GenericShuffleProducer producer =
         new GenericShuffleProducer(
