d5db77760c4be0ad007ee8134b30fbe8834606e2
Brandon Haynes <bhaynes@cs.washington.edu>
2017-03-02 15:44:16 -0800

    Added relation dict comparison helper and additional test

--- integration_tests.py
+++ integration_tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import unittest
+from collections import Counter
 from tempfile import NamedTemporaryFile
 from myria import MyriaConnection, MyriaRelation, MyriaQuery, MyriaError
 
@@ -10,6 +11,10 @@ class MyriaTestBase(unittest.TestCase):
         connection = MyriaConnection(hostname='localhost', port=8753, execution_url="http://127.0.0.1:8080")
         MyriaRelation.DefaultConnection = connection
         self.connection = connection
+
+    def assertListOfDictsEqual(self, left, right):
+        self.assertEqual(Counter([kv for d in left for kv in d.items()]),
+                         Counter([kv for d in right for kv in d.items()]))
 
 class DoWhileTest(MyriaTestBase):
     def test(self):
@@ -24,7 +29,7 @@ store(x, powersOfTwo);
         expected = [{'val': 32, 'exp': 5}]
 
         self.assertEqual(query.status, 'SUCCESS')
-        self.assertEqual(query.to_dict(), expected)
+        self.assertListOfDictsEqual(query.to_dict(), expected)
 
 
 class IngestEmptyQueryTest(MyriaTestBase):
@@ -39,7 +44,42 @@ store(emptyrelation, emptyrelation);
         expected = []
         query = MyriaQuery.submit(program)
         self.assertEqual(query.status, 'SUCCESS')
-        self.assertEqual(query.to_dict(), expected)
+        self.assertListOfDictsEqual(query.to_dict(), expected)
+
+
+class UploadAndReplaceDataTest(MyriaTestBase):
+    def test(self):
+        with NamedTemporaryFile() as f:
+            data = "foo,3242\n" \
+                   "bar,321\n"\
+                   "baz,104"
+            f.write(data)
+            #TODO change URL to local file
+            program = """
+uploaddatatest = load('https://s3-us-west-2.amazonaws.com/bhaynestemp/uploaddatatest', csv(schema(s:string, i:int)));
+store(uploaddatatest, uploaddatatest);
+"""
+            expected = [{u's': u'foo', u'i': 3242},
+                        {u's': u'bar', u'i': 321},
+                        {u's': u'baz', u'i': 104}]
+            query = MyriaQuery.submit(program)
+            self.assertEqual(query.status, 'SUCCESS')
+            self.assertListOfDictsEqual(query.to_dict(), expected)
+
+            # Now replace with another relation
+            program = """
+uploaddatatest = load('https://s3-us-west-2.amazonaws.com/bhaynestemp/uploaddatatest2',
+                       csv(schema(s:string, i:int), delimiter='\\t'));
+store(uploaddatatest, uploaddatatest);
+"""
+            expected = [{u's': u'mexico', u'i': 42},
+                        {u's': u'poland', u'i': 12342},
+                        {u's': u'belize', u'i': 802304}]
+            query = MyriaQuery.submit(program)
+            self.assertEqual(query.status, 'SUCCESS')
+            self.assertListOfDictsEqual(query.to_dict(), expected)
+
+
 
 
 if __name__ == '__main__':
