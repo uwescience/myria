5239d5742a9315ca845790ac1f7689a173ce4a9e
jingjingwang <jwang@cs.washington.edu>
2017-03-02 17:40:18 -0800

    add joinchain

--- integration_tests.py
+++ integration_tests.py
@@ -6,6 +6,7 @@ import unittest
 from tempfile import NamedTemporaryFile
 from collections import Counter
 from myria import MyriaConnection, MyriaRelation, MyriaQuery, MyriaError
+from sets import Set
 
 
 class MyriaTestBase(unittest.TestCase):
@@ -35,60 +36,9 @@ store(x, powersOfTwo);
         self.assertListOfDictsEqual(query.to_dict(), expected)
 
 
-class ConnectedComponentTest(MyriaTestBase):
-    # TODO: add failure injector and prioritization
-    MAXN = 50
-    MAXM = MAXN * 10
-
-    def get_program(self, inputfile, sync_mode=''):
-        return """
-E = load("file://%s", csv(schema(src:int, dst:int), skip=0));
-V = [from E emit src as x] + [from E emit dst as x];
-V = select distinct x from V;
-do
-  CC = [nid, MIN(cid) as cid] <-
-    [from V emit V.x as nid, V.x as cid] +
-    [from E, CC where E.src = CC.nid emit E.dst as nid, CC.cid];
-until convergence %s pull_idb;
-store(CC, CC_output);
-""" % (inputfile, sync_mode)
-
-    def get_expected(self, edges):
-        ans = {}
-        for edge in edges:
-            ans[edge[0]] = edge[0]
-            ans[edge[1]] = edge[1]
-        while True:
-            changed = False
-            for edge in edges:
-                if ans[edge[0]] < ans[edge[1]]:
-                    changed = True
-                    ans[edge[1]] = ans[edge[0]]
-            if not changed: break
-        return [{'nid':k, 'cid':ans[k]} for k in ans]
-
-    def test(self):
-        edges = []
-        for i in range(self.MAXM):
-            src = random.randint(0, self.MAXN)
-            dst = random.randint(0, self.MAXN)
-            edges.append((src, dst))
-        with NamedTemporaryFile(suffix='.csv') as cc_input:
-            for edge in edges:
-                cc_input.write('%d,%d\n' % (edge[0], edge[1]))
-            cc_input.flush()
-            query = MyriaQuery.submit(self.get_program(cc_input.name))
-            self.assertEqual(query.status, 'SUCCESS')
-            results = MyriaRelation('public:adhoc:CC_output').to_dict()
-            self.assertListOfDictsEqual(results, self.get_expected(edges))
-            query = MyriaQuery.submit(self.get_program(cc_input.name, 'sync'))
-            self.assertEqual(query.status, 'SUCCESS')
-            results = MyriaRelation('public:adhoc:CC_output').to_dict()
-            self.assertListOfDictsEqual(results, self.get_expected(edges))
-
-
 class IngestEmptyQueryTest(MyriaTestBase):
     def test(self):
+        # Create empty file
         #TODO change URL to local file
         program = """
 emptyrelation = load('https://s3-us-west-2.amazonaws.com/bhaynestemp/emptyrelation', csv(schema(foo:string, bar:int)));
@@ -131,6 +81,133 @@ store(uploaddatatest, uploaddatatest);
         self.assertListOfDictsEqual(query.to_dict(), expected)
 
 
+class ConnectedComponentTest(MyriaTestBase):
+    # TODO: add failure injector and prioritization
+    MAXN = 50
+    MAXM = MAXN * 10
+
+    def get_program(self, inputfile, sync_mode=''):
+        return """
+E = load("file://%s", csv(schema(src:int, dst:int), skip=0));
+V = [from E emit src as x] + [from E emit dst as x];
+V = select distinct x from V;
+do
+  CC = [nid, MIN(cid) as cid] <-
+    [from V emit V.x as nid, V.x as cid] +
+    [from E, CC where E.src = CC.nid emit E.dst as nid, CC.cid];
+until convergence %s pull_idb;
+store(CC, CC_output);
+""" % (inputfile, sync_mode)
+
+    def get_expected(self, edges):
+        ans = {}
+        for edge in edges:
+            ans[edge[0]] = edge[0]
+            ans[edge[1]] = edge[1]
+        while True:
+            changed = False
+            for edge in edges:
+                if ans[edge[0]] < ans[edge[1]]:
+                    changed = True
+                    ans[edge[1]] = ans[edge[0]]
+            if not changed: break
+        return [{'nid':k, 'cid':ans[k]} for k in ans]
+
+    def test(self):
+        edges = []
+        for i in range(self.MAXM):
+            src = random.randint(0, self.MAXN-1)
+            dst = random.randint(0, self.MAXN-1)
+            edges.append((src, dst))
+        with NamedTemporaryFile(suffix='.csv') as cc_input:
+            for edge in edges:
+                cc_input.write('%d,%d\n' % (edge[0], edge[1]))
+            cc_input.flush()
+            query = MyriaQuery.submit(self.get_program(cc_input.name))
+            self.assertEqual(query.status, 'SUCCESS')
+            results = MyriaRelation('public:adhoc:CC_output').to_dict()
+            self.assertListOfDictsEqual(results, self.get_expected(edges))
+            query = MyriaQuery.submit(self.get_program(cc_input.name, 'sync'))
+            self.assertEqual(query.status, 'SUCCESS')
+            results = MyriaRelation('public:adhoc:CC_output').to_dict()
+            self.assertListOfDictsEqual(results, self.get_expected(edges))
+
+
+class JoinChainTest(MyriaTestBase):
+    # TODO: add failure injector and prioritization
+    MAXN = 20
+    MAXM = 40
+
+    def get_join_chain_program(self, R, A0, B0, C0):
+        return """
+R = load("file://%s", csv(schema(src:int, dst:int), skip=0));
+A0 = load("file://%s", csv(schema(src:int, dst:int), skip=0));
+B0 = load("file://%s", csv(schema(src:int, dst:int), skip=0));
+C0 = load("file://%s", csv(schema(src:int, dst:int), skip=0));
+do
+  A = [src, dst] <-
+    [from A0 emit src, dst] +
+    [from R, A where R.dst = A.src emit R.src, A.dst];
+  B = [src, dst] <-
+    [from B0 emit src, dst] +
+    [from A, B where A.dst = B.src emit A.src, B.dst];
+  C = [src, dst] <-
+    [from C0 emit src, dst] +
+    [from B, C where B.dst = C.src emit B.src, C.dst];
+until convergence;
+store(C, joinChain);
+""" % (R, A0, B0, C0)
+
+    def get_join_chain_expected(self, R, A0, B0, C0):
+        print 'R', R
+        print 'A0', A0
+        print 'B0', B0
+        print 'C0', C0
+	A = Set(A0)
+	B = Set(B0)
+	C = Set(C0)
+        while True:
+            change = False
+	    for i in range(self.MAXN):
+	        for j in range(self.MAXN):
+	            for k in range(self.MAXN):
+                        if (j, i) in R and (i, k) in A and (j, k) not in A:
+                            change = True
+                            A.add((j, k))
+                        if (j, i) in A and (i, k) in B and (j, k) not in B:
+                            change = True
+                            B.add((j, k))
+                        if (j, i) in B and (i, k) in C and (j, k) not in C:
+                            change = True
+                            C.add((j, k))
+            if not change: break
+        return [{'src':x, 'dst':y} for x, y in C]
+
+    def genData(self, fin):
+        edges = []
+        for i in range(self.MAXM):
+            src = random.randint(0, self.MAXN-1)
+            dst = random.randint(0, self.MAXN-1)
+            edges.append((src, dst))
+            fin.write('%d,%d\n' % (src, dst))
+        fin.flush()
+        return edges
+
+    def test(self):
+        with NamedTemporaryFile(suffix='.csv') as R, NamedTemporaryFile(suffix='.csv') as A0, NamedTemporaryFile(suffix='.csv') as B0, NamedTemporaryFile(suffix='.csv') as C0:
+            R_data = self.genData(R)
+            A0_data = self.genData(A0)
+        """
+            B0_data = self.genData(B0)
+            C0_data = self.genData(C0)
+
+            query = MyriaQuery.submit(self.get_join_chain_program(R.name, A0.name, B0.name, C0.name))
+            self.assertEqual(query.status, 'SUCCESS')
+
+            results = MyriaRelation('public:adhoc:joinChain').to_dict()
+            self.assertListOfDictsEqual(results, self.get_join_chain_expected(R_data, A0_data, B0_data, C0_data))
+
+
 class IncorrectDelimiterTest(MyriaTestBase):
     def test(self):
         #TODO change URL to local file
@@ -140,7 +217,7 @@ r = load('https://s3-us-west-2.amazonaws.com/bhaynestemp/simple_two_col_int.txt'
 store(r, r);
 """
         query = MyriaQuery.submit(program)
-        self.assertEqual(query.status, 'ERROR')
+        self.assertEqual(query.status, 'ERROR'
 
 
 class NonNullChildTest(MyriaTestBase):
@@ -172,8 +249,7 @@ class DbDeleteTest(MyriaTestBase):
 
         # delete relation and check the catalog
         relation.delete()
-
-        self.assertRaises(MyriaError,self.connection.dataset,relation.qualified_name) 
+         self.assertRaises(MyriaError,self.connection.dataset,relation.qualified_name)
 
 
 if __name__ == '__main__':
