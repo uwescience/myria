ceddd39f1f3d5948eac4b9ab963a4c1ad47b1914
jortiz16 <jortiz16@cs.washington.edu>
2017-03-06 02:16:38 -0800

    addressing comments on PerfEnforce API

--- src/edu/washington/escience/myria/api/PerfEnforceResource.java
+++ src/edu/washington/escience/myria/api/PerfEnforceResource.java
@@ -10,7 +10,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.slf4j.LoggerFactory;
 
 import edu.washington.escience.myria.DbException;
@@ -41,7 +40,7 @@ public final class PerfEnforceResource {
   public Response prepareData(final List<PerfEnforceTableEncoding> tableList)
       throws PerfEnforceException, DbException, Exception {
     server.getPerfEnforceDriver().preparePSLA(tableList);
-    return Response.noContent().build();
+    return Response.status(201).build();
   }
 
   /**
@@ -78,8 +77,8 @@ public final class PerfEnforceResource {
    */
   @POST
   @Path("/setTier")
-  @Consumes(MediaType.MULTIPART_FORM_DATA)
-  public Response setTier(@FormDataParam("tier") final int tier) {
+  @Consumes(MediaType.TEXT_PLAIN)
+  public Response setTier(final int tier) {
     server.getPerfEnforceDriver().setTier(tier);
     return Response.noContent().build();
   }
@@ -91,8 +90,8 @@ public final class PerfEnforceResource {
    */
   @POST
   @Path("/findSLA")
-  @Consumes(MediaType.MULTIPART_FORM_DATA)
-  public Response findSLA(@FormDataParam("querySQL") final String querySQL) throws Exception {
+  @Consumes(MediaType.TEXT_PLAIN)
+  public Response findSLA(final String querySQL) throws Exception {
     server.getPerfEnforceDriver().findSLA(querySQL);
     return Response.noContent().build();
   }
@@ -104,8 +103,8 @@ public final class PerfEnforceResource {
    */
   @POST
   @Path("/recordRealRuntime")
-  @Consumes(MediaType.MULTIPART_FORM_DATA)
-  public Response recordRealRuntime(@FormDataParam("dataPointRuntime") final Double queryRuntime)
+  @Consumes(MediaType.TEXT_PLAIN)
+  public Response recordRealRuntime(final Double queryRuntime)
       throws PerfEnforceException {
     server.getPerfEnforceDriver().recordRealRuntime(queryRuntime);
     return Response.noContent().build();
