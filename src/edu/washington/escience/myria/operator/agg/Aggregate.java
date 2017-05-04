a788436d35a3ee75d326918810961abeed6aff31
jingjingwang <jwang@cs.washington.edu>
2017-05-04 16:02:58 -0700

    use numOutputTuples instead of a flag

--- src/edu/washington/escience/myria/operator/agg/Aggregate.java
+++ src/edu/washington/escience/myria/operator/agg/Aggregate.java
@@ -47,8 +47,6 @@ public class Aggregate extends UnaryOperator {
   protected final int[] gfields;
   /** Buffer for restoring results. */
   protected TupleBatchBuffer resultBuffer;
-  /** If we have outputted 0 as the count for count(*) on an empty relation. */
-  private boolean COUNTALL_ON_EMPTY;
 
   /**
    * Groups the input tuples according to the specified grouping fields, then produces the specified aggregates.
@@ -105,14 +103,13 @@ public class Aggregate extends UnaryOperator {
       tb = child.nextReady();
     }
     if (child.eos()) {
-      if (!COUNTALL_ON_EMPTY
+      if (getNumOutputTuples() == 0
           && gfields.length == 0
           && groupStates.numTuples() == 0
           && internalAggs.size() == 1
           && internalAggs.get(0) instanceof PrimitiveAggregator
           && ((PrimitiveAggregator) (internalAggs.get(0))).aggOp == AggregationOp.COUNT) {
         /* Special check for count(*) on an empty relation: emit 0. Do it only when count(*) is the only aggregate and with no group by. */
-        COUNTALL_ON_EMPTY = true;
         resultBuffer.putLong(0, 0);
       }
       generateResult();
