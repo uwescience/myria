55247a04820d96c9e1152979822e82cb839ad2a7
Dennis Orzikh <orzikhd@uw.edu>
2017-07-13 12:17:34 -0700

    removed unused imports

--- src/edu/washington/escience/myria/api/SQLResource.java
+++ src/edu/washington/escience/myria/api/SQLResource.java
@@ -3,32 +3,18 @@
  */
 package edu.washington.escience.myria.api;
 
-import java.io.IOException;
-import java.util.List;
-
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
-import javax.ws.rs.core.Response.Status;
-
-import org.apache.commons.httpclient.HttpStatus;
 import org.slf4j.LoggerFactory;
 
 import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiResponse;
-import com.wordnik.swagger.annotations.ApiResponses;
-
 import edu.washington.escience.myria.DbException;
-import edu.washington.escience.myria.api.encoding.CreateFunctionEncoding;
-import edu.washington.escience.myria.api.encoding.FunctionStatus;
 import edu.washington.escience.myria.parallel.Server;
 
 /**
