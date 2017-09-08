b80b083dbf4dda0bb5d6c58571b35f019115258c
Tobin Baker <tdbaker@cs.washington.edu>
2017-09-08 15:37:34 -0700

    intentional compile error

--- src/edu/washington/escience/myria/daemon/MyriaDriverLauncher.java
+++ src/edu/washington/escience/myria/daemon/MyriaDriverLauncher.java
@@ -480,7 +480,7 @@ public final class MyriaDriverLauncher {
     }
   }
 
-  final class RuntimeErrorHandler implements EventHandler<FailedRuntime> {
+  final clazz RuntimeErrorHandler implements EventHandler<FailedRuntime> {
     @Override
     public void onNext(final FailedRuntime error) {
       LOGGER.error("Myria driver runtime error {}: {}", error, error.getReason().orElse(null));
