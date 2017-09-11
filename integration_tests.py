5a3d59082308472f4a377652e973b954226ec60a
jortiz16 <jortiz16@cs.washington.edu>
2017-09-10 22:17:38 -0700

    reverted back changes to parallel ingest test

--- integration_tests.py
+++ integration_tests.py
@@ -387,7 +387,7 @@ T1 = load('{}',csv(schema(a:int,b:int))); T2 = [from T1 emit a, count(*) as c]; 
             count_result.to_dict(), uda_result.to_dict())
 
 class ParallelIngestTest(MyriaTestBase):
-    def test(self):
+    def smallFile(self):
         twitterDataS3 ='s3://uwdb/sampleData/TwitterK.csv'
         query_ingest = """
 T1 = load('{}',csv(schema(a:int,b:int))); T2 = [from T1 emit *]; store(T2, parallelIngest);
@@ -395,6 +395,7 @@ T1 = load('{}',csv(schema(a:int,b:int))); T2 = [from T1 emit *]; store(T2, paral
         result = MyriaQuery.submit(query_ingest)
         self.assertEqual(len(result.to_dict()), 100)
 
+    def largeFile(self):
         twitterDataS3 ='s3://uwdb/sampleData/TwitterK-Large-100MB.txt'
         query_ingest = """
 T1 = load('{}',csv(schema(a:int,b:int))); T2 = [from T1 emit *]; store(T2, parallelIngest);
